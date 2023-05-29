f40b976148a6ecc1dcc06c0ffdec3110718f0b26
Oleg Klimov <omgtech@gmail.com>
2023-05-29 08:59:46 +0200

    delete workdir

--- refact_self_hosting/server.py
+++ refact_self_hosting/server.py
@@ -1,6 +1,7 @@
 import logging
 import asyncio
 import sys
+import os
 
 from datetime import datetime
 from pathlib import Path
@@ -16,6 +17,9 @@ from refact_self_hosting.routers import ContrastRouter
 from refact_self_hosting.routers import ChatRouter
 
 
+WORKDIR = os.path.expanduser("~/perm-storage")
+
+
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
@@ -23,13 +27,12 @@ if __name__ == "__main__":
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8008)
     parser.add_argument("--cpu", action="store_true")
-    parser.add_argument("--workdir", type=Path)
     parser.add_argument("--model", type=str)
     parser.add_argument("--finetune", type=str)
     args = parser.parse_args()
 
-    logdir = args.workdir / "logs"
-    logdir.mkdir(exist_ok=True, parents=False)
+    logdir = Path(WORKDIR) / "infserver-logs"
+    logdir.mkdir(exist_ok=True, parents=True)
     file_handler = logging.FileHandler(filename=logdir / f"server_{datetime.now():%Y-%m-%d-%H-%M-%S}.log")
     stream_handler = logging.StreamHandler(stream=sys.stdout)
     logging.basicConfig(level=logging.INFO, handlers=[stream_handler, file_handler])
@@ -45,10 +48,10 @@ if __name__ == "__main__":
     @app.on_event("startup")
     async def startup_event():
         asyncio.create_task(inference.model_setup_loop_forever(
-            model_name=args.model, workdir=args.workdir, finetune=args.finetune
+            model_name=args.model, workdir=WORKDIR, finetune=args.finetune
         ))
 
-    key_filename, cert_filename = gen_certificate(args.workdir)
+    key_filename, cert_filename = gen_certificate(WORKDIR)
     uvicorn.run(
         app, host=args.host, port=args.port,
         loop="uvloop", timeout_keep_alive=600,
