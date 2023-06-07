5900eb28f780ef9a893204b719a55bd97ac7b65a
Oleg Klimov <omgtech@gmail.com>
2023-06-07 16:57:54 +0200

    deps

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     package_data={"refact_self_hosting": ["webgui/static/*", "webgui/static/js/*", "watchdog.d/*"]},
     version="0.0.4",
     install_requires=["numpy", "tokenizers", "fastapi", "uvicorn", "termcolor",
+                      "jsonlines",
                       "huggingface_hub", "tiktoken", "cdifflib", "cloudpickle",
                       "sentencepiece", "dataclasses_json", "torch", "transformers",
                       "smallcloud", "code_contrast", "uvloop", "aiohttp", "python-multipart"
