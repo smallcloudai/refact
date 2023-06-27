03cd99e24f4ab52935fba4de93880686c31802eb
Oleg Klimov <omgtech@gmail.com>
2023-06-27 10:57:51 +0200

    access to server logs

--- refact_self_hosting/webgui/tab_server_logs.py
+++ refact_self_hosting/webgui/tab_server_logs.py
@@ -1,11 +1,11 @@
 import os
-
+import json
 import glob
-
 import time
+import asyncio
 
 from fastapi import APIRouter
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 
 from refact_self_hosting.env import DIR_LOGS
 
@@ -13,20 +13,46 @@ from refact_self_hosting.env import DIR_LOGS
 class TabServerLogRouter(APIRouter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.add_api_route("/tab-server-log-get-log", self._tab_server_log_get_log, methods=["GET"])
+        self.add_api_route("/tab-server-log-get", self._tab_server_log_get, methods=["GET"])
+        self.add_api_route("/tab-server-log-plain/{log_name}", self._tab_server_log_plain, methods=["GET"])
+        # deprecated
+        self.add_api_route("/tab-server-log-get-log", self._tab_server_log_plain, methods=["GET"])
 
-    async def _tab_server_log_get_log(self):
-        def get_log():
-            list_of_files = glob.glob(f'{DIR_LOGS}/*')
-            latest_file = max(list_of_files, key=os.path.getctime)
+    async def _tab_server_log_get(self):
+        list_of_files = glob.glob(f'{DIR_LOGS}/watchdog_*.log')
+        list_of_files.sort()
+        results = []
+        latest_log = ""
+        for f in list_of_files:
+            f = os.path.basename(f)
+            results.append(f)
+            latest_log = f
+        return Response(json.dumps({
+            "all_logs": results,
+            "latest_log": latest_log,
+        }, indent=4), media_type="application/json")
 
-            with open(latest_file, "r", encoding="utf-8") as f:
-                yield f.read()
+    async def _tab_server_log_plain(self, log_name: str = "", stream: bool = False):
+        async def get_log(right_file):
+            with open(right_file, "r", encoding="utf-8") as f:
                 while True:
-                    yield f.read()
-                    time.sleep(0.5)
+                    tmp = f.read()
+                    if not stream and not tmp:
+                        break
+                    yield tmp
+                    await asyncio.sleep(0.5)
+
+        list_of_files = glob.glob(f'{DIR_LOGS}/watchdog_*.log')
+        if log_name in ["", "latest"]:
+            list_of_files.sort()
+            list_of_files = list_of_files[-1:]
+        else:
+            list_of_files = [f for f in list_of_files if f.endswith(log_name)]
+        if len(list_of_files) == 0:
+            return Response("File \"%s\" not found\n" % log_name, media_type="text/plain")
+        right_file = list_of_files[-1]
 
         return StreamingResponse(
-            get_log(),
+            get_log(right_file),
             media_type="text/event-stream"
         )
