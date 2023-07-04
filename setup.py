50eb2f162b19ce974b3c9d66564531c2de22eef1
mitya <dimitry.ageev@gmail.com>
2023-07-04 16:16:00 +0300

    add init and setup for all modules

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="refact-self-hosting",
+    version="0.9.0",
+    py_modules=[
+        "code_contrast",
+        "encoding_wrapper",
+        "known_models_db",
+        "refact_models",
+        "self_hosting_machinery",
+    ],
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
