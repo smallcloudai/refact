1193bf0d47b9041fb088ef81b791311d7a0a0dec
mitya <dimitry.ageev@gmail.com>
2023-07-06 17:42:17 +0300

    quant cuda setup

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+import os
 from setuptools import setup, find_packages
+
+
+additional_setup_kwargs = dict()
+if os.environ.get("BUILD_QUANT_CUDA", "0") == "1":
+    try:
+        import torch
+        from torch.utils import cpp_extension
+        additional_setup_kwargs = {
+            "ext_modules": [
+                cpp_extension.CUDAExtension("quant_cuda", [
+                    "quant_cuda/quant_cuda.cpp",
+                    "quant_cuda/quant_cuda_kernel.cu"
+                ])
+            ],
+            "cmdclass": {"build_ext": cpp_extension.BuildExtension},
+        }
+    except ImportError:
+        print("To build quant_cuda extension install torch")
 
 
 setup(
@@ -31,4 +50,5 @@ setup(
         # models
         "transformers", "torch",
     ],
+    **additional_setup_kwargs,
 )
