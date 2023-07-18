91caae892d1628bf10e4171077a29849a8f4eace
mitya <dimitry.ageev@gmail.com>
2023-07-18 16:01:58 +0300

    remove old models

--- known_models_db/refact_known_models/bigcode.py
+++ known_models_db/refact_known_models/bigcode.py
@@ -1,15 +1,5 @@
 big_code_mini_db = {
 
-"starchat/15b/beta8bit": {
-    "model_path_type": "huggingface",
-    "model_path": "rahuldshetty/starchat-beta-8bit",
-    "diff_scratchpad_class": None,
-    "chat_scratchpad_class": "refact_scratchpads:ScratchpadStarChat",
-    "model_class": "refact_models:StarChatModel",
-    "T": 2048,
-    "filter_caps": ["starchat"],
-},
-
 "starcoder/santacoder": {
     "model_path_type": "huggingface",
     "model_path": "bigcode/santacoder",
@@ -19,42 +9,6 @@ big_code_mini_db = {
     "T": 2048,
     "filter_caps": ["santacoder"],
     "hidden": True,   # only for debugging because it doesn't work well
-},
-
-"starcoder/15b": {
-    "model_path_type": "huggingface",
-    "model_path": "bigcode/starcoder",
-    "diff_scratchpad_class": "refact_scratchpads:ScratchpadBigCode",
-    "chat_scratchpad_class": "refact_scratchpads:ScratchpadBigChat",
-    "model_class": "refact_models:HFModel",
-    "T": 2048,
-    "filter_caps": ["starcoder", "completion"],
-},
-
-"starcoder/15b/base4bit": {
-    "model_path_type": "huggingface",
-    "model_path": "smallcloudai/starcoder_15b_4bit",
-    "diff_scratchpad_class": "refact_scratchpads:ScratchpadBigCode",
-    "chat_scratchpad_class": "refact_scratchpads:ScratchpadBigChat",
-    "model_class": "refact_models:GPTQBigCodeModel",
-    "model_class_kwargs": {
-        "bits": 4,
-    },
-    "T": 2048,
-    "filter_caps": ["starcoder", "completion"],
-},
-
-"starcoder/15b/base8bit": {
-    "model_path_type": "huggingface",
-    "model_path": "smallcloudai/starcoder_15b_8bit",
-    "diff_scratchpad_class": "refact_scratchpads:ScratchpadBigCode",
-    "chat_scratchpad_class": "refact_scratchpads:ScratchpadBigChat",
-    "model_class": "refact_models:GPTQBigCodeModel",
-    "model_class_kwargs": {
-        "bits": 8,
-    },
-    "T": 2048,
-    "filter_caps": ["starcoder", "completion"],
 },
 
 }
