eea5f737f1389938c23a11370f1df8ae66715fef
mitya <dimitry.ageev@gmail.com>
2023-07-18 18:07:36 +0300

    correct import order

--- refact_models/codify_modules/__init__.py
+++ refact_models/codify_modules/__init__.py
@@ -1,5 +1,5 @@
+from refact_models.codify_modules.mlp import MLP
 from refact_models.codify_modules.alibias import ALiBiBias
 from refact_models.codify_modules.attention import MultiheadSelfAttention
 from refact_models.codify_modules.block import Block
 from refact_models.codify_modules.block import BlockCheckpointing
-from refact_models.codify_modules.mlp import MLP
