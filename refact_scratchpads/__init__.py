6c1df4c9d3e36ca8b0db93b52388acc1eb859647
mitya <dimitry.ageev@gmail.com>
2023-07-19 17:26:03 +0300

    remove deprecated scratchpads

--- refact_scratchpads/__init__.py
+++ refact_scratchpads/__init__.py
@@ -1,9 +1,6 @@
 from refact_scratchpads.scratchpad import ScratchpadBase
 from refact_scratchpads.scratchpad_completion import ScratchpadCompletion
 from refact_scratchpads.scratchpad_2022q4_diff import ScratchpadDiff
-from refact_scratchpads.scratchpad_bigcode import ScratchpadBigCode
-from refact_scratchpads.scratchpad_bigchat import ScratchpadBigChat
-from refact_scratchpads.scratchpad_starchat import ScratchpadStarChat
 from refact_scratchpads.scratchpad_refact import ScratchpadRefact
 
 from refact_scratchpads.scratchpad_hf import ScratchpadHuggingfaceBase
