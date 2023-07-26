95d52a508976de97b73336710c610218852e28cd
mitya <dimitry.ageev@gmail.com>
2023-07-26 20:32:03 +0300

    upper more

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ all_refact_packages = {
     ),
     "self_hosting_machinery": PyPackage(
         requires=["aiohttp", "cryptography", "fastapi==0.100.0", "giturlparse", "pydantic==1.10.10",
-                  "starlette==0.27.0", "uvicorn", "uvloop", "python-multipart", "auto-gptq==0.2.2",
+                  "starlette==0.27.0", "uvicorn", "uvloop", "python-multipart", "auto-gptq==0.3.2",
                   "torch", "transformers", "termcolor"],
         requires_packages=["refact_scratchpads", "refact_scratchpads_no_gpu", "refact_models",
                            "known_models_db", "refact_data_pipeline"],
