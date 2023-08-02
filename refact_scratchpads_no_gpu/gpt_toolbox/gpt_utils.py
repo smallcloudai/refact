930243bf058a75e246d376ca3f3d301568fb88f9
Valeryi <ponyparser@gmail.com>
2023-08-02 13:16:47 +0100

    full_line_selection moved to gpt_utils.py

--- refact_scratchpads_no_gpu/gpt_toolbox/gpt_utils.py
+++ refact_scratchpads_no_gpu/gpt_toolbox/gpt_utils.py
@@ -4,12 +4,45 @@ from typing import Dict, Optional, Tuple
 
 import tiktoken
 
-from refact_scratchpads_no_gpu.gpt_toolbox.scratchpad_utils import full_line_selection
-
 
 def msg(role: str, content: str) -> Dict[str, str]:
     assert role in ['system', 'user', 'assistant']
     return {'role': role, 'content': content}
+
+
+def full_line_selection(cursor0: int, cursor1: int, txt: str) -> Tuple[int, int, str]:
+    """
+    Adjusts selection to only include full lines.
+    """
+    c0, c1, buff = '<|cursor0|>', '<|cursor1|>', ''
+    txt: str = txt[:cursor0] + c0 + txt[cursor0:cursor1] + c1 + txt[cursor1:]
+
+    lines_new = []
+    for line in txt.split('\n'):
+        if buff:
+            line = buff + line
+            buff = ''
+        if c0 in line:
+            if not line.split(c0)[1].strip():
+                buff = c0
+                line = line.replace(c0, "")
+            else:
+                line = c0 + line.replace(c0, "")
+
+        if c1 in line:
+            if not line.split(c1)[0].strip() and lines_new:
+                lines_new[-1] += c1
+                line = line.replace(c1, "")
+            else:
+                line = line.replace(c1, "") + c1
+        lines_new.append(line)
+
+    txt_new = '\n'.join(lines_new)
+    cursor0 = txt_new.index(c0)
+    cursor1 = txt_new.replace(c0, "").index(c1)
+    selection = txt_new.replace(c0, "").replace(c1, "")[cursor0:cursor1]
+
+    return cursor0, cursor1, selection
 
 
 def code_block_postprocess(txt: str) -> str:
