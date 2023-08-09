e21a081de3fece63be65e80d9ff6982fa6913a9c
Oleg Klimov <omgtech@gmail.com>
2023-08-09 10:01:18 +0200

    WIP

--- refact_lsp/__main__.py
+++ refact_lsp/__main__.py
@@ -2,6 +2,7 @@ from refact_lsp import refact_lsp_server
 from refact_lsp import test_code_completion
 import argparse
 import asyncio
+import logging
 
 
 def main():
@@ -9,11 +10,13 @@ def main():
     parser.add_argument('--test', action='store_true', help='Run tests')
     parser.add_argument('--tcp-server', type=int, help='Listen on 127.0.0.1 on the specified port, useful for debugging')
     args = parser.parse_args()
+    # logging.basicConfig(level=logging.DEBUG)
 
     loop = asyncio.new_event_loop()
     if args.test:
         loop.run_until_complete(test_code_completion.test_everything())
     elif args.tcp_server:
+        print("listening on 127.0.0.1 on port %d" % args.tcp_server)
         refact_lsp_server.server.start_tcp("127.0.0.1", args.tcp_server)
     else:
         parser.print_help()
--- refact_lsp/refact_lsp_server.py
+++ refact_lsp/refact_lsp_server.py
@@ -1,14 +1,28 @@
 import asyncio
+import os
 from pygls.server import LanguageServer
 from refact_lsp import refact_client
+from typing import Any, Coroutine, Optional
+import aiohttp
 
 
 from lsprotocol.types import (
     TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DID_CHANGE,
+    CANCEL_REQUEST,
+    InitializeParams,
+    InitializeResult,
+    ServerCapabilities,
+    TextDocumentSyncOptions,
+    TextDocumentSyncKind,
+    CancelRequestNotification,
+    DidChangeTextDocumentParams,
     CompletionItem,
     CompletionList,
     CompletionParams,
     CompletionItemKind,
+    CompletionOptions,
+    CancelParams,
     TextEdit,
     Range,
     Position,
@@ -18,31 +32,60 @@ from lsprotocol.types import (
 server = LanguageServer("refact-lsp", "v0.1")
 
 
+global_only_one_active_request: Optional[Coroutine[Any, Any, Any]] = None
+global_socket_session: Optional[aiohttp.ClientSession] = None
+
+
 @server.feature(TEXT_DOCUMENT_COMPLETION)
 async def completions(params: CompletionParams):
     items = []
     document = server.workspace.get_document(params.text_document.uri)
-    current_line = document.lines[params.position.line].strip()
-    print("\"%s\"" % current_line)
-    if current_line.endswith("hello."):
-        items = [
-            CompletionItem(label="world"),
-            CompletionItem(label="friend"),
-        ]
-    if current_line.endswith("trigger_text"):
-        items = [
-            CompletionItem(
-                label="trigger_text.line1\nline2\nline3",
-                text_edit=TextEdit(
-                    range=Range(
-                        start=Position(line=params.position.line, character=params.position.character - len("trigger_text")),
-                        end=params.position
-                    ),
-                    new_text="trigger_text.line1\nline2\nline3"
+    uri = params.text_document.uri
+    root_uri = server.workspace.root_uri
+    if uri.startswith(root_uri):
+        short_filename = uri[len(root_uri):].lstrip("/")
+    else:
+        short_filename = os.path.basename(uri)
+    print("file \"%s\"" % uri)
+    print("root_uri \"%s\"" % root_uri)
+    print("short path \"%s\"" % short_filename)
+
+    try:
+        files = {
+            short_filename: "\n".join(document.lines),
+        }
+
+        current_line = document.lines[params.position.line].strip()
+        print("asked for completions \"%s\"" % current_line)
+        if current_line.endswith("trigger_text"):
+            print(files)
+            # completion_coroutine = refact_client.regular_code_completion(
+            #     global_socket_session,
+            #     files,
+            #     "hello_world.py",
+            #     len(example1),
+            #     50,
+            #     multiline=True,
+            # )
+
+            items = [
+                CompletionItem(
+                    label="trigger_text.line1\nline2\nline3",
+                    text_edit=TextEdit(
+                        range=Range(
+                            start=Position(line=params.position.line, character=params.position.character - len("trigger_text")),
+                            end=params.position
+                        ),
+                        new_text="trigger_text.line1\nline2\nline3"
+                    )
                 )
-            )
-        ]
-        await asyncio.sleep(1)
+            ]
+            await asyncio.sleep(5)
+
+    except asyncio.CancelledError:
+        print("Cancelled")
+    finally:
+        print("finally", current_line.strip())
 
     return CompletionList(
         is_incomplete=False,
@@ -50,5 +93,22 @@ async def completions(params: CompletionParams):
     )
 
 
-if __name__ == '__main__':
-    server.start_tcp("127.0.0.1", 1337)
+@server.feature(TEXT_DOCUMENT_DID_CHANGE)
+async def did_change(params: DidChangeTextDocumentParams):
+    for change in params.content_changes:
+        # Process each content change
+        range = change.range
+        text = change.text
+        print("change is \"%s\"" % text)
+    return None
+
+
+@server.feature(CANCEL_REQUEST)
+async def cancel_request(params: CancelParams):
+    print("Cancel request (1) received", params.id)
+
+
+# server.register_capability(CANCEL_REQUEST)
+# @server.notification(CancelRequestNotification)
+# async def handle_cancel_request(params: CancelParams):
+#     print("Cancel request (2) received", params.id)
