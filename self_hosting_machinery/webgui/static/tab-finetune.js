c94fb41b6a1bc28c42f184e8773b7fe0ca1830af
oxyplay <max@oxyplay.com>
2023-08-30 20:29:10 +0100

    hide eta for finished runs

--- self_hosting_machinery/webgui/static/tab-finetune.js
+++ self_hosting_machinery/webgui/static/tab-finetune.js
@@ -122,6 +122,7 @@ function render_runs() {
             run_status.innerHTML = `<span class="badge rounded-pill ${status_color}">${element.status}</span>`;
         }
         if (element.eta_minutes) {
+            if(element.status === 'completed' || element.status === 'finished') { return; }
             run_minutes.innerHTML = element.eta_minutes;
         }
         run_steps.innerHTML = element.worked_steps;
