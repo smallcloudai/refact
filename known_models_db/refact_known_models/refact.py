9fbd8c28413ffc91a802a1a47a5de8479bb2cac1
mitya <dimitry.ageev@gmail.com>
2023-09-01 18:21:29 +0300

    temporary disable finetune property for refact model

--- known_models_db/refact_known_models/refact.py
+++ known_models_db/refact_known_models/refact.py
@@ -6,7 +6,7 @@ refact_mini_db = {
         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceRefact",
         "model_class_kwargs": {},
         "required_memory_mb": 6000,
-        "filter_caps": ["Refact", "completion", "finetune"],
+        "filter_caps": ["Refact", "completion"],
     },
 
     "CONTRASTcode/medium/multi": {
