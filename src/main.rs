233f05c3a6016b827d89d88f246ceff7910eea96
Oleg Klimov <omgtech@gmail.com>
2023-09-02 20:39:09 +0200

    cleanup, warnings

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
 // use reqwest::header::AUTHORIZATION;
 // use ropey::Rope;
+use std::convert::Infallible;
+use std::net::SocketAddr;
 use serde::{Deserialize, Serialize};
-use serde_json::Error as SerdeJsonError;
-use std::collections::HashMap;
+// use serde_json::Error as SerdeJsonError;
+// use std::collections::HashMap;
 
-use std::fmt::Display;
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
+// use std::sync::Arc;
 
-use tokenizers::Tokenizer;
+// use tokenizers::Tokenizer;
 
-use tokio::io::AsyncWriteExt;
-use tokio::sync::RwLock;
+// use tokio::io::AsyncWriteExt;
+// use tokio::sync::RwLock;
+// use async_trait::async_trait;
 
 use hyper::{Body, Request, Response, Server};
+use hyper::{Method, StatusCode};
+use hyper::server::conn::{AddrStream};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::Error;
 
-use tower::ServiceBuilder;
+use route_recognizer::{Match, Params, Router};
+
+// use tower::ServiceBuilder;
 
 use tracing::{error, info};
 
+// https://blog.logrocket.com/a-minimal-web-service-in-rust-using-hyper/
 
-async fn hello_world() {
-    info!("test test test");
+
+#[derive(Debug)]
+struct MyContext {
+    router: Router<String>,
 }
-
 
 #[derive(Debug, Deserialize)]
 struct MyRequest {
@@ -33,59 +39,12 @@ struct MyRequest {
 }
 
 
-// https://blog.logrocket.com/a-minimal-web-service-in-rust-using-hyper/
-
-// #[derive(Debug)]
-// pub struct Context {
-//     pub state: AppState,
-//     pub req: Request<Body>,
-//     pub params: Params,
-//     body_bytes: Option<Bytes>,
-// }
-
-// route-recognizer = "0.2"
-// bytes = "0.5"
-// async-trait = "0.1"
-
-// pub async fn param_handler(ctx: Context) -> String {
-//     let param = match ctx.params.find("some_param") {
-//         Some(v) => v,
-//         None => "empty",
-//     };
-//     format!("param called, param was: {}", param)
-// }
-
-// async fn route(
-//     router: Arc<Router>,
-//     req: Request<hyper::Body>,
-//     app_state: AppState,
-// ) -> Result<Response, Error> {
-//     let found_handler = router.route(req.uri().path(), req.method());
-//     let resp = found_handler
-//         .handler
-//         .invoke(Context::new(app_state, req, found_handler.params))
-//         .await;
-//     Ok(resp)
-// }
-
-// pub async fn body_json<T: serde::de::DeserializeOwned>(&mut self) -> Result<T, Error> {
-//     let body_bytes = match body_bytes {
-//         Some(ref v) => v,
-//         _ => {
-//             let body = to_bytes(self.req.body_mut()).await?;
-//             body_bytes = Some(body);
-//             body_bytes.as_ref().expect("body_bytes was set above")
-//         }
-//     };
-//     Ok(serde_json::from_slice(&body_bytes)?)
-// }
-
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    // Deserialize the request body into the struct
-    let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
-    let my_request_result = serde_json::from_slice(&body_bytes);
+async fn handle_v1_code_completion(
+    body_bytes: hyper::body::Bytes
+) -> Result<Response<Body>, hyper::Error> {
+    let my_request_result = serde_json::from_slice::<MyRequest>(&body_bytes);
     let my_request: MyRequest = match my_request_result {
-        Ok(my_request) => my_request,
+        Ok(x) => x,
         Err(e) => {
             error!("Error deserializing request body: {}", e);
             return Ok(Response::builder()
@@ -95,45 +54,66 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
               .into());
         }
     };
-
-    let txt = format!("model was: {}", my_request.model);
-
+    let txt = format!("hurray a call! model was: {}\n",
+        my_request.model,
+        );
+    info!("handle_v1_code_completion returning: {}", txt);
     let response = Response::builder()
         .header("Content-Type", "application/json")
         .body(Body::from(txt))
         .unwrap();
+    Ok(response)
+}
 
+async fn handle_request(
+    remote_addr: SocketAddr,
+    path: String,
+    method: Method,
+    req: Request<Body>,
+) -> Result<Response<Body>, hyper::Error> {
+    if method!= Method::POST {
+        return Ok(Response::builder()
+           .status(StatusCode::METHOD_NOT_ALLOWED)
+           .body(Body::from("Method not allowed"))
+           .unwrap());
+    }
+    let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
+    if path.starts_with("/v1/code-completion") {
+        return handle_v1_code_completion(body_bytes).await;
+    }
+    let txt = format!("not found path {}, your ip is {}\n",
+        path,
+        remote_addr);
+    let response = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .header("Content-Type", "application/json")
+        .body(Body::from(txt))
+        .unwrap();
     Ok(response)
 }
 
 
 #[tokio::main]
 async fn main() {
-    // let mut stdout = tokio::io::stdout();
     let _builder1 = tracing_subscriber::fmt()
         .with_writer(std::io::stdout)
         .with_target(true)
         .with_line_number(true)
         .compact()
         .init();
-    // stdout.write_all(b"Hello, world\n").await.unwrap();
-    hello_world().await;
-
-    // let socket = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let make_svc = make_service_fn(|_conn| {
-        async {
-            Ok::<_, hyper::Error>(service_fn(handle_request))
+    let make_svc = make_service_fn(|conn: &AddrStream| {
+        let remote_addr = conn.remote_addr();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                let path = req.uri().path().to_string();
+                let method = req.method().clone();
+                handle_request(remote_addr, path, method, req)
+            }))
         }
     });
-
-    let addr = ([127, 0, 0, 1], 8000).into();
-    let server = Server::bind(&addr)
-        .serve(make_svc);
-    // let server = Server::new(socket, make_svc).serve(addr);
-
+    let addr = ([127, 0, 0, 1], 8008).into();
+    let server = Server::bind(&addr).serve(make_svc);
     println!("Server listening on http://{}", addr);
-
-
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
