70d57d80fc1a5d85d3025a3b99a3955ff1a4f89d
adam-weinberger <adam.e.weinberger@gmail.com>
2023-10-14 12:01:41 +0000

    Code Llama fine-tuning but fails on checkpoint

--- refact_data_pipeline/finetune/finetune_utils.py
+++ refact_data_pipeline/finetune/finetune_utils.py
@@ -97,10 +97,12 @@ def get_active_loras(models_db: Dict[str, Any]) -> Dict[str, Dict[str, Any]]:
 def get_finetune_config(models_db: Dict[str, Any], logger: Optional[Callable] = None) -> Dict[str, Any]:
     cfg = copy.deepcopy(finetune_train_defaults)
     if os.path.exists(env.CONFIG_FINETUNE):
+        print('user config exists')
         if logger is not None:
             logger("Reading %s" % env.CONFIG_FINETUNE)
         cfg.update(**json.load(open(env.CONFIG_FINETUNE)))
     if cfg.get("model_name", None) not in models_db:
+        print('user config does not exist')
         cfg["model_name"] = default_finetune_model
     return cfg
 
