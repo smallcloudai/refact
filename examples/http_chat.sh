a93411b1ad96cb3b932ae2a43eed9a8218dad2bd
Oleg Klimov <omgtech@gmail.com>
2023-10-19 16:18:04 +0200

    http examples

---
+++ examples/http_chat.sh
@@ -0,0 +1,15 @@
+curl http://127.0.0.1:8001/v1/chat -k \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {"role": "user", "content": "Who is Bill Clinton? What is his favorite programming language?"}
+  ],
+  "stream": true,
+  "parameters": {
+    "temperature": 0.1,
+    "max_new_tokens": 1000
+  }
+}'
+
+# Other possible parameters:
+# "model": "meta-llama/Llama-2-70b-chat-hf",
---
+++ examples/http_completion.sh
@@ -0,0 +1,22 @@
+curl http://127.0.0.1:8001/v1/code-completion -k \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "inputs": {
+    "sources": {"hello.py": "def hello_world():"},
+    "cursor": {
+      "file": "hello.py",
+      "line": 0,
+      "character": 18
+    },
+    "multiline": true
+  },
+  "stream": false,
+  "parameters": {
+    "temperature": 0.1,
+    "max_new_tokens": 20
+  }
+}'
+
+# Other possible parameters:
+# "scratchpad": "FIM-PSM",
+# "model": "smallcloudai/Refact-1_6b-fim",
