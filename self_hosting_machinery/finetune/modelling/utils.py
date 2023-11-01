4fb00e50ddcda825e6314472b22c94e072fdd3fd
JegernOUTT <sergey.vakhreev@gmail.com>
2023-11-01 18:32:33 +1030

    fixing layer retrieval for some models (now we correctly find layers for some models to set a lora to them)

--- self_hosting_machinery/finetune/modelling/utils.py
+++ self_hosting_machinery/finetune/modelling/utils.py
@@ -10,7 +10,8 @@ def map_model_specific_params(
 ) -> Tuple[List[str], List[str]]:
     assert model_name in supported_models.config
     model_config = supported_models.config[model_name]
-    freeze_exceptions = [model_config["freeze_exceptions_mapping"][e] for e in freeze_exceptions]
+    freeze_exceptions = [mapped for e in freeze_exceptions
+                         for mapped in model_config["freeze_exceptions_mapping"][e]]
     lora_target_modules_mapping = [m for modules in lora_target_modules
                                    for m in model_config["lora_target_modules_mapping"][modules]]
     return list(set(freeze_exceptions)), list(set(lora_target_modules_mapping))
