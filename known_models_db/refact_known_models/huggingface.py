c02fbc9a2102dc9a14630674f1fe6ce6ff535d46
JegernOUTT <sergey.vakhreev@gmail.com>
2023-11-06 18:09:40 +1030

    no extra kwargs

--- known_models_db/refact_known_models/huggingface.py
+++ known_models_db/refact_known_models/huggingface.py
@@ -135,9 +135,7 @@ huggingface_mini_db = {
         "model_path": "deepseek-ai/deepseek-coder-1.3b-base",
         "diff_scratchpad_class": "refact_scratchpads:ScratchpadDeepSeekCoderFIM",
         "chat_scratchpad_class": None,
-        "model_class_kwargs": {
-            "load_in_4bit": True,
-        },
+        "model_class_kwargs": {},
         "T": 4096,
         "filter_caps": ["completion", "finetune"],
      },
@@ -146,9 +144,7 @@ huggingface_mini_db = {
         "model_path": "deepseek-ai/deepseek-coder-6.7b-base",
         "diff_scratchpad_class": "refact_scratchpads:ScratchpadDeepSeekCoderFIM",
         "chat_scratchpad_class": None,
-        "model_class_kwargs": {
-            "load_in_4bit": True,
-        },
+        "model_class_kwargs": {},
         "T": 4096,
         "filter_caps": ["completion", "finetune"],
      },
