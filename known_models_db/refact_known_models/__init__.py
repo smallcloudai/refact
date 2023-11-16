0c2cd5be9673d493870135187f927cae8bd74233
mitya <dimitry.ageev@gmail.com>
2023-11-16 11:52:23 +0300

    add all prev models

--- known_models_db/refact_known_models/__init__.py
+++ known_models_db/refact_known_models/__init__.py
@@ -2,9 +2,24 @@ from known_models_db.refact_known_models.utils import ModelSpec
 from known_models_db.refact_known_models.utils import ModelRegistry
 
 from known_models_db.refact_known_models.refact import refact_specs
-from known_models_db.refact_known_models.huggingface import huggingface_specs
+from known_models_db.refact_known_models.huggingface import starcoder_specs
+from known_models_db.refact_known_models.huggingface import wizardcoder_specs
+from known_models_db.refact_known_models.huggingface import codellama_specs
+from known_models_db.refact_known_models.huggingface import deepseek_specs
+from known_models_db.refact_known_models.huggingface import starchat_specs
+from known_models_db.refact_known_models.huggingface import wizardlm_specs
+from known_models_db.refact_known_models.huggingface import llama2_specs
 
 from itertools import chain
 
 
-models_registry: ModelRegistry = ModelRegistry(chain(refact_specs, huggingface_specs))
+models_registry: ModelRegistry = ModelRegistry(chain(
+    refact_specs,
+    starcoder_specs,
+    wizardcoder_specs,
+    codellama_specs,
+    deepseek_specs,
+    starchat_specs,
+    wizardlm_specs,
+    llama2_specs,
+))
--- known_models_db/refact_known_models/huggingface.py
+++ known_models_db/refact_known_models/huggingface.py
@@ -1,172 +1,10 @@
-# huggingface_mini_db = {
-#     "starcoder/15b/base": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/starcoder-GPTQ",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 18000,
-#         "T": 4096,
-#         "filter_caps": ["completion"],
-#     },
-#     "starcoder/15b/plus": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/starcoderplus-GPTQ",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 18000,
-#         "T": 4096,
-#         "filter_caps": ["completion"],
-#     },
-#     "starchat/15b/beta": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/starchat-beta-GPTQ",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceStarChat",
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 18000,
-#         "T": 4096,
-#         "filter_caps": ["starchat"],
-#     },
-#     "starcoder/1b/base": {
-#         "backend": "transformers",
-#         "model_path": "smallcloudai/starcoderbase-1b",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 8000,
-#         "T": 8192,
-#         "filter_caps": ["completion", "finetune"],
-#     },
-#     "starcoder/3b/base": {
-#         "backend": "transformers",
-#         "model_path": "smallcloudai/starcoderbase-3b",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 12000,
-#         "T": 4096,
-#         "filter_caps": ["completion", "finetune"],
-#     },
-#     "starcoder/7b/base": {
-#         "backend": "transformers",
-#         "model_path": "smallcloudai/starcoderbase-7b",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 20000,
-#         "T": 4096,
-#         "filter_caps": ["completion", "finetune"],
-#     },
-#     "wizardcoder/15b": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/WizardCoder-15B-1.0-GPTQ",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadPSM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 18000,
-#         "T": 4096,
-#         "filter_caps": ["completion"],
-#     },
-#     "wizardlm/7b": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/WizardLM-7B-V1.0-Uncensored-GPTQ",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceWizard",
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 8000,
-#         "T": 2048,
-#         "filter_caps": ["wizardlm"],
-#     },
-#     "wizardlm/13b": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/WizardLM-13B-V1.1-GPTQ",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceWizard",
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 14000,
-#         "T": 2048,
-#         "filter_caps": ["wizardlm"],
-#     },
-#     "llama2/7b": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/Llama-2-7b-Chat-GPTQ",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceLlama2",
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 8000,
-#         "T": 2048,
-#         "filter_caps": ["llama2"],
-#     },
-#     "llama2/13b": {
-#         "backend": "autogptq",
-#         "model_path": "TheBloke/Llama-2-13B-chat-GPTQ",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceLlama2",
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 14000,
-#         "T": 2048,
-#         "filter_caps": ["llama2"],
-#     },
-#     "codellama/7b": {
-#         "backend": "transformers",
-#         "model_path": "TheBloke/CodeLlama-7B-fp16",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadCodeLlamaSPM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "required_memory_mb": 14000,
-#         "T": 2048,
-#         "filter_caps": ["completion", "finetune"],
-#     },
-#     "wizardlm/30b": {
-#         "backend": "transformers",
-#         "model_path": "TheBloke/WizardLM-30B-fp16",
-#         "diff_scratchpad_class": None,
-#         "chat_scratchpad_class": "refact_scratchpads:ScratchpadHuggingfaceWizard",
-#         "model_class_kwargs": {
-#             "load_in_4bit": True,
-#         },
-#         "T": 2048,
-#         "filter_caps": ["wizardlm"],
-#      },
-#     "deepseek-coder/1.3b/base": {
-#         "backend": "transformers",
-#         "model_path": "deepseek-ai/deepseek-coder-1.3b-base",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadDeepSeekCoderFIM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "T": 4096,
-#         "filter_caps": ["completion", "finetune"],
-#      },
-#     "deepseek-coder/5.7b/mqa-base": {
-#         "backend": "transformers",
-#         "model_path": "deepseek-ai/deepseek-coder-5.7bmqa-base",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadDeepSeekCoderFIM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "T": 4096,
-#         "filter_caps": ["completion", "finetune"],
-#      },
-#     "deepseek-coder/6.7b/base": {
-#         "backend": "transformers",
-#         "model_path": "deepseek-ai/deepseek-coder-6.7b-base",
-#         "diff_scratchpad_class": "refact_scratchpads:ScratchpadDeepSeekCoderFIM",
-#         "chat_scratchpad_class": None,
-#         "model_class_kwargs": {},
-#         "T": 4096,
-#         "filter_caps": ["completion", "finetune"],
-#      },
-# }
-
-
 from known_models_db.refact_known_models.utils import ModelSpec
 from known_models_db.refact_known_models.utils import model_specs_from_list
 
 from typing import List
 
 
-huggingface_specs: List[ModelSpec] = [
+starcoder_specs: List[ModelSpec] = [
     *model_specs_from_list(
         name="starcoder/15b/base", context_sizes=[4096], completion=True,
         diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
@@ -174,6 +12,191 @@ huggingface_specs: List[ModelSpec] = [
             {
                 "backend": "autogptq",
                 "model_path": "TheBloke/starcoder-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="starcoder/15b/plus", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/starcoderplus-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="starcoder/1b/base", context_sizes=[8192], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "smallcloudai/starcoderbase-1b",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="starcoder/3b/base", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "smallcloudai/starcoderbase-3b",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="starcoder/7b/base", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "smallcloudai/starcoderbase-7b",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+]
+
+wizardcoder_specs = [
+    *model_specs_from_list(
+        name="wizardcoder/15b", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadPSM",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/WizardCoder-15B-1.0-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+]
+
+codellama_specs = [
+    *model_specs_from_list(
+        name="codellama/7b", context_sizes=[2048], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadCodeLlamaSPM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "TheBloke/CodeLlama-7B-fp16",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+]
+
+deepseek_specs = [
+    *model_specs_from_list(
+        name="deepseek-coder/1.3b/base", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadDeepSeekCoderFIM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "deepseek-ai/deepseek-coder-1.3b-base",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="deepseek-coder/5.7b/mqa-base", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadDeepSeekCoderFIM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "deepseek-ai/deepseek-coder-5.7bmqa-base",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="deepseek-coder/6.7b/base", context_sizes=[4096], completion=True,
+        diff_scratchpad_class="refact_scratchpads:ScratchpadDeepSeekCoderFIM",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "deepseek-ai/deepseek-coder-6.7b-base",
+                "finetune": True,
+                "default": True, "default_finetune": True,
+            },
+        ]),
+]
+
+starchat_specs = [
+    *model_specs_from_list(
+        name="starchat/15b/beta", context_sizes=[4096], filter_caps=["starchat"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceStarChat",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/starchat-beta-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+]
+
+
+wizardlm_specs = [
+    *model_specs_from_list(
+        name="wizardlm/7b", context_sizes=[2048], filter_caps=["wizardlm"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceWizard",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/WizardLM-7B-V1.0-Uncensored-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="wizardlm/13b", context_sizes=[2048], filter_caps=["wizardlm"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceWizard",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/WizardLM-13B-V1.1-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="wizardlm/30b", context_sizes=[2048], filter_caps=["wizardlm"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceWizard",
+        specs_kwargs=[
+            {
+                "backend": "transformers",
+                "model_path": "TheBloke/WizardLM-30B-fp16",
+                "quantization": "4 bit",
+                "model_class_kwargs": {"load_in_4bit": True},
+                "default": True,
+            },
+        ]),
+]
+
+llama2_specs = [
+    *model_specs_from_list(
+        name="llama2/7b", context_sizes=[2048], filter_caps=["llama2"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceLlama2",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/Llama-2-7b-Chat-GPTQ",
+                "quantization": "4 bit",
+                "default": True,
+            },
+        ]),
+    *model_specs_from_list(
+        name="llama2/13b", context_sizes=[2048], filter_caps=["llama2"],
+        chat_scratchpad_class="refact_scratchpads:ScratchpadHuggingfaceLlama2",
+        specs_kwargs=[
+            {
+                "backend": "autogptq",
+                "model_path": "TheBloke/Llama-2-13B-chat-GPTQ",
                 "quantization": "4 bit",
                 "default": True,
             },
