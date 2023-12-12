d8da80334271840d8dc0c537d99a81a0097389a3
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-12-12 14:32:13 +0100

    chore(tooling): set up vite, vitest, eslint and. prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    "plugin:react/jsx-runtime",
+    'plugin:react-hooks/recommended',
+    "prettier",
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    }
+  }
+}
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { PluginOption, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    eslint(),
+  ] as PluginOption[],
+  test: {
+    environment: "jsdom",
+  }
+})
