bbdcdf66883074ce238c7e23b19e7cc47b1fa47a
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-12-13 11:36:32 +0100

    fix(linter): remove unused eslint-disable line statments

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -7,12 +7,9 @@ const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
 ): ReturnType<typeof render> & { user: UserEvent}  => {
-    // TODO: figure out why this type is wrong :/
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     const user = userEvent.setup();
     return {
        ...render(ui, {...options}),
-       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
        user,
     }
 }
