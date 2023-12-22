8653600fe21f5b253e040563457745cf52d2de68
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-12-22 15:32:05 +0100

    feat(dark mode): add button to toggle it one and off

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Theme as RadixTheme } from "@radix-ui/themes";
+import { Theme as RadixTheme, IconButton } from "@radix-ui/themes";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useDarkMode } from "usehooks-ts";
 import "@radix-ui/themes/styles.css";
 
-export const Theme: React.FC<React.ComponentProps<typeof RadixTheme>> = (
-  props,
-) => {
-  const { isDarkMode } = useDarkMode();
-  return <RadixTheme {...props} appearance={isDarkMode ? "dark" : "light"} />;
+export const Theme: React.FC<React.ComponentProps<typeof RadixTheme>> = ({
+  children,
+  ...props
+}) => {
+  const { isDarkMode, toggle } = useDarkMode();
+  const Icon = isDarkMode ? MoonIcon : SunIcon;
+  return (
+    <RadixTheme {...props} appearance={isDarkMode ? "dark" : "light"}>
+      <IconButton
+        variant="surface"
+        color="gray"
+        title="toggle dark mode"
+        style={{ position: "fixed", zIndex: 999, right: 15, top: 15 }}
+        onClick={toggle}
+      >
+        <Icon />
+      </IconButton>
+      {children}
+    </RadixTheme>
+  );
 };
