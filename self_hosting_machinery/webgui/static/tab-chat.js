5347f977c8a6e6a11b51e935a6f8258f84de3796
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-01-05 20:27:18 +0100

    wip: render the chat in page

--- self_hosting_machinery/webgui/static/tab-chat.js
+++ self_hosting_machinery/webgui/static/tab-chat.js
@@ -1,4 +1,24 @@
-export function init() {
+export async function init() {
   const elem = document.querySelector("#chat");
-  RefactChat(elem, "lsp");
+  const iframe = document.createElement("iframe");
+  iframe.src = "tab-chat.html";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.minHeight = "480px";
+
+  elem.appendChild(iframe);
+  // const req = await fetch("tab-chat.html")
+  // const text = await req.text();
 }
+
+export function tab_switched_here() {
+  // const elem = document.querySelector("#chat");
+  // RefactChat(elem, "lsp");
+}
+
+export function tab_switched_away() {
+  // const elem = document.querySelector("#chat")
+  // elem.children =  []
+}
+
+export function tab_update_each_couple_of_seconds() {}
