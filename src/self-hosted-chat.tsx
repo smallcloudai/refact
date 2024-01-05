f737df6823cf6348f7c4c76b4651bf7e81d1339b
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-01-05 17:08:44 +0100

    chore: add code comments for self hosted lib

--- src/self-hosted-chat.tsx
+++ src/self-hosted-chat.tsx
@@ -4,6 +4,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+/**
+ *
+ * @param element element to add the chat to
+ * @param lspUrl where to send requests for the lsp or lsp-proxy
+ */
 function Chat(element: HTMLElement, lspUrl?: string) {
   ReactDOM.createRoot(element).render(<App lspUrl={lspUrl} />);
 }
