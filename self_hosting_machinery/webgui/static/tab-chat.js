873766696cc83cff43f345ffb5b1f096851ebdec
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-01-08 16:31:13 +0100

    ui(chat): use bootstrap classes for the chat pane

--- self_hosting_machinery/webgui/static/tab-chat.js
+++ self_hosting_machinery/webgui/static/tab-chat.js
@@ -1,24 +1,23 @@
 export async function init() {
   const elem = document.querySelector("#chat");
+  const flex = document.createElement("div");
+  flex.style.display = "flex";
   const iframe = document.createElement("iframe");
   iframe.src = "tab-chat.html";
-  iframe.style.width = "100%";
+  iframe.className = "container-lg pane";
+  // TODO: calculate hight for multiple displays
   iframe.style.height = "100%";
-  iframe.style.minHeight = "480px";
-
-  elem.appendChild(iframe);
-  // const req = await fetch("tab-chat.html")
-  // const text = await req.text();
+  iframe.style.minHeight = "470px";
+  flex.appendChild(iframe);
+  elem.appendChild(flex);
 }
 
 export function tab_switched_here() {
-  // const elem = document.querySelector("#chat");
-  // RefactChat(elem, "lsp");
+
 }
 
 export function tab_switched_away() {
-  // const elem = document.querySelector("#chat")
-  // elem.children =  []
+
 }
 
 export function tab_update_each_couple_of_seconds() {}
