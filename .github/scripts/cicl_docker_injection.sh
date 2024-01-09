cf70f8c0ec66e5999f582e754758d43baaa73b9a
Kirill Starkov <starkov.kirill123@gmail.com>
2024-01-09 15:23:40 +0800

    add nightly and release workflows
    fix workflows

--- .github/scripts/cicl_docker_injection.sh
+++ .github/scripts/cicl_docker_injection.sh
@@ -4,9 +4,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR=$(realpath $SCRIPT_DIR/../..)
 
 ORIGIN_DOCKERFILE=${REPO_DIR}/Dockerfile
-BASE_IMAGE=$(cat $ORIGIN_DOCKERFILE | grep FROM | head -1 | awk '{print $2}')
 
-CACHE_IMAGE="ghcr.io/smallcloud/refact_base_image:latest"
+CACHE_IMAGE="FROM ghcr.io/smallcloudai/refact_base_image:latest"
 
-sed -i "s!${BASE_IMAGE}!${CACHE_IMAGE}!" ${ORIGIN_DOCKERFILE}
+sed -i "s!INCLUDE Dockerfile.base!${CACHE_IMAGE}!" ${ORIGIN_DOCKERFILE}
 
