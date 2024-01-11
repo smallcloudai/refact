eae15b957325a03f000107725f55426729218d4b
Valeryi <ponyparser@gmail.com>
2024-01-11 16:27:28 +0000

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -z "$REFACT_DATABASE_HOST" ]; then
-    sudo MAX_HEAP_SIZE=4G HEAP_NEWSIZE=200M service cassandra start
+    sudo sed -i '/MAX_HEAP_SIZE/c\MAX_HEAP_SIZE="4G"' /etc/cassandra/cassandra-env.sh
+    sudo sed -i '/HEAP_NEWSIZE/c\HEAP_NEWSIZE="400M"' /etc/cassandra/cassandra-env.sh
+
     echo "cassandra database started on localhost"
 fi
 python -m self_hosting_machinery.watchdog.docker_watchdog
