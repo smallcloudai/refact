79d6718f2e8ff1f2213f012d04b3e06d60bff3b6
Valeryi <ponyparser@gmail.com>
2024-01-15 14:03:41 +0000

    implemented mitya's suggestion. Fixed deps

--- self_hosting_machinery/webgui/tab_models_host.py
+++ self_hosting_machinery/webgui/tab_models_host.py
@@ -51,15 +51,6 @@ class TabHostRouter(APIRouter):
             else:
                 validated["completion"] = ""
 
-        current_embeddings_model = validated.get("embeddings", "")
-        if not current_embeddings_model or current_embeddings_model not in post.model_assign:
-            for info in self._model_assigner.models_info["models"]:
-                if info["has_embeddings"] and info["name"] in post.model_assign:
-                    validated["embeddings"] = info["name"]
-                    break
-            else:
-                validated["embeddings"] = ""
-
         self._model_assigner.models_to_watchdog_configs(validated)
         self._model_assigner.restart_lsp()
         return JSONResponse("OK")
