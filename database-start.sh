a76f326c390ee48fed6689e282dd328173437637
mitya <dimitry.ageev@gmail.com>
2024-01-15 13:09:35 +0300

    less memory consumption

--- database-start.sh
+++ database-start.sh
@@ -13,7 +13,7 @@ sed -i "s|/var/lib/cassandra|$REFACT_CASSANDRA_DIR|g" /etc/cassandra/cassandra.y
 # patch cassandra.in.sh for less memory consumption and logging to REFACT_CASSANDRA_DIR/log
 REFACT_CASSANDRA_INCLUDE=/usr/sbin/cassandra.in.sh
 cp /usr/share/cassandra/cassandra.in.sh "$REFACT_CASSANDRA_INCLUDE"
-echo "MAX_HEAP_SIZE=4G" >> "$REFACT_CASSANDRA_INCLUDE"
+echo "MAX_HEAP_SIZE=2G" >> "$REFACT_CASSANDRA_INCLUDE"
 echo "HEAP_NEWSIZE=400M" >> "$REFACT_CASSANDRA_INCLUDE"
 echo "CASSANDRA_LOG_DIR=$REFACT_CASSANDRA_DIR/log" >> "$REFACT_CASSANDRA_INCLUDE"
 
