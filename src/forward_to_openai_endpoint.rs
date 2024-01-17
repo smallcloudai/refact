91a0093dd15b240b49f24955c51d3b58efa446ec
Oleg Klimov <omgtech@gmail.com>
2024-01-17 13:53:20 +0100

    simplify forward_to_openai_endpoint

--- src/forward_to_openai_endpoint.rs
+++ src/forward_to_openai_endpoint.rs
@@ -130,36 +130,31 @@ pub async fn get_embedding_openai_style(
         input: text,
         model: model_name.clone(),
     };
-
-    let maybe_response = client
-        .post(endpoint_template)
-        .bearer_auth(api_key)
+    let url = endpoint_template.clone();
+    let api_key_clone = api_key.clone();
+    let response = client
+        .post(&url)
+        .bearer_auth(api_key_clone.clone())
         .json(&payload)
         .send()
-        .await;
+        .await
+        .map_err(|e| format!("Failed to send a request: {:?}", e))?;
 
-    match maybe_response {
-        Ok(response) => {
-            if response.status().is_success() {
-                let response_json = response.json::<serde_json::Value>().await;
+    if !response.status().is_success() {
+        info!("get_embedding_openai_style: {:?}", response);
+        return Err(format!("get_embedding_openai_style: bad status: {:?}", response.status()))
+    }
 
-                match response_json {
-                    Ok(json) => match &json["data"][0]["embedding"] {
-                        serde_json::Value::Array(embedding) => {
-                            let embedding_values: Result<Vec<f32>, _> =
-                                serde_json::from_value(serde_json::Value::Array(embedding.clone()));
-                            embedding_values.map_err(|err| {
-                                format!("Failed to parse the response: {:?}", err)
-                            })
-                        }
-                        _ => Err("Response is missing 'data[0].embedding' field or it's not an array".to_string()),
-                    },
-                    Err(err) => Err(format!("Failed to parse the response: {:?}", err)),
-                }
-            } else {
-                Err(format!("Failed to get a response: {:?}", response.status()))
-            }
+    let json = response.json::<serde_json::Value>()
+        .await
+        .map_err(|err| format!("get_embedding_openai_style: failed to parse the response: {:?}", err))?;
+
+    // info!("get_embedding_openai_style: {:?}", json);
+    match &json["data"][0]["embedding"] {
+        serde_json::Value::Array(embedding) => {
+            serde_json::from_value(serde_json::Value::Array(embedding.clone()))
+                .map_err(|err| { format!("Failed to parse the response: {:?}", err) })
         }
-        Err(err) => Err(format!("Failed to send a request: {:?}", err)),
+        _ => Err("Response is missing 'data[0].embedding' field or it's not an array".to_string()),
     }
 }
