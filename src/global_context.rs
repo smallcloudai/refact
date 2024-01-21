4bf10997002c1b41bc598de89749b4818e281ec2
Oleg Klimov <omgtech@gmail.com>
2024-01-21 12:33:56 +0100

    no HTTP port by default

--- src/global_context.rs
+++ src/global_context.rs
@@ -37,7 +37,7 @@ pub struct CommandLine {
     pub address_url: String,
     #[structopt(long, short="k", default_value="", help="The API key to authenticate your requests, will appear in HTTP requests this binary makes.")]
     pub api_key: String,
-    #[structopt(long, short="p", default_value="8001", help="Bind 127.0.0.1:<port> to listen for HTTP requests, such as /v1/code-completion, /v1/chat, /v1/caps.")]
+    #[structopt(long, short="p", default_value="0", help="Bind 127.0.0.1:<port> to listen for HTTP requests, such as /v1/code-completion, /v1/chat, /v1/caps.")]
     pub http_port: u16,
     #[structopt(long, default_value="", help="End-user client version, such as version of VS Code plugin.")]
     pub enduser_client_version: String,
