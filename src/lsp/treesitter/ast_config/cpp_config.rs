b9c86d15964f2b623437d4d0152fc2bac72454ea
Kirill Starkov <starkov.kirill123@gmail.com>
2024-02-01 23:35:41 +0700

    add get_vars_function_calls_static_data

--- src/lsp/treesitter/ast_config/cpp_config.rs
+++ src/lsp/treesitter/ast_config/cpp_config.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use lazy_static::lazy_static;
 use similar::DiffableStr;
 use tracing_subscriber::fmt::format;
-use tree_sitter::{Node, Parser, Tree};
+use tree_sitter::{Node, Parser, Point, Query, QueryCapture, Range, Tree};
 use crate::lsp::document::TypeDeclarationSearchInfo;
 use crate::lsp::treesitter::ast_config::{AstConfig, Language};
 use crate::lsp::treesitter::index::{Index, SymbolInfo, SymbolType};
@@ -229,7 +229,7 @@ pub fn get_indexes(parser: &mut Parser, code: &str, path: PathBuf) -> HashMap<St
                     });
                     indexes.insert(key,
                                    Index {
-                                       name: name,
+                                       name,
                                        definition_info: SymbolInfo { path: path.clone(), range },
                                        children: vec![],
                                        symbol_type: SymbolType::Function,
@@ -245,7 +245,7 @@ pub fn get_indexes(parser: &mut Parser, code: &str, path: PathBuf) -> HashMap<St
                     });
                     indexes.insert(key,
                                    Index {
-                                       name: name,
+                                       name,
                                        definition_info: SymbolInfo { path: path.clone(), range },
                                        children: vec![],
                                        symbol_type: SymbolType::GlobalVar,
@@ -258,10 +258,184 @@ pub fn get_indexes(parser: &mut Parser, code: &str, path: PathBuf) -> HashMap<St
     indexes
 }
 
+pub struct VariableInfo {
+    pub name: String,
+    pub range: Range,
+    pub type_name: Option<String>,
+}
+
+pub struct FunctionCallInfo {
+    pub name: String,
+    pub range: Range,
+}
+
+pub enum StaticType {
+    Comment,
+    Literal,
+}
+
+pub struct StaticInfo {
+    pub static_type: StaticType,
+    pub range: Range,
+}
+
+pub fn get_variable(captures: &[QueryCapture], query: &Query, code: &str) -> Option<VariableInfo> {
+    let mut var = VariableInfo {
+        name: "".to_string(),
+        range: Range {
+            start_byte: 0,
+            end_byte: 0,
+            start_point: Default::default(),
+            end_point: Default::default(),
+        },
+        type_name: None,
+    };
+    for capture in captures {
+        let capture_name = &query.capture_names()[capture.index as usize];
+        match capture_name.as_str() {
+            "variable" => {
+                var.range = capture.node.range();
+            }
+            "variable_name" => {
+                let text = code.slice(capture.node.byte_range());
+                var.name = text.to_string();
+            }
+            "variable_type" => {
+                let text = code.slice(capture.node.byte_range());
+                var.type_name = Some(text.to_string());
+            }
+            &_ => {}
+        }
+    }
+    if var.name.is_empty() {
+        return None;
+    }
+
+    Some(var)
+}
+
+pub fn get_call(captures: &[QueryCapture], query: &Query, code: &str) -> Option<FunctionCallInfo> {
+    let mut var = FunctionCallInfo {
+        name: "".to_string(),
+        range: Range {
+            start_byte: 0,
+            end_byte: 0,
+            start_point: Default::default(),
+            end_point: Default::default(),
+        },
+    };
+    for capture in captures {
+        let capture_name = &query.capture_names()[capture.index as usize];
+        match capture_name.as_str() {
+            "call" => {
+                var.range = capture.node.range();
+            }
+            "call_name" => {
+                let text = code.slice(capture.node.byte_range());
+                var.name = text.to_string();
+            }
+            &_ => {}
+        }
+    }
+    if var.name.is_empty() {
+        return None;
+    }
+    Some(var)
+}
+
+pub fn get_static(captures: &[QueryCapture], query: &Query, code: &str) -> Option<StaticInfo> {
+    let text = code.slice(captures[0].node.byte_range());
+    for capture in captures {
+        let capture_name = &query.capture_names()[capture.index as usize];
+        return match capture_name.as_str() {
+            "comment" => {
+                Some(StaticInfo {
+                    static_type: StaticType::Comment,
+                    range: capture.node.range(),
+                })
+            }
+            "string_literal" => {
+                Some(StaticInfo {
+                    static_type: StaticType::Literal,
+                    range: capture.node.range(),
+                })
+            }
+            &_ => {
+                None
+            }
+        }
+    }
+    None
+}
+
+const CPP_PARSER_QUERY_FIND_VARIABLES: &str = r#"((declaration type: [
+(template_type name: (type_identifier) @variable_type)
+(primitive_type) @variable_type
+] 
+(init_declarator declarator: [
+(identifier) @variable_name
+(array_declarator (identifier) @variable_name)
+])) @variable)"#;
+
+const CPP_PARSER_QUERY_FIND_CALLS: &str = r#"((call_expression function: [
+(field_expression (field_identifier) @call_name)
+(identifier) @call_name
+]) @call)"#;
+
+const CPP_PARSER_QUERY_FIND_STATICS: &str = r#"(
+([
+(comment) @comment
+(string_literal) @string_literal
+])
+)"#;
+
+lazy_static! {
+    static ref CPP_PARSER_QUERY_FIND_ALL: String = format!("{}\n{}\n{}", 
+        CPP_PARSER_QUERY_FIND_VARIABLES, CPP_PARSER_QUERY_FIND_CALLS, CPP_PARSER_QUERY_FIND_STATICS);
+}
+
+
+pub fn get_vars_function_calls_static_data(parser: &mut Parser, code: &str) -> (Vec<VariableInfo>, Vec<FunctionCallInfo>, Vec<StaticInfo>) {
+    let tree: Tree = parser.parse(code, None).unwrap();
+    let mut vars: Vec<VariableInfo> = Default::default();
+    let mut calls: Vec<FunctionCallInfo> = Default::default();
+    let mut statics: Vec<StaticInfo> = Default::default();
+    let mut qcursor = tree_sitter::QueryCursor::new();
+    let query = Query::new(tree_sitter_cpp::language(), &**CPP_PARSER_QUERY_FIND_ALL).unwrap();
+    let mut matches = qcursor.matches(&query, tree.root_node(), code.as_bytes());
+    for match_ in matches {
+        match match_.pattern_index {
+            0 => {
+                match get_variable(match_.captures, &query, code) {
+                    None => {}
+                    Some(var) => { vars.push(var) }
+                }
+            }
+            1 => {
+                match get_call(match_.captures, &query, code) {
+                    None => {}
+                    Some(var) => { calls.push(var) }
+                }
+            }
+            2 => {
+                match get_static(match_.captures, &query, code) {
+                    None => {}
+                    Some(var) => { statics.push(var) }
+                }
+            }
+            _ => {}
+        }
+        
+    }
+    (vars, calls, statics)
+}
+
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::lsp::treesitter::ast_config::cpp_config::get_indexes;
+    use ropey::Rope;
+    use tree_sitter::Point;
+    use crate::lsp::treesitter::ast_config::cpp_config::{get_indexes, get_vars_function_calls_static_data};
 
     const TEST_CODE: &str =
         r#"
@@ -269,7 +443,10 @@ mod tests {
 using namespace std;
  
 int b = 0;
- 
+
+// comment
+String cat = "cat";
+
 struct asd {};
 namespace internal {
 int a = 0;
@@ -306,6 +483,7 @@ int main()
     int arr[5] = { 1, 2, 3, 4, 5 };
     Array<int> a(arr, 5);
     a.print();
+    print();
     return 0;
 }
 "#;
@@ -314,6 +492,7 @@ int main()
     fn test_query_cpp_function() {
         let mut parser = tree_sitter::Parser::new();
         parser.set_language(tree_sitter_cpp::language()).unwrap();
+        let zxc = get_vars_function_calls_static_data(&mut parser, TEST_CODE);
         let indexes = get_indexes(&mut parser, TEST_CODE, PathBuf::from("test.cpp"));
         assert_eq!(indexes.len(), 1);
         // assert_eq!(indexes.get("function").unwrap().name, "foo");
