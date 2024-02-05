8c63425dca4f92321a32aa2c3c1d3f7e8b4df056
JegernOUTT <sergey.vakhreev@gmail.com>
2024-02-05 17:40:39 +1030

    add extra fields for usage ast structs

--- src/ast/treesitter/structs.rs
+++ src/ast/treesitter/structs.rs
@@ -1,5 +1,9 @@
-use tree_sitter::Range;
+use tree_sitter::{Point, Range};
 
+pub trait UsageSymbolInfo {
+    fn dump_path(&self) -> String;
+    fn distance_to_cursor(&self, cursor: Point) -> usize;
+}
 
 pub struct VariableInfo {
     pub name: String,
@@ -7,9 +11,34 @@ pub struct VariableInfo {
     pub type_name: Option<String>,
 }
 
+impl UsageSymbolInfo for VariableInfo {
+    fn dump_path(&self) -> String {
+        match self.type_name.as_ref() {
+            Some(t) => format!("{}::{}", self.name, t),
+            None => self.name.clone(),
+        }
+    }
+    fn distance_to_cursor(&self, cursor: Point) -> usize {
+        cursor.row.abs_diff(self.range.start_point.row)
+    }
+}
+
 pub struct FunctionCallInfo {
     pub name: String,
     pub range: Range,
+    pub caller_type_name: Option<String>,
+}
+
+impl UsageSymbolInfo for FunctionCallInfo {
+    fn dump_path(&self) -> String {
+        match self.caller_type_name.as_ref() {
+            Some(t) => format!("{}::{}", self.name, t),
+            None => self.name.clone(),
+        }
+    }
+    fn distance_to_cursor(&self, cursor: Point) -> usize {
+        cursor.row.abs_diff(self.range.start_point.row)
+    }
 }
 
 pub enum StaticType {
@@ -18,7 +47,17 @@ pub enum StaticType {
 }
 
 pub struct StaticInfo {
+    pub data: String,
     pub static_type: StaticType,
     pub range: Range,
+}
+
+impl UsageSymbolInfo for StaticInfo {
+    fn dump_path(&self) -> String {
+        format!("{}", self.data)
+    }
+    fn distance_to_cursor(&self, cursor: Point) -> usize {
+        cursor.row.abs_diff(self.range.start_point.row)
+    }
 }
 
