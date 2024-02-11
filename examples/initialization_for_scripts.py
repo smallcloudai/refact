f8325a8ba061f0bc7b1c6270dd976802ac946165
Oleg Klimov <omgtech@gmail.com>
2024-02-11 21:40:58 +0100

    working initialization_for_scripts.py

--- examples/initialization_for_scripts.py
+++ examples/initialization_for_scripts.py
@@ -1,17 +1,19 @@
-import os, subprocess
+import os, json, subprocess, requests
 
 cmdline = [
     "target/debug/refact-lsp",
     "--address-url", "Refact",
     "--api-key", os.environ["SMALLCLOUD_API_KEY"],
     "--http-port", "8001",
-    "--files-jsonl-path", "hurray.jsonl",
+    "--workspace-folder", os.path.dirname(os.path.dirname(__file__)),
     "--ast",
+    # "--logs-stderr"
     ]
 print(" ".join(cmdline))
 
 def start_rust():
     rust = subprocess.Popen(cmdline, stderr=subprocess.PIPE)
+    ready = [0, 0]
     while True:
         if rust.poll() is not None:
             print("Oops, something went wrong, exit code: %i" % rust.returncode)
@@ -19,11 +21,31 @@ def start_rust():
         line = rust.stderr.readline().decode("utf-8").strip()
         print("RUST", line)
         if line == "AST COMPLETED":
+            ready[0] = 1
+        if line == "CAPS":
+            ready[1] = 1
+        if all(ready):
             break
     return rust
 
 
 rust = start_rust()
-print("DONE")
+
+symbol = "pretty_print_wrapper"
+response = requests.post(
+    "http://127.0.0.1:8001/v1/ast-query-search",
+    json={
+        "query": symbol,
+        "top_n": 3,
+    },
+    headers={
+        "Content-Type": "application/json",
+    },
+    timeout=60,
+)
+j = response.json()
+# print(response.json())
 rust.terminate()
 rust.wait()
+print("DONE")
+print(json.dumps(j, indent=4))
