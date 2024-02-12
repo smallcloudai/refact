edfd0b1e6b1cc6d79c8cc2e0f785f962ef2c4443
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-02-12 14:58:31 +0100

    ui(chat): increase textarea size with user input

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useImperativeHandle, useRef } from "react";
 import { TextArea as RadixTextArea } from "@radix-ui/themes";
 import classNames from "classnames";
 import styles from "./TextArea.module.css";
@@ -10,11 +10,23 @@ export type TextAreaProps = React.ComponentProps<typeof RadixTextArea> & {
 
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   (props, ref) => {
+    const innerRef = useRef<HTMLTextAreaElement>(null);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    useImperativeHandle(ref, () => innerRef.current!, []);
+
+    useEffect(() => {
+      if (innerRef.current) {
+        innerRef.current.style.height = "1px";
+        innerRef.current.style.height =
+          2 + innerRef.current.scrollHeight + "px";
+      }
+    }, [innerRef.current?.value]);
+
     return (
       <RadixTextArea
         {...props}
         className={classNames(styles.textarea, props.className)}
-        ref={ref}
+        ref={innerRef}
       />
     );
   },
