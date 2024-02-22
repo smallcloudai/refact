5459b1612d17ac119eee388136531599a07c9138
Kirill Starkov <starkov.kirill123@gmail.com>
2024-02-22 21:44:53 +0700

    Fix tests (#94)

--- src/ast/treesitter/parsers/tests.rs
+++ src/ast/treesitter/parsers/tests.rs
@@ -14,8 +14,14 @@ pub(crate) fn test_query_function(mut parser: Box<dyn LanguageParser>,
                                   ref_usages: Vec<Box<dyn UsageSymbolInfo>>) {
     let indexes = parser.parse_declarations(code, &path).unwrap();
     let usages = parser.parse_usages(code).unwrap();
-
-    assert_eq!(indexes, ref_indexes);
+    
+    indexes.iter().for_each(|(key, index)| {
+        assert_eq!(index, ref_indexes.get(key).unwrap());
+    });
+    ref_indexes.iter().for_each(|(key, index)| {
+        assert_eq!(index, indexes.get(key).unwrap());
+    });
+    
     usages.iter().for_each(|usage| {
         assert!(ref_usages.contains(usage));
     });
