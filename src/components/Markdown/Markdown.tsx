c249d666651414c11f3a6c60cf289caf4251ec3e
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-02-25 11:51:08 +0100

    ui(markdown): add text variants from radix

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -5,6 +5,16 @@ import classNames from "classnames";
 import "./highlightjs.css";
 import styles from "./Markdown.module.css";
 import { MarkdownCodeBlock, type MarkdownControls } from "./CodeBlock";
+import {
+  Text,
+  Heading,
+  Blockquote,
+  Em,
+  Kbd,
+  Link,
+  Quote,
+  Strong,
+} from "@radix-ui/themes";
 
 export type MarkdownProps = Pick<
   React.ComponentProps<typeof ReactMarkdown>,
@@ -35,6 +45,51 @@ export const Markdown: React.FC<MarkdownProps> = ({ children, ...rest }) => {
         },
         code(props) {
           return <MarkdownCodeBlock {...props} {...rest} />;
+        },
+        p({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Text as="p" {...props} />;
+        },
+        h1({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h1" {...props} />;
+        },
+        h2({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h2" {...props} />;
+        },
+        h3({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h3" {...props} />;
+        },
+        h4({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h4" {...props} />;
+        },
+        h5({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h5" {...props} />;
+        },
+        h6({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Heading as="h6" {...props} />;
+        },
+        blockquote({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Blockquote {...props} />;
+        },
+        em({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Em {...props} />;
+        },
+        kbd({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Kbd {...props} />;
+        },
+        a({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Link {...props} />;
+        },
+        q({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Quote {...props} />;
+        },
+        strong({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Strong {...props} />;
+        },
+        b({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Text {...props} weight="bold" />;
+        },
+        i({ color: _color, ref: _ref, node: _node, ...props }) {
+          return <Em {...props} />;
         },
       }}
     >
