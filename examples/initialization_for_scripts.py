2f8c39fbb93e938b6991b34e3e5db35c3919d938
Oleg Klimov <omgtech@gmail.com>
2024-02-26 21:24:12 +0100

    scripts

--- examples/initialization_for_scripts.py
+++ examples/initialization_for_scripts.py
@@ -1,17 +1,7 @@
 import os, json, subprocess, requests
 
-cmdline = [
-    "target/debug/refact-lsp",
-    "--address-url", "Refact",
-    "--api-key", os.environ["SMALLCLOUD_API_KEY"],
-    "--http-port", "8001",
-    "--workspace-folder", os.path.dirname(os.path.dirname(__file__)),
-    "--ast",
-    # "--logs-stderr"
-    ]
-print(" ".join(cmdline))
 
-def start_rust():
+def start_rust(cmdline):
     rust = subprocess.Popen(cmdline, stderr=subprocess.PIPE)
     ready = [0, 0]
     while True:
@@ -29,23 +19,39 @@ def start_rust():
     return rust
 
 
-rust = start_rust()
+def test_ast_search():
+    cmdline = [
+        "target/debug/refact-lsp",
+        "--address-url", "Refact",
+        "--api-key", os.environ["SMALLCLOUD_API_KEY"],
+        "--http-port", "8001",
+        "--workspace-folder", os.path.dirname(os.path.dirname(__file__)),
+        "--ast",
+        # "--logs-stderr"
+        ]
+    print(" ".join(cmdline))
+    rust = start_rust(cmdline)
+    j = None
+    try:
+        response = requests.post(
+            "http://127.0.0.1:8001/v1/ast-references-query-search",
+            json={
+                "query": "pretty_print_wrapper",
+                "top_n": 3,
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+            timeout=60,
+        )
+        print(response.text)
+        j = response.json()
+    finally:
+        rust.terminate()
+        rust.wait()
+        print("DONE")
+    print(json.dumps(j, indent=4))
 
-symbol = "pretty_print_wrapper"
-response = requests.post(
-    "http://127.0.0.1:8001/v1/ast-query-search",
-    json={
-        "query": symbol,
-        "top_n": 3,
-    },
-    headers={
-        "Content-Type": "application/json",
-    },
-    timeout=60,
-)
-j = response.json()
-# print(response.json())
-rust.terminate()
-rust.wait()
-print("DONE")
-print(json.dumps(j, indent=4))
+
+if __name__ == "__main__":
+    test_ast_search()
