33ad402957ea6c256fb65fc2a1017c4260dec0bd
Oleg Klimov <omgtech@gmail.com>
2024-02-27 20:26:48 +0100

    toolbox 1

--- src/toolbox/toolbox_compiled_in.rs
+++ src/toolbox/toolbox_compiled_in.rs
@@ -1,27 +1,30 @@
-pub const COMPILED_IN_TOOLBOX_YAML : &str = r#"
-# Toolbox will merge this compiled-in config and the user config.
+pub const COMPILED_IN_CUSTOMIZATION_YAML : &str = r#"
+# Customization will merge this compiled-in config and the user config.
 #
 # There are magic keys:
 #    %ARGS%
-#       expanded to arguments of a command, like this /command <ARGS>
+#       expanded to arguments of a toolbox command, like this /command <ARGS>
 #    %CODE_SELECTION%
 #       plain text code that user has selected
-#    %CODE_AROUND_CURSOR_JSON%
-#       Json that has the current file, possibly cut (if it's large)
-#       The json format is suitable to attach to a role="context_file" message
-#    %CODE_INSERT_HERE_JSON%
-#       Json that has the current file, cursor position marked with "|INSERT-HERE|" in the text
+#    %CURRENT_FILE_PATH_COLON_CURSOR%
+#       expanded to file.ext:42
+#       useful to form a "@file xxx" command that will insert the file text around the cursor
 #
+# You can also use top-level keys to reduce copy-paste, like you see there with DEFAULT_PROMPT.
 
-SYSTEM_PROMPT: "You are a programming assistant. Use backquotes for code blocks, give links to documentation at the end of the response."
+DEFAULT_PROMPT: "You are a programming assistant. Use backquotes for code blocks, give links to documentation at the end of the response."
 
-commands:
+system_prompts:
+  default:
+    text: "%DEFAULT_PROMPT%"
+
+toolbox_commands:
   shorter:
     selection_needed: [1, 50]
     description: "Make code shorter"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nMake this specific code block shorter:\n\n```\n%CODE_SELECTION%```\n"
   bugs:
@@ -29,15 +32,15 @@ commands:
     description: "Find and fix bugs"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nFind and fix bugs in this specific code block:\n\n```\n%CODE_SELECTION%```\n"
-  "improve":
+  improve:
     selection_needed: [1, 50]
     description: "Rewrite this specific code block of code to improve it"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nRewrite this specific code block of code to improve it:\n\n```\n%CODE_SELECTION%```\n"
   comment:
@@ -45,7 +48,7 @@ commands:
     description: "Comment each line"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nComment each line of this specific code block:\n\n```\n%CODE_SELECTION%```\n"
   typehints:
@@ -53,7 +56,7 @@ commands:
     description: "Add type hints"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nAdd type hints to this specific code block:\n\n```\n%CODE_SELECTION%```\n"
   naming:
@@ -61,7 +64,7 @@ commands:
     description: "Improve variable names"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nImprove variable names in this specific code block:\n\n```\n%CODE_SELECTION%```\n"
   explain:
@@ -69,7 +72,7 @@ commands:
     description: "Explain code"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nExplain this specific code block:\n\n```\n%CODE_SELECTION%```\n"
   summarize:
@@ -77,7 +80,7 @@ commands:
     description: "Summarize code in 1 paragraph"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nSummarize this specific code block in 1 paragraph:\n\n```\n%CODE_SELECTION%```\n"
   typos:
@@ -85,7 +88,34 @@ commands:
     description: "Fix typos"
     messages:
     - role: "system"
-      content: "%SYSTEM_PROMPT%"
+      content: "%DEFAULT_PROMPT%"
     - role: "user"
       content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nRewrite this specific code block to fix typos, especially inside strings and comments:\n\n```\n%CODE_SELECTION%```\n"
+"#;
+
+
+pub const COMPILED_IN_INITIAL_USER_YAML : &str = r#"
+# Customization will override the default config you can see at the bottom of this file, in the comments.
+# You can find the default config by searching for COMPILED_IN_CUSTOMIZATION_YAML in `refact-lsp` repo.
+# If your custom command is good, you can post a PR changing the default for everybody.
+#
+# It's easy, just make your commands by analogy and experiment!
+
+system_prompts:
+  write_pseudo_code:
+    description: "User-defined: write pseudo code"
+    text: "You are a programming assistant. Use backquotes for code blocks, but write pseudo code in comments instead of code. Replace real code offered by the user with pseudo code when you rewrite it."
+  insert_jokes:
+    description: "User-defined: write funny comments"
+    text: "You are a programming assistant. Use backquotes for code blocks, but insert into comments inside code blocks funny remarks, a joke inspired by the code or play on words. For example ```\n// Hocus, pocus\ngetTheFocus();\n```."
+
+toolbox_commands:
+   user0:
+    description: "User-defined: translate to horrible code"
+    selection_needed: [1, 50]
+    messages:
+    - role: "system"
+      content: "%DEFAULT_PROMPT%"
+    - role: "user"
+      content: "@file %CURRENT_FILE_PATH_COLON_CURSOR%\nRewrite this specific code block into a very inefficient and cryptic one, but still correct:\n\n```\n%CODE_SELECTION%```\n
 "#;
--- src/toolbox/toolbox_config.rs
+++ src/toolbox/toolbox_config.rs
@@ -2,15 +2,20 @@ use serde_yaml::Value;
 use serde_yaml;
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
-
 use crate::call_validation::ChatMessage;
 
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ToolboxConfig {
-    // #[serde(alias = "SYSTEM_PROMPT")]   
-    // pub system_prompt: String,
-    pub commands: HashMap<String, ToolboxCommand>,
+    pub system_prompts: HashMap<String, SystemPrompt>,
+    pub toolbox_commands: HashMap<String, ToolboxCommand>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SystemPrompt {
+    #[serde(default)]
+    pub description: String,
+    pub text: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -25,27 +30,40 @@ pub struct ToolboxCommand {
     pub insert_at_cursor: bool,
 }
 
-pub fn load_and_mix_with_users_config() -> ToolboxConfig {
-    let unstructured: serde_yaml::Value = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_TOOLBOX_YAML).unwrap();
-    let mut tconfig: ToolboxConfig = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_TOOLBOX_YAML).unwrap();
-    let mut variables = HashMap::<String, String>::new();
-    if let Some(mapping) = unstructured.as_mapping() {
+
+fn _extract_mapping_values(mapping: &serde_yaml::Value, variables: &mut HashMap<String, String>) {
+    if let Some(mapping) = mapping.as_mapping() {
         for (k, v) in mapping {
             if let (Value::String(key), Value::String(value)) = (k, v) {
                 variables.insert(key.clone(), value.clone());
             }
         }
     }
-    for (_, command) in tconfig.commands.iter_mut() {
+}
+
+fn _replace_variables_in_messages(config: &mut ToolboxConfig, variables: &HashMap<String, String>) {
+    for (_, command) in config.toolbox_commands.iter_mut() {
         for (_i, msg) in command.messages.iter_mut().enumerate() {
             let mut tmp = msg.content.clone();
             for (vname, vtext) in variables.iter() {
-                tmp = tmp.replace(format!("%{}%", vname).as_str(), vtext.as_str());
+                tmp = tmp.replace(&format!("%{}%", vname), vtext);
             }
             msg.content = tmp;
         }
     }
-    tconfig
+}
+
+pub fn load_and_mix_with_users_config() -> ToolboxConfig {
+    let default_unstructured: serde_yaml::Value = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_CUSTOMIZATION_YAML).unwrap();
+    let user_unstructured: serde_yaml::Value = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_INITIAL_USER_YAML).unwrap();
+    let mut variables = HashMap::<String, String>::new();
+    _extract_mapping_values(&default_unstructured, &mut variables);
+    _extract_mapping_values(&user_unstructured, &mut variables);
+    let mut work_config: ToolboxConfig = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_CUSTOMIZATION_YAML).unwrap();
+    let mut user_config: ToolboxConfig = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_INITIAL_USER_YAML).unwrap();
+    _replace_variables_in_messages(&mut work_config, &variables);
+    _replace_variables_in_messages(&mut user_config, &variables);
+    work_config
 }
 
 #[cfg(test)]
@@ -54,11 +72,11 @@ mod tests {
 
     #[test]
     fn is_compiled_in_toolbox_valid_toml() {
-        let _yaml: serde_yaml::Value = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_TOOLBOX_YAML).unwrap();
+        let _yaml: serde_yaml::Value = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_CUSTOMIZATION_YAML).unwrap();
     }
 
     #[test]
     fn does_compiled_in_toolbox_fit_structs() {
-        let _yaml: ToolboxConfig = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_TOOLBOX_YAML).unwrap();
+        let _yaml: ToolboxConfig = serde_yaml::from_str(crate::toolbox::toolbox_compiled_in::COMPILED_IN_CUSTOMIZATION_YAML).unwrap();
     }
 }
