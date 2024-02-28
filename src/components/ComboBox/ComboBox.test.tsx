93a248559131e358f11b68da31e00e98f8b1a385
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-02-28 15:05:34 +0100

    test(combobox): fix flacky test

--- src/components/ComboBox/ComboBox.test.tsx
+++ src/components/ComboBox/ComboBox.test.tsx
@@ -174,7 +174,8 @@ describe("ComboBox", () => {
       <App requestCommandsCompletion={executableSpy} />,
     );
     const textarea = app.getByRole("combobox");
-    await user.type(textarea, "@{ArrowDown}{Tab}");
+    await user.type(textarea, "@");
+    await user.keyboard("{ArrowDown}{Enter}");
     expect(executableSpy).toHaveBeenLastCalledWith("@workspace ", 11, null);
   });
 
