cf0dd312c5f3512eadf7159178e33a350aabe56f
Valerii <ponyparser@gmail.com>
2024-02-28 10:41:26 +0000

    added debug handler (#99)
    * update cicl 3 (#107)
    
    * added debug handler
    
    temp
    
    added mock data
    
    * changes
    
    * reset
    
    ---------
    
    Co-authored-by: Kirill Starkov <starkov.kirill123@gmail.com>

--- src/http/routers/v1.rs
+++ src/http/routers/v1.rs
@@ -29,6 +29,7 @@ use crate::http::utils::telemetry_wrapper;
 use crate::http::routers::v1::dashboard::get_dashboard_plots;
 use crate::http::routers::v1::vecdb::{handle_v1_vecdb_search, handle_v1_vecdb_status, handle_v1_vecdb_caps};
 use crate::http::routers::v1::at_commands::{handle_v1_command_completion, handle_v1_command_preview};
+use crate::http::routers::v1::debug_handler::debug_fim_data;
 
 pub mod code_completion;
 pub mod chat;
@@ -42,6 +43,7 @@ pub mod toolbox;
 pub mod vecdb;
 mod at_commands;
 mod ast;
+mod debug_handler;
 
 pub fn make_v1_router() -> Router {
     Router::new()
@@ -63,6 +65,8 @@ pub fn make_v1_router() -> Router {
         .route("/lsp-did-changed", telemetry_post!(handle_v1_lsp_did_change))
 
         .route("/get-dashboard-plots", telemetry_get!(get_dashboard_plots))
+
+        .route("/debug-fim-data", telemetry_post!(debug_fim_data))
         .route("/ast-declarations-cursor-search", telemetry_post!(handle_v1_ast_declarations_cursor_search))
         .route("/ast-declarations-query-search", telemetry_post!(handle_v1_ast_declarations_query_search))
         .route("/ast-references-cursor-search", telemetry_post!(handle_v1_ast_references_cursor_search))
