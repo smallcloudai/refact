9cb59b003a09800b152e731ffd7269496623dbcd
Valeryi <ponyparser@gmail.com>
2024-03-04 13:07:04 +0000

    init

--- refact_webgui/webgui/selfhost_queue.py
+++ refact_webgui/webgui/selfhost_queue.py
@@ -54,9 +54,9 @@ class InferenceQueue:
                 self._models_available.append(model)
             self._models_available_ts = time.time()
             if j.get("openai_api_enable", False):
-                self._models_available.append('gpt-3.5-turbo')
-                self._models_available.append('gpt-3.5-turbo-1106')
+                self._models_available.append('gpt-3.5-turbo-0125')
                 self._models_available.append('gpt-4')
+                self._models_available.append('gpt-4-0125-preview')
             if j.get("anthropic_api_enable", False):
                 self._models_available.append('claude-instant-1.2')
                 self._models_available.append('claude-2.1')
