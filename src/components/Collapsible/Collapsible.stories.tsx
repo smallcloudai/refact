de2963ed3013ea7d670df868dd5b9433464b06c5
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-03-04 14:18:32 +0100

    feat(controls): add Collapsible component

---
+++ src/components/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Collapsible } from ".";
+import { Text } from "../Text";
+import { Flex } from "@radix-ui/themes";
+
+const meta = {
+  title: "Collapsible",
+  component: Collapsible,
+} satisfies Meta<typeof Collapsible>;
+
+export default meta;
+
+export const Default: StoryObj<typeof Collapsible> = {
+  args: {
+    children: (
+      <Flex direction="column">
+        <Text>Item 1</Text>
+        <Text>Item 2</Text>
+        <Text>Item 3</Text>
+      </Flex>
+    ),
+  },
+};
---
+++ src/components/Collapsible/Collapsible.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import * as RadixCollapsible from "@radix-ui/react-collapsible";
+import { Cross2Icon, RowSpacingIcon } from "@radix-ui/react-icons";
+import { Flex, Button } from "@radix-ui/themes";
+
+export type CollapsibleProps = Pick<
+  RadixCollapsible.CollapsibleProps,
+  "disabled" | "className"
+> &
+  React.PropsWithChildren<{
+    className?: string;
+    disabled?: boolean;
+  }>;
+
+export const Collapsible: React.FC<CollapsibleProps> = ({
+  children,
+  ...props
+}) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <RadixCollapsible.Root
+      {...props}
+      className={props.className}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <Flex align="center" justify="between">
+        <RadixCollapsible.Trigger asChild>
+          <Button variant="ghost">
+            Advanced: {open ? <Cross2Icon /> : <RowSpacingIcon />}{" "}
+          </Button>
+        </RadixCollapsible.Trigger>
+      </Flex>
+
+      <RadixCollapsible.Content>{children}</RadixCollapsible.Content>
+    </RadixCollapsible.Root>
+  );
+};
