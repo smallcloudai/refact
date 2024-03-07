02b25419f5a1d97a72f1d203816153bae641d465
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-03-07 14:59:30 +0100

    test(chat form): update "symbols-at" spec

--- src/components/ChatForm/ChatForm.test.tsx
+++ src/components/ChatForm/ChatForm.test.tsx
@@ -115,7 +115,7 @@ describe("ChatForm", () => {
     await user.type(textarea, "foo");
     await user.keyboard("{Enter}");
     expect(fakeOnSubmit).toHaveBeenCalledWith(
-      `foo\n@symbols-at ${activeFile.path}:${activeFile.line1}-${activeFile.line2}\n`,
+      `foo\n@symbols-at ${activeFile.path}:${activeFile.cursor}\n`,
     );
   });
 
