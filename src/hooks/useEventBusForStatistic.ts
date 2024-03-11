82929f912326822ff4dc12f933b800f4bf397eb2
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-03-11 18:01:58 +0100

    refactor(statistics): remove cache

--- src/hooks/useEventBusForStatistic.ts
+++ src/hooks/useEventBusForStatistic.ts
@@ -10,14 +10,10 @@ import {
   isSetLoadingStatisticData,
   isReceiveFillInTheMiddleDataError,
   isSetStatisticData,
-  isSetStatisticHash,
-  ReadyMessageFromStatistic,
 } from "../events";
 import { usePostMessage } from "./usePostMessage";
 import { useCallback, useEffect, useReducer } from "react";
 import { ChatContextFile, StatisticData } from "../services/refact";
-import { useEffectOnce } from "./useEffectOnce";
-import { useInterval } from "usehooks-ts";
 
 export type StatisticState = {
   statisticData: StatisticData | null;
@@ -27,12 +23,10 @@ export type StatisticState = {
     files: ChatContextFile[];
     error: string;
   };
-  hash: string;
 };
 
 function createInitialState(): StatisticState {
   return {
-    hash: "",
     statisticData: null,
     isLoading: true,
     error: "",
@@ -104,13 +98,6 @@ function reducer(
     };
   }
 
-  if (isSetStatisticHash(action)) {
-    return {
-      ...state,
-      hash: action.payload.data,
-    };
-  }
-
   return state;
 }
 
@@ -125,22 +112,13 @@ export const useEventBusForStatistic = () => {
   };
 
   const fetchData = useCallback(() => {
-    if (!state.hash) return;
-
     dispatch({
       type: EVENT_NAMES_TO_STATISTIC.REQUEST_STATISTIC_DATA,
     });
     postMessage({
       type: EVENT_NAMES_TO_STATISTIC.REQUEST_STATISTIC_DATA,
     });
-  }, [postMessage, state.hash]);
-
-  useEffectOnce(() => {
-    const message: ReadyMessageFromStatistic = {
-      type: EVENT_NAMES_FROM_STATISTIC.READY,
-    };
-    postMessage(message);
-  });
+  }, [postMessage]);
 
   useEffect(() => {
     const listener = (event: MessageEvent) => {
@@ -153,8 +131,6 @@ export const useEventBusForStatistic = () => {
           type: EVENT_NAMES_TO_STATISTIC.SET_STATISTIC_DATA,
           payload: parsedStatisticData,
         });
-
-        cache.saveData(parsedStatisticData, state.hash);
 
         dispatch({
           type: EVENT_NAMES_TO_STATISTIC.RECEIVE_STATISTIC_DATA_ERROR,
@@ -175,81 +151,12 @@ export const useEventBusForStatistic = () => {
     return () => {
       window.removeEventListener("message", listener);
     };
-  }, [fetchData, postMessage, state.hash]);
+  }, [fetchData, postMessage]);
 
-  const oneHour = 1000 * 60 * 60;
-
-  useEffect(() => {
-    if (!state.hash) return;
-    const cachedStatisticData = cache.getData<StatisticData>(
-      oneHour,
-      state.hash,
-    );
-
-    if (cachedStatisticData) {
-      dispatch({
-        type: EVENT_NAMES_TO_STATISTIC.SET_STATISTIC_DATA,
-        payload: cachedStatisticData,
-      });
-    } else {
-      fetchData();
-    }
-  }, [fetchData, state.hash, oneHour]);
-
-  useInterval(fetchData, oneHour);
+  useEffect(fetchData, [fetchData]);
 
   return {
     backFromStatistic,
     state,
   };
-};
-
-type CacheData<T> = {
-  created_at: number;
-  hash: string;
-  data: T;
-};
-
-function isCacheData<T>(data: unknown): data is CacheData<T> {
-  return (
-    data !== null &&
-    typeof data === "object" &&
-    "created_at" in data &&
-    "data" in data &&
-    "hash" in data
-  );
-}
-
-const cache = {
-  getData<T>(timeLimit: number, hash: string): T | null {
-    const str = localStorage.getItem("statisticData");
-    if (!str) return null;
-
-    try {
-      const data: unknown = JSON.parse(str);
-      if (!isCacheData<T>(data)) return null;
-
-      const now = Date.now();
-      const limit = now - timeLimit;
-
-      if (data.created_at < limit || data.hash !== hash) {
-        localStorage.clear();
-        return null;
-      }
-
-      return data.data;
-    } catch (e) {
-      localStorage.clear();
-      return null;
-    }
-  },
-  saveData<T>(data: T, hash = "") {
-    const payload: CacheData<T> = {
-      created_at: Date.now(),
-      data,
-      hash,
-    };
-
-    localStorage.setItem("statisticData", JSON.stringify(payload));
-  },
 };
