9d1faf596ce620d611a3b5048f3967305d27a10e
Oleg Klimov <omgtech@gmail.com>
2024-03-15 11:02:55 +0100

    fixes

--- refact_utils/finetune/train_defaults.py
+++ refact_utils/finetune/train_defaults.py
@@ -1,16 +1,14 @@
 finetune_train_defaults = {
-    "limit_time_seconds": 48 * 60 * 60,
+    "trainable_embeddings": False,
+    "low_gpu_mem_mode": True,
     "lr": 30e-5,
     "batch_size": 128,
     "warmup_num_steps": 20,
     "weight_decay": 0.1,
-    "use_heuristics": True,
-    # These settings will be used if it's use_heuristics=False
-    "train_steps": 250,
-    "lr_decay_steps": 250,
     "lora_r": 16,
     "lora_alpha": 32,
     "lora_dropout": 0.01,
-    "trainable_embeddings": False,
-    "low_gpu_mem_mode": True
+    # if train_steps==0 then set_train_steps() and  set_lr_decay_steps() is automatic
+    "train_steps": 0,
+    "lr_decay_steps": 0,
 }
