36b82fe241c857d9bb973a7e5bc1a9d90406a268
JegernOUTT <sergey.vakhreev@gmail.com>
2024-03-21 19:12:37 +1030

    change random seed each epoch

--- refact_data_pipeline/finetune_datasource.py
+++ refact_data_pipeline/finetune_datasource.py
@@ -1,6 +1,5 @@
 import os
 import random
-from pathlib import Path
 from typing import Iterable, Dict, Any, List
 
 import jsonlines
@@ -9,26 +8,28 @@ import torch.utils.data
 
 from refact_data_pipeline import DatasetOpts
 from refact_data_pipeline import pipeline_pieces as pp
+from refact_data_pipeline.datadef import PipelineNode
 from refact_data_pipeline.filters_fim_v2 import FIMv2, FIMv2CodeLlama
-from refact_utils.scripts import env
 
 __all__ = [
     'RefactDataset', 'RefactPlainCodeDataset', 'RefactFIMCodeDataset'
 ]
 
 
-class ReadFileByFile:
+class ReadFileByFile(PipelineNode):
     def __init__(
             self,
             basedir: str,
             inner_filter: Iterable[Dict[str, Any]],
             dataopts: DatasetOpts,
     ):
+        super().__init__(dataopts)
         assert isinstance(basedir, str), f"basedir must be a string, not {type(basedir).__name__}"
         self.basedir = basedir
         self.inner_filter = inner_filter
         self.dataopts = dataopts
         self.quit_on_epoch = dataopts.get("quit_on_epoch", 0)
+        self.epoch_callback = lambda x: x
 
     @staticmethod
     def _cut_zip_name(j):
@@ -56,16 +57,21 @@ class ReadFileByFile:
                 }
                 file_num += 1
             epoch += 1
+            self.epoch_callback(epoch)
             if epoch == self.quit_on_epoch:
                 break
 
+    def set_epoch_callback(self, callback):
+        self.epoch_callback = callback
 
-class CodeToPrefixCompletion:
+
+class CodeToPrefixCompletion(PipelineNode):
     def __init__(
             self,
             inner_filter: Iterable[Dict[str, Any]],
             dataopts: DatasetOpts,
     ):
+        super().__init__(dataopts)
         self.inner_filter = inner_filter
         self.dataopts = dataopts
 
@@ -84,13 +90,18 @@ class RefactDataset(torch.utils.data.IterableDataset):
             basedir: str,
             files: List[Dict[str, Any]],
             dataset_options: str,
-            encoding: 'Encoding'
+            encoding: 'Encoding',
+            change_seed_every_epoch: bool = True
     ):
         self.basedir = basedir
         self._files = files
         self._ds_options = DatasetOpts(dataset_options)
         self._encoding = encoding
+        self._change_seed_every_epoch = change_seed_every_epoch
         self._ds_options.set_encoding(self._encoding)
+
+    def get_option(self, name, default):
+        return self._ds_options.get(name, default)
 
     @staticmethod
     def from_a_single_file(
@@ -117,6 +128,16 @@ class RefactDataset(torch.utils.data.IterableDataset):
     def files_len(self) -> int:
         return len(self._files)
 
+    def set_epoch_callback(self, callback):
+        assert len(self._pipeline) > 0
+        file_reader = self._pipeline[0]
+        assert type(file_reader) is ReadFileByFile
+        file_reader.set_epoch_callback(callback)
+
+    def set_random_state(self, seed):
+        for node in self._pipeline:
+            node.set_random_state(seed)
+
     def _get_files_by_worker(self) -> List[Dict[str, Any]]:
         files = self._files
         random.Random(self._ds_options.get("seed", 42)).shuffle(files)
@@ -130,34 +151,42 @@ class RefactDataset(torch.utils.data.IterableDataset):
     def _build_pipeline(self, files: List[Dict[str, Any]]):
         raise NotImplementedError()
 
+    def pipeline_nodes(self):
+        return self._pipeline
+
     def __iter__(self):
-        return iter(self._build_pipeline(self._get_files_by_worker()))
+        self._pipeline = self._build_pipeline(self._get_files_by_worker())
+        if self._change_seed_every_epoch:
+            self.set_epoch_callback(lambda epoch: self.set_random_state(
+                seed=self.get_option("seed", 42) + epoch
+            ))
+        return iter(self._pipeline[-1])
 
 
 class RefactPlainCodeDataset(RefactDataset):
     def _build_pipeline(self, files: List[Dict[str, Any]]):
-        ds = ReadFileByFile(self.basedir, files, self._ds_options)
-        ds = CodeToPrefixCompletion(ds, self._ds_options)
-        ds = pp.Tokenizer(ds, self._ds_options)
-        ds = pp.PromptCompletionToTokensMask(ds, self._ds_options)
-        ds = pp.DensePacker(ds, self._ds_options)
-        ds = pp.Shuffle(ds, self._ds_options)
-        return ds
+        read_by_file = ReadFileByFile(self.basedir, files, self._ds_options)
+        cpc = CodeToPrefixCompletion(read_by_file, self._ds_options)
+        tkn = pp.Tokenizer(cpc, self._ds_options)
+        pctm = pp.PromptCompletionToTokensMask(tkn, self._ds_options)
+        dp = pp.DensePacker(pctm, self._ds_options)
+        shf = pp.Shuffle(dp, self._ds_options)
+        return [read_by_file, cpc, tkn, pctm, dp, shf]
 
 
 class RefactFIMCodeDataset(RefactDataset):
     def _build_pipeline(self, files: List[Dict[str, Any]]):
-        ds = ReadFileByFile(self.basedir, files, self._ds_options)
-        ds = FIMv2(ds, self._ds_options)
-        ds = pp.DensePacker(ds, self._ds_options)
-        ds = pp.Shuffle(ds, self._ds_options)
-        return ds
+        read_by_file = ReadFileByFile(self.basedir, files, self._ds_options)
+        fim = FIMv2(read_by_file, self._ds_options)
+        dp = pp.DensePacker(fim, self._ds_options)
+        shf = pp.Shuffle(dp, self._ds_options)
+        return [read_by_file, fim, dp, shf]
 
 
 class CodeLLamaFIMDataset(RefactDataset):
     def _build_pipeline(self, files: List[Dict[str, Any]]):
-        ds = ReadFileByFile(self.basedir, files, self._ds_options)
-        ds = FIMv2CodeLlama(ds, self._ds_options)
-        ds = pp.DensePacker(ds, self._ds_options)
-        ds = pp.Shuffle(ds, self._ds_options)
-        return ds
+        read_by_file = ReadFileByFile(self.basedir, files, self._ds_options)
+        fim = FIMv2CodeLlama(read_by_file, self._ds_options)
+        dp = pp.DensePacker(fim, self._ds_options)
+        shf = pp.Shuffle(dp, self._ds_options)
+        return [read_by_file, fim, dp, shf]
