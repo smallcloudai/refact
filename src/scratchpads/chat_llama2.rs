d56ca983a9e1be93f2fc4aecfacafd6dfe9c27a3
Valeryi <ponyparser@gmail.com>
2024-03-24 12:22:31 +0000

    fix: unwrapping vector_of_context_files

--- src/scratchpads/chat_llama2.rs
+++ src/scratchpads/chat_llama2.rs
@@ -5,7 +5,7 @@ use async_trait::async_trait;
 use serde_json::Value;
 use tokenizers::Tokenizer;
 use tokio::sync::RwLock as ARwLock;
-use tracing::info;
+use tracing::{info, error};
 
 use crate::call_validation::{ChatMessage, ChatPost, ContextFile, SamplingParameters};
 use crate::global_context::GlobalContext;
@@ -92,7 +92,7 @@ impl ScratchpadAbstract for ChatLlama2 {
                 // prompt.push_str("\n\n");
             }
             if msg.role == "context_file" {
-                let vector_of_context_files: Vec<ContextFile> = serde_json::from_str(&msg.content).unwrap(); // FIXME unwrap
+                let vector_of_context_files: Vec<ContextFile> = serde_json::from_str(&msg.content).map_err(|e|error!("parsing context_files has failed: {}; content: {}", e, &msg.content)).unwrap_or_default();
                 for context_file in vector_of_context_files {
                     prompt.push_str(format!("{}\n```\n{}```\n\n", context_file.file_name, context_file.file_content).as_str());
                 }
