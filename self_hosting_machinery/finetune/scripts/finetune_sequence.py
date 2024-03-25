2f36384b504df233f45f90d969415e427e6f372d
mitya <dimitry.ageev@gmail.com>
2024-03-25 19:54:06 +0300

    fix single gpu training

--- self_hosting_machinery/finetune/scripts/finetune_sequence.py
+++ self_hosting_machinery/finetune/scripts/finetune_sequence.py
@@ -5,26 +5,29 @@ import subprocess
 import sys
 import psutil
 
+
 def catch_sigusr1(signum, frame):
     print("catched SIGUSR1")
     current_process = psutil.Process()
     for child in current_process.children(recursive=False):
         os.kill(child.pid, signal.SIGUSR1)
 
+
 @click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
 @click.option('--pname', default='', help='Project name')
-@click.argument('the_rest_of_args', nargs=-1)
-def main(pname, the_rest_of_args):
+@click.argument('args', nargs=-1)
+def main(pname, args):
     cuda_visible_devices = os.getenv("CUDA_VISIBLE_DEVICES", "")
-    ngpus = len(cuda_visible_devices.split(","))
-    if ngpus > 1:
+    num_gpus = len(cuda_visible_devices.split(","))
+    cmd = [sys.executable]
+    if num_gpus > 1:
         # this tries to run the same as:
         # CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node=4 ~/code/refact/self_hosting_machinery/finetune/scripts/finetune_train.py --run_id helloworld-20240320-000003 --pname Refact --model_name Refact/1.6B --low_gpu_mem_mode False
-        finetune_train = os.path.join(os.path.dirname(os.path.realpath(__file__)), "finetune_train.py")
+        # max 20999, use 21000 in case of single GPU train
         port = 20000 + hash(cuda_visible_devices) % 1000
-        subprocess.check_call(["torchrun", "--master-port", str(port), "--nproc_per_node=%d" % ngpus, finetune_train, "--pname", pname, *the_rest_of_args])
-    else:
-        subprocess.check_call([sys.executable, "-m", "self_hosting_machinery.finetune.scripts.finetune_train", "--pname", pname, *the_rest_of_args])
+        cmd = ["torchrun", "--master-port", str(port), f"--nproc_per_node={num_gpus}"]
+    finetune_train_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), "finetune_train.py")
+    subprocess.check_call([*cmd, finetune_train_script, "--pname", pname, *args])
 
 
 if __name__ == '__main__':
