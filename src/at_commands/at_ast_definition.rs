479125588a8030b2dd8afce8eceb0ea9da3890c5
Oleg Klimov <omgtech@gmail.com>
2024-03-31 10:37:20 +0200

    comment old

--- src/at_commands/at_ast_definition.rs
+++ src/at_commands/at_ast_definition.rs
@@ -8,117 +8,115 @@ use crate::ast::structs::AstQuerySearchResult;
 use crate::at_commands::at_commands::{AtCommand, AtCommandsContext, AtParam};
 use crate::at_commands::at_params::AtParamSymbolPathQuery;
 use crate::call_validation::{ChatMessage, ContextFile};
-use tracing::{info, error};
+use tracing::info;
 use crate::ast::ast_index::RequestSymbolType;
-use crate::ast::ast_module::AstModule;
-use crate::files_in_workspace::DocumentInfo;
-use crate::ast::treesitter::ast_instance_structs::SymbolInformation;
 
 
-async fn oleg_check_out_file_map(
-    astmod: &AstModule,
-    result: &AstQuerySearchResult
-) {
-    let mut files_affected: Vec<DocumentInfo> = vec![];
-    for res in &result.search_results {
-        let file_path: String = res.symbol_declaration.get_path_str();
-        files_affected.push(DocumentInfo::from_pathbuf(&std::path::PathBuf::from(file_path)).unwrap());
-    }
-    files_affected.sort();
-    files_affected.dedup();
-    for file_info in files_affected {
-        let filename = file_info.uri.to_file_path().unwrap_or_default().to_str().unwrap_or_default().to_string();  // sync with SymbolInformation::get_path_str
-        let file_markup: crate::ast::structs::FileASTMarkup = match astmod.file_markup(&file_info).await {
-            Ok(x) => x,
-            Err(e) => {
-                error!("file_markup error {:?}", e);
-                continue;
-            }
-        };
-        let lines_cnt = file_markup.file_content.lines().count();
-        let mut color: Vec<String> = vec!["".to_string(); lines_cnt];
-        let mut useful: Vec<f64> = vec![0.0; lines_cnt];
+// fn path_of_guid(guid: &String, file_markup: &crate::ast::structs::FileASTMarkup) -> String
+// {
+//     match file_markup.guid2symbol.get(guid) {
+//         Some(x) => {
+//             let pname = if !x.name.is_empty() { x.name.clone() } else { x.guid[..8].to_string() };
+//             let pp = path_of_guid(&x.parent_guid, &file_markup);
+//             return format!("{}::{}", pp, pname);
+//         },
+//         None => {
+//             info!("parent_guid {} not found, maybe outside of this file", guid);
+//             return format!("UNK");
+//         }
+//     };
+// }
 
-        let mut colorize = |symb: &SymbolInformation, spath: &String, maxuse: f64|
-        {
-            if symb.declaration_range.end_byte != 0 {
-                // full_range Range { start_byte: 696, end_byte: 1563, start_point: Point { row: 23, column: 4 }, end_point: Point { row: 47, column: 5 } }
-                // declaration_range Range { start_byte: 696, end_byte: 842, start_point: Point { row: 23, column: 4 }, end_point: Point { row: 27, column: 42 } }
-                // definition_range Range { start_byte: 843, end_byte: 1563, start_point: Point { row: 27, column: 43 }, end_point: Point { row: 47, column: 5 } }
-                for line_num in symb.declaration_range.start_point.row..symb.declaration_range.end_point.row+1 {
-                    if !color[line_num].is_empty() {
-                        continue;
-                    }
-                    color[line_num] = format!("decl {}", spath);
-                    useful[line_num] = maxuse;
-                }
-                for line_num in symb.definition_range.start_point.row..symb.definition_range.end_point.row+1 {
-                    if !color[line_num].is_empty() {
-                        continue;
-                    }
-                    color[line_num] = format!("def  {}", spath);
-                    useful[line_num] = maxuse - 3.0;
-                }
-            } else {
-                for line_num in symb.full_range.start_point.row..symb.full_range.end_point.row+1 {
-                    if !color[line_num].is_empty() {
-                        continue;
-                    }
-                    color[line_num] = format!("full {}", spath);
-                    useful[line_num] = maxuse - 6.0;
-                }
-            }
-        };
+// async fn oleg_check_out_file_map(
+//     astmod: &AstModule,
+//     result: &AstQuerySearchResult
+// ) {
+//     let mut files_affected: Vec<DocumentInfo> = vec![];
+//     for res in &result.search_results {
+//         let file_path: String = res.symbol_declaration.get_path_str();
+//         files_affected.push(DocumentInfo::from_pathbuf(&std::path::PathBuf::from(file_path)).unwrap());
+//     }
+//     files_affected.sort();
+//     files_affected.dedup();  // TODO: sort again by usefullness
+//     for file_info in files_affected {
+//         let filename = file_info.uri.to_file_path().unwrap_or_default().to_str().unwrap_or_default().to_string();  // sync with SymbolInformation::get_path_str
+//         let file_markup: crate::ast::structs::FileASTMarkup = match astmod.file_markup(&file_info).await {
+//             Ok(x) => x,
+//             Err(e) => {
+//                 error!("file_markup error {:?}", e);
+//                 continue;
+//             }
+//         };
+//         let lines_cnt = file_markup.file_content.lines().count();
+//         let mut color: Vec<String> = vec!["".to_string(); lines_cnt];
+//         let mut useful: Vec<f64> = vec![0.0; lines_cnt];
 
-        fn path_of_guid(guid: &String, file_markup: &crate::ast::structs::FileASTMarkup) -> String
-        {
-            match file_markup.guid2symbol.get(guid) {
-                Some(x) => {
-                    let pname = if !x.name.is_empty() { x.name.clone() } else { x.guid[..8].to_string() };
-                    let pp = path_of_guid(&x.parent_guid, &file_markup);
-                    return format!("{}::{}", pp, pname);
-                },
-                None => {
-                    info!("parent_guid {} not found, maybe outside of this file", guid);
-                    return format!("UNK");
-                }
-            };
-        }
+//         let mut colorize = |symb: &SymbolInformation, spath: &String, maxuse: f64|
+//         {
+//             if symb.declaration_range.end_byte != 0 {
+//                 // full_range Range { start_byte: 696, end_byte: 1563, start_point: Point { row: 23, column: 4 }, end_point: Point { row: 47, column: 5 } }
+//                 // declaration_range Range { start_byte: 696, end_byte: 842, start_point: Point { row: 23, column: 4 }, end_point: Point { row: 27, column: 42 } }
+//                 // declaration_tail row: 47
+//                 // definition_range Range { start_byte: 843, end_byte: 1563, start_point: Point { row: 27, column: 43 }, end_point: Point { row: 47, column: 5 } }
+//                 for line_num in symb.declaration_range.start_point.row..symb.declaration_range.end_point.row+1 {
+//                     if !color[line_num].is_empty() {
+//                         continue;
+//                     }
+//                     color[line_num] = format!("decl {}", spath);
+//                     useful[line_num] = maxuse;
+//                 }
+//                 for line_num in symb.definition_range.start_point.row..symb.definition_range.end_point.row+1 {
+//                     if !color[line_num].is_empty() {
+//                         continue;
+//                     }
+//                     color[line_num] = format!("def  {}", spath);
+//                     useful[line_num] = maxuse - 3.0;
+//                 }
+//             } else {
+//                 for line_num in symb.full_range.start_point.row..symb.full_range.end_point.row+1 {
+//                     if !color[line_num].is_empty() {
+//                         continue;
+//                     }
+//                     color[line_num] = format!("full {}", spath);
+//                     useful[line_num] = maxuse - 6.0;
+//                 }
+//             }
+//         };
 
-        info!("markup {}", filename);
-        for res in &result.search_results {
-            if res.symbol_declaration.get_path_str() != filename {
-                continue;
-            }
-            let mut guid = res.symbol_declaration.guid.clone();
-            let mut maxuse = 90.0;
-            while !guid.is_empty() {
-                let spath = path_of_guid(&guid, &file_markup);
-                let symbol: &SymbolInformation = match file_markup.guid2symbol.get(&guid) {
-                    Some(x) => x,
-                    None => { break; }
-                };
-                colorize(&symbol, &spath, maxuse);
-                guid = symbol.parent_guid.clone();
-                maxuse -= 10.0;
-            }
-        }
-        let mut prev_n = 0 as usize;
-        let mut result = String::new();
-        for (line_n, line) in file_markup.file_content.lines().enumerate() {
-            if useful[line_n] == 0.0 {
-                continue;
-            }
-            if line_n > prev_n + 1 {
-                result += "...\n";
-                // info!("{:05} line {:?}", line_n, line);
-            }
-            result += &format!("{:05} {:>5.1} {}\n", line_n, useful[line_n], color[line_n]);
-            prev_n = line_n;
-        }
-        info!("result {}\n{}", filename, result);
-    }
-}
+//         info!("markup {}", filename);
+//         for res in &result.search_results {
+//             if res.symbol_declaration.get_path_str() != filename {
+//                 continue;
+//             }
+//             let mut guid = res.symbol_declaration.guid.clone();
+//             let mut maxuse = 90.0;
+//             while !guid.is_empty() {
+//                 let spath = path_of_guid(&guid, &file_markup);
+//                 let symbol: &SymbolInformation = match file_markup.guid2symbol.get(&guid) {
+//                     Some(x) => x,
+//                     None => { break; }
+//                 };
+//                 colorize(&symbol, &spath, maxuse);
+//                 guid = symbol.parent_guid.clone();
+//                 maxuse -= 10.0;
+//             }
+//         }
+//         let mut prev_n = 0 as usize;
+//         let mut result = String::new();
+//         for (line_n, _line) in file_markup.file_content.lines().enumerate() {
+//             if useful[line_n] == 0.0 {
+//                 continue;
+//             }
+//             if line_n > prev_n + 1 {
+//                 result += "...\n";
+//                 // info!("{:05} line {:?}", line_n, line);
+//             }
+//             result += &format!("{:05} {:>5.1} {}\n", line_n, useful[line_n], color[line_n]);
+//             prev_n = line_n;
+//         }
+//         info!("result {}\n{}", filename, result);
+//     }
+// }
 
 
 async fn results2message(result: &AstQuerySearchResult) -> ChatMessage {
@@ -132,6 +130,7 @@ async fn results2message(result: &AstQuerySearchResult) -> ChatMessage {
             file_content: content,
             line1: res.symbol_declaration.full_range.start_point.row + 1,
             line2: res.symbol_declaration.full_range.end_point.row + 1,
+            symbol: res.symbol_declaration.guid.clone(),
             usefulness: 100.0 * res.sim_to_query
         });
     }
@@ -181,7 +180,7 @@ impl AtCommand for AtAstDefinition {
                 match ast.search_by_name(symbol_path.clone(), RequestSymbolType::Declaration).await {
                     Ok(res) => {
                         // TMP
-                        oleg_check_out_file_map(ast, &res).await;
+                        // oleg_check_out_file_map(ast, &res).await;
                         // TMP
                         Ok(results2message(&res).await)
                     },
