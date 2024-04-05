e316d398a58c3ca96adbf80b9605d04f33a8b5d6
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-04-05 15:33:46 +0200

    ui(fim): don't display headers if context properties are empty

--- src/components/FIMDebug/SearchContext.tsx
+++ src/components/FIMDebug/SearchContext.tsx
@@ -12,7 +12,7 @@ export type SearchContextProps = {
 export const SearchContext: React.FC<SearchContextProps> = ({ data }) => {
   return (
     <Flex direction="column">
-      {data.was_looking_for && (
+      {data.was_looking_for && data.was_looking_for.length > 0 && (
         <Container py="3">
           <Heading as="h4" size="2" mb="2">
             Look up symbols
@@ -21,7 +21,7 @@ export const SearchContext: React.FC<SearchContextProps> = ({ data }) => {
         </Container>
       )}
 
-      {data.attached_files && (
+      {data.attached_files && data.attached_files.length > 0 && (
         <Container py="3">
           <Heading as="h4" size="2" mb="2">
             Context files
