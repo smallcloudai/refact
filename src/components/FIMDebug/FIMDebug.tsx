45b6045c306eb77424960a47f86dc7046ca36876
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-04-08 14:03:49 +0200

    ui(fim): remove meta data from display

--- src/components/FIMDebug/FIMDebug.tsx
+++ src/components/FIMDebug/FIMDebug.tsx
@@ -32,79 +32,13 @@ export const FIMDebug: React.FC<FimDebugProps> = ({ data }) => {
             <DataList.Item>
               <DataList.Label>
                 <Text size="1" weight="medium">
-                  Cached
-                </Text>
-              </DataList.Label>
-              <DataList.Value>{data.cached ? "true" : "false"}</DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label>
-                <Text size="1" weight="medium">
-                  Snippet
-                </Text>
-              </DataList.Label>
-              <DataList.Value>{data.snippet_telemetry_id}</DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label>
-                <Text size="1" weight="medium">
                   Model
                 </Text>
               </DataList.Label>
               <DataList.Value>{data.model}</DataList.Value>
             </DataList.Item>
-            {data.created && (
-              <DataList.Item>
-                <DataList.Label>
-                  <Text size="1" weight="medium">
-                    Created
-                  </Text>
-                </DataList.Label>
-                <DataList.Value>{data.created}</DataList.Value>
-              </DataList.Item>
-            )}
-            {data.elapsed && (
-              <DataList.Item>
-                <DataList.Label>
-                  <Text size="1" weight="medium">
-                    Elapsed
-                  </Text>
-                </DataList.Label>
-                <DataList.Value>{data.elapsed}</DataList.Value>
-              </DataList.Item>
-            )}
           </DataList.Root>
         </Section>
-
-        {/* <Heading size="5">Choices</Heading>
-      <Section size="1">
-        {data.choices.map((choice, i) => {
-          return (
-            <DataList.Root
-              key={i}
-              orientation={{
-                initial: "vertical",
-                xs: "horizontal",
-              }}
-            >
-              <DataList.Item>
-                <DataList.Label>Index</DataList.Label>
-                <DataList.Value>{choice.index}</DataList.Value>
-              </DataList.Item>
-              <DataList.Item>
-                <DataList.Label>Code</DataList.Label>
-                <DataList.Value>
-                  <Code>{choice.code_completion}</Code>
-                </DataList.Value>
-              </DataList.Item>
-              <DataList.Item>
-                <DataList.Label>Finish reason</DataList.Label>
-                <DataList.Value>{choice.finish_reason}</DataList.Value>
-              </DataList.Item>
-            </DataList.Root>
-          );
-        })}
-      </Section> */}
 
         {data.context && <SearchContext data={data.context} />}
       </Flex>
