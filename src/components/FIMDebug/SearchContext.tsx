4af57d45e64f65607c8e703cb4e2ca37fdede994
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-04-09 13:46:47 +0200

    ui(FIM): show a message when there is no context file in the completion

--- src/components/FIMDebug/SearchContext.tsx
+++ src/components/FIMDebug/SearchContext.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading, Flex, Container } from "@radix-ui/themes";
+import { Heading, Flex, Container, Text } from "@radix-ui/themes";
 import { FileList } from "../FileList";
 import { SymbolList } from "./SymoblList";
 
@@ -12,22 +12,21 @@ export type SearchContextProps = {
 export const SearchContext: React.FC<SearchContextProps> = ({ data }) => {
   return (
     <Flex direction="column">
-      {data.attached_files && data.attached_files.length > 0 && (
-        <Container py="3">
-          <Heading as="h4" size="2" mb="2">
-            Context files
-          </Heading>
+      <Container py="3">
+        {data.attached_files && data.attached_files.length > 0 ? (
           <FileList files={data.attached_files} />
-        </Container>
-      )}
-      {data.was_looking_for && data.was_looking_for.length > 0 && (
-        <Container py="3">
-          <Heading as="h4" size="2" mb="4">
-            Look up symbols
-          </Heading>
-          <SymbolList symbols={data.was_looking_for} />
-        </Container>
-      )}
+        ) : (
+          <Text wrap="nowrap" style={{ overflow: "hidden" }} size="2">
+            No attached files
+          </Text>
+        )}
+      </Container>
+      <Container py="3">
+        <Heading as="h4" size="2" mb="4">
+          Look up symbols
+        </Heading>
+        <SymbolList symbols={data.was_looking_for} />
+      </Container>
     </Flex>
   );
 };
