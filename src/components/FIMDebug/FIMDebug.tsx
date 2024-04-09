d545290428959b4c7b218f9feb77120177e759c1
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-04-09 09:41:49 +0200

    ui(fim): adjust padding on the heading and heading text and remove word-wrap

--- src/components/FIMDebug/FIMDebug.tsx
+++ src/components/FIMDebug/FIMDebug.tsx
@@ -10,11 +10,13 @@ export const FIMDebug: React.FC<FimDebugProps> = ({ data }) => {
   return (
     <ScrollArea scrollbars="vertical" fullHeight>
       {/** change scrollbars to both to remove word wrap */}
-      <Flex direction="column" px="2" height="100%">
-        <Heading size="4">Code Completion Context</Heading>
+      <Flex direction="column" px="2" py="2" height="100%">
+        <Heading size="4" wrap="nowrap" style={{ overflow: "hidden" }}>
+          Code Completion Context
+        </Heading>
         {data.context && <SearchContext data={data.context} />}
 
-        <Box mt="auto" pb="2">
+        <Box mt="auto">
           <Text wrap="nowrap" style={{ overflow: "hidden" }} size="1">
             model: {data.model}
           </Text>
