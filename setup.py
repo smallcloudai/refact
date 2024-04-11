3c63c9eaf882004762bedc2d95d8ee312b51b9cb
mitya <dimitry.ageev@gmail.com>
2024-04-11 11:47:11 +0300

    starcodeer2 already in transformers release, install it

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ all_refact_packages = {
               "webgui/static/dashboards/*", "webgui/static/assets/*", "webgui/static/utils/*",]),
     "self_hosting_machinery": PyPackage(
         requires=["python-multipart", "auto-gptq==0.6.0", "accelerate",
-                  "termcolor", "torch", "transformers @ git+https://github.com/huggingface/transformers",
+                  "termcolor", "torch", "transformers>=4.39.3",
                   "bitsandbytes", "safetensors", "peft", "triton",
                   "torchinfo", "mpi4py", "deepspeed==0.12.6",
                   "sentence-transformers", "huggingface-hub>=0.19.3"],
