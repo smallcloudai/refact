58205e556c9509b1eff8a62b101607cf0268e9a1
mitya <dimitry.ageev@gmail.com>
2024-04-27 13:10:33 +0300

    specify model in error message

--- refact_webgui/webgui/selfhost_model_assigner.py
+++ refact_webgui/webgui/selfhost_model_assigner.py
@@ -23,8 +23,12 @@ def has_context_switch(filter_caps: List[str]) -> bool:
     return "chat" in filter_caps or "completion" in filter_caps
 
 
-def get_default_n_ctx(model_info: Dict[str, Any]) -> int:
-    return model_info.get("T", model_info["n_ctx"])
+def get_default_n_ctx(model_name: str, model_info: Dict[str, Any]) -> int:
+    if "T" in model_info:
+        return model_info["T"]
+    if "n_ctx" in model_info:
+        return model_info["n_ctx"]
+    raise ValueError(f"context size is not specified for '{model_name}'")
 
 
 @dataclass
@@ -218,7 +222,7 @@ class ModelAssigner:
                 ]
             has_finetune = bool("finetune" in rec["filter_caps"])
             finetune_model = rec.get("finetune_model", k if has_finetune else None)
-            default_n_ctx = get_default_n_ctx(rec)
+            default_n_ctx = get_default_n_ctx(k, rec)
             available_n_ctx = []
             if has_context_switch(rec["filter_caps"]):
                 available_n_ctx = list(filter(lambda n_ctx: n_ctx <= default_n_ctx, ALLOWED_N_CTX))
@@ -249,7 +253,7 @@ class ModelAssigner:
             j = {"model_assign": {}}
 
         def _set_n_ctx(model: str, record: Dict) -> Dict:
-            default_n_ctx = get_default_n_ctx(self.models_db[model])
+            default_n_ctx = get_default_n_ctx(model, self.models_db[model])
             if not has_context_switch(self.models_db[model].get("filter_caps", [])):
                 record["n_ctx"] = default_n_ctx
                 return record
