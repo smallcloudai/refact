53a22152712b9c76694f122bf808479a4212bde0
Oleg Klimov <omgtech@gmail.com>
2024-05-04 06:51:15 +0200

    remove HEADLESS

--- self_hosting_machinery/watchdog/docker_watchdog.py
+++ self_hosting_machinery/watchdog/docker_watchdog.py
@@ -8,14 +8,11 @@ import sys
 import time
 import uuid
 import psutil
-
 from pathlib import Path
-
 from typing import Dict, Optional, List
 
 from refact_utils.scripts import env
 
-HEADLESS = ("--headless" in sys.argv)    # for ui-less docker: skip all standard cfg-s (web ui, enum gpus, etc), skip first_run script
 FIRST_RUN_CMDLINE = [sys.executable, "-m", "self_hosting_machinery.scripts.first_run"]
 
 
@@ -312,7 +309,7 @@ class TrackedJob:
                f"  status: {self.status_from_stderr}\n"
 
 tracked: Dict[str, TrackedJob] = {}
-watchdog_templates = list(Path(env.DIR_WATCHDOG_TEMPLATES).iterdir()) if not HEADLESS else []
+watchdog_templates = list(Path(env.DIR_WATCHDOG_TEMPLATES).iterdir())
 
 
 def create_tracked_jobs_from_configs():
@@ -452,21 +449,10 @@ def factory_reset():
 
 
 def first_run():
-    if HEADLESS:
-        return
     subprocess.check_call(FIRST_RUN_CMDLINE)
 
 
 def main_loop():
-    if not HEADLESS:
-        # Generate a random SMALLCLOUD_API_KEY, it will be inherited by subprocesses,
-        # this allows inference_worker to authorize on the local web server (both use
-        # this variable), and work safely even if we expose http port to the world.
-        os.environ["SMALLCLOUD_API_KEY"] = str(uuid.uuid4())
-    else:
-        assert "SMALLCLOUD_API_KEY" in os.environ
-
-    first_run()
     while 1:
         main_loop_body()
         time.sleep(1)
@@ -479,4 +465,9 @@ def main_loop():
 
 
 if __name__ == '__main__':
+    # Generate a random SMALLCLOUD_API_KEY, it will be inherited by subprocesses,
+    # this allows inference_worker to authorize on the local web server (both use
+    # this variable), and work safely even if we expose http port to the world.
+    os.environ["SMALLCLOUD_API_KEY"] = str(uuid.uuid4())
+    first_run()
     main_loop()
