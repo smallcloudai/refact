334e51fc06b3fe4c63a9ca00aef56478cef6d040
Dimitry Ageev <dimitry.ageev@gmail.com>
2024-05-08 17:49:01 +0300

    kill all child processes first (#419)
    * kill all child processes first
    
    * parent process may be not exists

--- self_hosting_machinery/watchdog/docker_watchdog.py
+++ self_hosting_machinery/watchdog/docker_watchdog.py
@@ -241,15 +241,18 @@ class TrackedJob:
         if self.please_shutdown and self.sent_sigusr1_ts + sigkill_timeout < time.time():
             log("%s SIGUSR1 timed out, sending kill %s" % (time.strftime("%Y%m%d %H:%M:%S"), self.p.pid))
             try:
+                itself = psutil.Process(self.p.pid)
+                for child in itself.children(recursive=True):
+                    try:
+                        child.kill()
+                    except psutil.NoSuchProcess:
+                        pass
+            except psutil.NoSuchProcess:
+                pass
+            try:
                 self.p.kill()
             except psutil.NoSuchProcess:
                 pass
-            itself = psutil.Process(self.p.pid)
-            for child in itself.children(recursive=True):
-                try:
-                    child.kill()
-                except psutil.NoSuchProcess:
-                    pass
 
     def maybe_can_start(self):
         if self.p is not None:
