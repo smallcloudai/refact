306d41b10a796130bdb7fcd28317217e4428a9e5
Oleg Klimov <omgtech@gmail.com>
2024-06-16 05:56:24 +0200

    ast references tool

--- src/at_tools/att_ast_reference.rs
+++ src/at_tools/att_ast_reference.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
 use serde_json::Value;
 use tracing::info;
+use itertools::Itertools;
 
 use async_trait::async_trait;
 
-use crate::at_commands::at_ast_reference::{execute_at_ast_reference, text_on_clip};
-use crate::at_commands::at_commands::{AtCommandsContext, vec_context_file_to_context_tools};
+use crate::at_commands::at_ast_definition::results2message;
+use crate::at_commands::at_commands::AtCommandsContext;
 use crate::at_tools::at_tools::AtTool;
 use crate::call_validation::{ChatMessage, ContextEnum};
+use crate::ast::ast_index::RequestSymbolType;
 
 
 pub struct AttAstReference;
@@ -26,20 +28,58 @@ impl AtTool for AttAstReference {
             symbol = symbol[dot_index+1..].to_string();
         }
 
-        let (query_result, refs_n) = execute_at_ast_reference(ccx, &symbol).await?;
+        let ast_mb = ccx.global_context.read().await.ast_module.clone();
+        let ast = ast_mb.ok_or_else(|| "AST support is turned off".to_string())?;
 
-        let mut results = vec_context_file_to_context_tools(query_result);
-        let text = text_on_clip(&symbol, refs_n);
+        let search_results: crate::ast::structs::AstQuerySearchResult = ast.read().await.search_by_name(
+            symbol.clone(),
+            RequestSymbolType::Usage,
+            false,
+            ccx.top_n,
+        ).await?;
+        if search_results.search_results.len() == 0 {
+            let search_results_fuzzy: crate::ast::structs::AstQuerySearchResult = ast.read().await.search_by_name(
+                symbol.clone(),
+                RequestSymbolType::Usage,
+                true,
+                6,
+            ).await?;
+            if search_results_fuzzy.search_results.len() == 0 {
+                return Err("There is no `{}` in the syntax tree, and no similar names found :/".to_string());
+            } else {
+                let mut s = String::new();
+                s.push_str("There is no `{}` in the syntax tree, call again with one of these close names:\n");
+                let all_names_unique = search_results_fuzzy.search_results.iter()
+                    .map(|r| r.symbol_declaration.name.clone())
+                    .sorted()
+                    .unique()
+                    .collect::<Vec<String>>();
+                for x in all_names_unique.iter() {
+                    s.push_str(&format!("{}\n", x));
+                }
+                return Err(s);
+            }
+        }
+        let mut results = results2message(&search_results).await
+            .into_iter().map(|x| ContextEnum::ContextFile(x)).collect::<Vec<ContextEnum>>();
 
+        let mut s = String::new();
+        s.push_str(format!("References to `{}` found at:\n", symbol).as_str());
+        for r in search_results.search_results.iter() {
+            let file_path_str = r.symbol_declaration.file_path.to_str().unwrap().to_string();
+            let decl_range = &r.symbol_declaration.full_range;
+            s.push_str(&format!("{}:{}\n", file_path_str, decl_range.start_point.row + 1));
+        }
         results.push(ContextEnum::ChatMessage(ChatMessage {
             role: "tool".to_string(),
-            content: text,
+            content: s.clone(),
             tool_calls: None,
             tool_call_id: tool_call_id.clone(),
         }));
 
         Ok(results)
     }
+
     fn depends_on(&self) -> Vec<String> {
         vec!["ast".to_string()]
     }
