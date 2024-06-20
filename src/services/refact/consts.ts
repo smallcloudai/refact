9a9614bda8df33d8540082505f3ba44a5abd5789
Oleg Klimov <omgtech@gmail.com>
2024-06-20 14:39:04 +0200

    rename /at-tools-available -> /tools

--- src/services/refact/consts.ts
+++ src/services/refact/consts.ts
@@ -4,4 +4,4 @@ export const STATISTIC_URL = `/v1/get-dashboard-plots`;
 export const AT_COMMAND_COMPLETION = "/v1/at-command-completion";
 export const AT_COMMAND_PREVIEW = "/v1/at-command-preview";
 export const CUSTOM_PROMPTS_URL = "/v1/customization";
-export const AT_TOOLS_AVAILABLE_URL = "/v1/at-tools-available";
+export const AT_TOOLS_AVAILABLE_URL = "/v1/tools";
