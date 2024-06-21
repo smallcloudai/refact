92cc5133e50c9f76dc60031714be78afcd20097a
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-06-21 13:37:20 +0200

    refactor(diff): remove markdown and use the syntax highlighter directly

--- src/components/ChatContent/DiffContent.tsx
+++ src/components/ChatContent/DiffContent.tsx
@@ -2,109 +2,70 @@ import React from "react";
 import { Text, Container, Box, Flex } from "@radix-ui/themes";
 import { DiffAction } from "../../events";
 import { ScrollArea } from "../ScrollArea";
-import SyntaxHighlighter, {
-  type SyntaxHighlighterProps,
-} from "react-syntax-highlighter";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import classNames from "classnames";
-import ReactMarkdown from "react-markdown";
-import { type Element } from "hast";
+
 import styles from "./ChatContent.module.css";
 import hljsStyle from "react-syntax-highlighter/dist/esm/styles/hljs/agate";
 
-function toDiffMarkdown(str: string, type: "add" | "remove") {
-  const replacement = type === "add" ? "\n+" : "\n-";
+function toDiff(str: string, type: "add" | "remove"): string {
+  const sign = type === "add" ? "+" : "-";
+
   const replaceEscapedEOL = str
     .split("\\n")
     .filter((_) => _)
-    .join(replacement);
+    .join("\n" + sign);
 
-  return "```diff" + replacement + replaceEscapedEOL + "\n```";
+  return sign + replaceEscapedEOL;
 }
 
-// TODO: add this to the Markdown components
-
-type CodeBlockProps = React.JSX.IntrinsicElements["code"] & {
-  node?: Element | undefined;
-  style?: Record<string, React.CSSProperties>;
-} & Pick<
-    SyntaxHighlighterProps,
-    "showLineNumbers" | "startingLineNumber" | "style"
-  >;
-
-const CodeBlock: React.FC<CodeBlockProps> = ({
-  children,
-  className,
-  color: _color,
-  ref: _ref,
-  node: _node,
-  ...rest
-}) => {
-  const match = /language-(\w+)/.exec(className ?? "");
-  const textWithOutTrailingNewLine = String(children).replace(/\n$/, "");
-
-  const language: string = match && match.length > 0 ? match[1] : "text";
+const Highlight: React.FC<{
+  children: string;
+  showLineNumbers?: boolean;
+  startingLineNumber?: number;
+  className: string;
+}> = ({ children, className, ...rest }) => {
   return (
     <SyntaxHighlighter
-      className={className}
+      style={hljsStyle}
       PreTag={(props) => (
-        <pre {...props} className={classNames(styles.diff_pre)} />
+        <pre {...props} className={classNames(styles.diff_pre, className)} />
       )}
-      language={language}
+      language="diff"
       {...rest}
     >
-      {textWithOutTrailingNewLine.trim()}
+      {children}
     </SyntaxHighlighter>
   );
 };
 
-type MarkdownProps = {
-  children?: string;
-  className?: string;
-} & Pick<CodeBlockProps, "showLineNumbers" | "startingLineNumber">;
-
-const Markdown: React.FC<MarkdownProps> = ({
-  children,
-  className,
-  ...rest
-}) => {
-  return (
-    <ReactMarkdown
-      className={className}
-      components={{
-        code: (props) => <CodeBlock {...props} {...rest} style={hljsStyle} />,
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-  );
-};
-
 export const Diff: React.FC<{ diff: DiffAction }> = ({ diff }) => {
-  const removeString =
-    diff.lines_remove && toDiffMarkdown(diff.lines_remove, "remove");
-  const addString = diff.lines_add && toDiffMarkdown(diff.lines_add, "add");
+  const removeString = diff.lines_remove && toDiff(diff.lines_remove, "remove");
+  const addString = diff.lines_add && toDiff(diff.lines_add, "add");
 
   return (
     <Box>
       <Text size="1">{diff.file_name}</Text>
       <ScrollArea scrollbars="horizontal">
         <Box className={styles.diff}>
-          {diff.lines_remove && (
-            <Markdown
+          {removeString && (
+            <Highlight
               className={styles.diff_first}
               showLineNumbers={!!diff.line1}
               startingLineNumber={diff.line1}
             >
               {removeString}
-            </Markdown>
+            </Highlight>
           )}
-          <Markdown
-            className={styles.diff_second}
-            showLineNumbers={!!diff.line1}
-            startingLineNumber={diff.line1}
-          >
-            {addString}
-          </Markdown>
+          {addString && (
+            <Highlight
+              className={styles.diff_second}
+              showLineNumbers={!!diff.line1}
+              startingLineNumber={diff.line1}
+            >
+              {addString}
+            </Highlight>
+          )}
         </Box>
       </ScrollArea>
     </Box>
