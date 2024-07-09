76d220105ffdcedc3254573287554712901ca6ba
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2024-07-09 10:11:54 +0200

    feat: exporting app

--- src/lib/render/Chat.tsx
+++ src/lib/render/Chat.tsx
@@ -1,18 +1,15 @@
 import React, { StrictMode } from "react";
 import { ConfigProvider, type Config } from "../../contexts/config-context.tsx";
 import { ChatWithOutSideBar } from "./ChatWithoutSideBar.tsx";
-import { App } from "../../features/App.tsx";
-import { Theme } from "../../components/Theme/index.ts";
 import "./web.css";
+import { ChatWithSideBar } from "./ChatWithSideBar.tsx";
 
 export const Chat: React.FC<Config> = (config) => {
   return (
     <StrictMode>
       <ConfigProvider config={config}>
         {config.host === "web" || config.dev ? (
-          <Theme>
-            <App />
-          </Theme>
+          <ChatWithSideBar />
         ) : (
           <ChatWithOutSideBar />
         )}
