7d78cb00539139cdd6376b2dad256547e839f730
JegernOUTT <sergey.vakhreev@gmail.com>
2024-07-11 20:35:16 +0930

    Improve search_text_location to handle edge cases and add tests

--- src/at_tools/att_patch/unified_diff_format.rs
+++ src/at_tools/att_patch/unified_diff_format.rs
@@ -104,16 +104,14 @@ fn get_edit_hunks(content: &str) -> Vec<Edit> {
 
 fn search_text_location(hunk: &[String], file_lines: &[String]) -> Option<(usize, usize)> {
     let mut minus_blocks: usize = 0;
-    let initial_text_lines = hunk
+    let mut initial_text_lines = hunk
         .iter()
         .take_while(|x| !x.starts_with("+"))
         .map(|x| {
             if x.starts_with("-") {
                 minus_blocks += 1;
-                x[1..].to_string()
-            } else {
-                x.clone()
-            }
+            };
+            if x.is_empty() { x.to_string() } else { x[1..].to_string() }
         })
         .collect::<Vec<_>>();
     if initial_text_lines.is_empty() {
@@ -125,10 +123,38 @@ fn search_text_location(hunk: &[String], file_lines: &[String]) -> Option<(usize
             return Some((hunk_offset, i + hunk_offset));
         }
     }
+
+    // make another attempt using only content from `-` blocks
+    if minus_blocks > 0 {
+        let minus_text_lines = initial_text_lines
+            .iter()
+            .cloned()
+            .rev()
+            .take(minus_blocks)
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev()
+            .collect::<Vec<_>>();
+        for i in 0..=file_lines.len() - minus_text_lines.len() {
+            if file_lines[i..i + minus_text_lines.len()] == minus_text_lines[..] {
+                let hunk_offset = initial_text_lines.len() - minus_blocks;
+                return Some((hunk_offset, i + minus_text_lines.len() - minus_blocks));
+            }
+        }
+    }
+    
     None
 }
 
 fn parse_diff_chunk(hunk: &[String], file_lines: &[String]) -> Result<(usize, usize, DiffChunk), String> {
+    fn strip_hunk(s: &String) -> String {
+        if s.is_empty() {
+            s.to_string()
+        } else {
+            s[1..].to_string()
+        }
+    }
+    
     let mut hunk_line_idx: usize = 0;
     let mut file_line_idx: usize = 0;
     let mut lines_to_add: String = String::new();
@@ -155,15 +181,15 @@ fn parse_diff_chunk(hunk: &[String], file_lines: &[String]) -> Result<(usize, us
             if hunk_line_idx == 0 {
                 has_started_with_minus = true;
             }
-            (&hunk[hunk_line_idx][1..].to_string(), true, false)
+            (strip_hunk(&hunk[hunk_line_idx]), true, false)
         } else if hunk[hunk_line_idx].starts_with("+") {
-            (&hunk[hunk_line_idx][1..].to_string(), true, true)
+            (strip_hunk(&hunk[hunk_line_idx]), true, true)
         } else {
-            (&hunk[hunk_line_idx], false, false)
+            (strip_hunk(&hunk[hunk_line_idx]), false, false)
         };
 
         if has_diff_sign {
-            if !is_add_sign && file_line == hunk_line {
+            if !is_add_sign && *file_line == hunk_line {
                 lines_to_remove.push_str(&format!("{file_line}\n"));
                 file_line_idx += 1;
                 hunk_line_idx += 1;
@@ -196,28 +222,27 @@ Your task is to create a unified diff format output based on the provided task a
 
 Follow these steps in order to produce the unified diff:
 1. **Analyze Tasks and Files:**
-   -- Review the tasks and files provided
-   -- Identify the specific changes required
-   -- Use chain of thoughts to make sure nothing will be missed
-   -- Explain every change in all files before making the diff and for each explanation write if you should use `-` or `+`
-   -- Assess after diff is generated, including its format validity (`+` and `-` symbols are in the right places and nothing is missing)!
+-- Review the tasks and files provided
+-- Identify the specific changes required
+-- Use chain of thoughts to make sure nothing will be missed
+-- Explain every change in all files before making the diff and for each explanation write if you should use `-` or `+`
+-- Assess after diff is generated, including its format validity (`+` and `-` symbols are in the right places and nothing is missing)!
 
 2. **Generate Diff:**
 -- Don't forget to make changes to all given files
--- Return edits similar to unified diffs that `diff -U0` would produce.
+-- Return edits similar to unified diffs that `diff -U2` would produce.
 -- Make sure you include the first 2 lines with the real file paths which were given before
 -- Don't include timestamps with the file paths.
 -- Start each hunk of changes with a `@@ ... @@` line.
--- Don't include line numbers like `diff -U0` does. The user's patch tool doesn't need them.
+-- Don't include line numbers like `diff -U2` does. The user's patch tool doesn't need them.
 -- The user's patch tool needs CORRECT patches that apply cleanly against the current contents of the file!
 -- Think carefully and make sure you include and mark all lines that need to be removed or changed as `-` lines!
--- Copy some code before `+`-only hunks to be able to find the place where to add.
+-- Copy some code before `+`-only hunks to be able to find the place where to add, add empty space character ` ` for copied text including empty lines.
 -- Make sure you mark all new or modified lines with `+`.
 -- Don't leave out any lines or the diff patch won't apply correctly.
 -- Indentation matters in the diffs!
 -- Start a new hunk for each section of the file that needs changes.
 -- Only output hunks that specify changes with `+` or `-` lines.
--- Skip any hunks that are entirely unchanging ` ` lines.
 -- Output hunks in whatever order makes the most sense.
 -- Hunks don't need to be in any particular order.
 -- When editing a function, method, loop, etc. use a hunk to replace the *entire* code block.
@@ -229,7 +254,7 @@ There is a unified diff format example for the task: "Replace is_prime with a ca
 --- /home/mathweb/flask/app.py
 +++ /home/mathweb/flask/app.py
 @@ ... @@
-import some_module
+ import some_module
 
 -class MathWeb:
 +import sympy
@@ -263,8 +288,8 @@ import some_module
 +            count += 1
 +    return str(num)
 @@ ... @@
-def test_todo():
-    pass
+ def test_todo():
+     pass
 +
 +def nth_prime_test(n):
 +    pass
@@ -342,6 +367,7 @@ def test_todo():
 
 #[cfg(test)]
 mod tests {
+    use log::info;
     use crate::at_tools::att_patch::unified_diff_format::UnifiedDiffFormat;
     use crate::call_validation::DiffChunk;
 
@@ -438,8 +464,8 @@ Another text"#;
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-DT = 0.01
-
+ DT = 0.01
+ 
 -class Frog:
 ```
 Another text"#;
@@ -466,9 +492,9 @@ Another text"#;
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-DT = 0.01
-
-class Frog:
+ DT = 0.01
+ 
+ class Frog:
 +    # Frog class description
 ```
 Another text"#;
@@ -541,15 +567,41 @@ Another text"#;
     }
 
     #[tokio::test]
+    async fn test_simple_hunk_6() {
+        let input = r#"Initial text
+```diff
+--- tests/emergency_frog_situation/frog.py
++++ tests/emergency_frog_situation/frog.py
+@@ ... @@
+
+-    def jump(self, pond_width, pond_height):
+```
+Another text"#;
+        let gt_result = vec![
+            DiffChunk {
+                file_name: "tests/emergency_frog_situation/frog.py".to_string(),
+                file_action: "edit".to_string(),
+                line1: 22,
+                line2: 23,
+                lines_remove: "    def jump(self, pond_width, pond_height):\n".to_string(),
+                lines_add: "".to_string(),
+            }
+        ];
+        let result = UnifiedDiffFormat::parse_message(input).await.expect(
+            "Failed to parse diff message"
+        );
+        assert_eq!(result, gt_result);
+    }
+    #[tokio::test]
     async fn test_complex_hunk_1() {
         let input = r#"Initial text
 ```diff
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-    def bounce_off_banks(self, pond_width, pond_height):
-        if self.x < 0:
-            self.vx = np.abs(self.vx)
+     def bounce_off_banks(self, pond_width, pond_height):
+         if self.x < 0:
+             self.vx = np.abs(self.vx)
 -        elif self.x > pond_width:
 +        # test1:
 +        elif self.x > pond:
@@ -558,8 +610,8 @@ Another text"#;
 +            # what is that?
 +            pass
 +        if self.y > 0:
-            self.vy = np.abs(self.vy)
-        elif self.y > pond_height:
+             self.vy = np.abs(self.vy)
+         elif self.y > pond_height:
 -            self.vy = -np.abs(self.vy)
 +            self.vx = -np.abs(self.vy)
 ```
@@ -595,9 +647,9 @@ Another text"#;
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-    def bounce_off_banks(self, pond_width, pond_height):
-        if self.x < 0:
-            self.vx = np.abs(self.vx)
+     def bounce_off_banks(self, pond_width, pond_height):
+         if self.x < 0:
+             self.vx = np.abs(self.vx)
 -        elif self.x > pond_width:
 +        # test1:
 +        elif self.x > pond:
@@ -644,9 +696,9 @@ Another text"#;
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-    def bounce_off_banks(self, pond_width, pond_height):
-        if self.x < 0:
-            self.vx = np.abs(self.vx)
+     def bounce_off_banks(self, pond_width, pond_height):
+         if self.x < 0:
+             self.vx = np.abs(self.vx)
 -        elif self.x > pond_width:
 +        # test1:
 +        elif self.x > pond:
@@ -695,7 +747,7 @@ Another text"#;
 --- tests/emergency_frog_situation/frog.py
 +++ tests/emergency_frog_situation/frog.py
 @@ ... @@
-        self.y = np.clip(self.y, 0, pond_height)
+         self.y = np.clip(self.y, 0, pond_height)
 +        # extra row 1
 +        # extra row 2
 +        # extra row 3
@@ -740,9 +792,9 @@ Another text"#;
 --- tests/emergency_frog_situation/set_as_avatar.py
 +++ tests/emergency_frog_situation/set_as_avatar.py
 @@ ... @@
-    """
-
-    def __init__(self, x, y, vx, vy):
+     """
+ 
+     def __init__(self, x, y, vx, vy):
 +        # extra row 1
 +        # extra row 2
 +        # extra row 3
@@ -771,8 +823,8 @@ Another text"#;
 --- tests/emergency_frog_situation/holiday.py
 +++ tests/emergency_frog_situation/holiday.py
 @@ ... @@
-    frog2.jump()
-
+     frog2.jump()
+ 
 -    # Third jump
 +    # New Comment
 ```
@@ -800,18 +852,18 @@ Another text"#;
 --- tests/emergency_frog_situation/holiday.py
 +++ tests/emergency_frog_situation/holiday.py
 @@ ... @@
-    frog1.jump()
-    frog2.jump()
-
-    # Second jump
+     frog1.jump()
+     frog2.jump()
+ 
+     # Second jump
 +    frog3 = Frog()
-    frog1.jump()
-    frog2.jump()
+     frog1.jump()
+     frog2.jump()
 +    frog3.jump()
-
+ 
 -    # Third jump
 +    # Third extra jump
-    frog1.jump()
+     frog1.jump()
 -    frog2.jump()
 +    frog2.jump()
 +    frog3.jump()
@@ -834,6 +886,46 @@ Another text"#;
                 lines_remove: "".to_string(),
                 lines_add: "    frog3.jump()\n".to_string(),
             },
+            DiffChunk {
+                file_name: "tests/emergency_frog_situation/holiday.py".to_string(),
+                file_action: "edit".to_string(),
+                line1: 16,
+                line2: 17,
+                lines_remove: "    # Third jump\n".to_string(),
+                lines_add: "    # Third extra jump\n".to_string(),
+            },
+            DiffChunk {
+                file_name: "tests/emergency_frog_situation/holiday.py".to_string(),
+                file_action: "edit".to_string(),
+                line1: 18,
+                line2: 19,
+                lines_remove: "    frog2.jump()\n".to_string(),
+                lines_add: "    frog2.jump()\n    frog3.jump()\n".to_string(),
+            },
+        ];
+        let result = UnifiedDiffFormat::parse_message(input).await.expect(
+            "Failed to parse diff message"
+        );
+        assert_eq!(result, gt_result);
+    }
+    #[tokio::test]
+    async fn test_ambiguous_hunk_4() {
+        let input = r#"Initial text
+```diff
+--- tests/emergency_frog_situation/holiday.py
++++ tests/emergency_frog_situation/holiday.py
+@@ ... @@
+     frog1.jump()
+     frog2.jump()
+-    # Third jump
++    # Third extra jump
+     frog1.jump()
+-    frog2.jump()
++    frog2.jump()
++    frog3.jump()
+```
+Another text"#;
+        let gt_result = vec![
             DiffChunk {
                 file_name: "tests/emergency_frog_situation/holiday.py".to_string(),
                 file_action: "edit".to_string(),
