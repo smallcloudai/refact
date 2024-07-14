0e8141202e8635f11133f2236a364b673f7e89e0
mitya <dimitry.ageev@gmail.com>
2024-07-14 05:26:03 +0300

    combine all ateps together

--- swe/swe_steps.py
+++ swe/swe_steps.py
@@ -6,29 +6,71 @@ from argparse import ArgumentParser
 
 from agent_runner import AgentRunner
 from agent_runner import get_swe_bench_lite_instance
-from step1 import SetTaskStep
-from step2 import SolveTaskStep
+from step1 import ExploreRepoStep
+from step2 import ProducePatchStep
+from step3 import ChooseSolutionStep
 
 from pathlib import Path
 from typing import Dict, Any
 
 
-MODEL = "gpt-3.5-turbo"
+# MODEL = "gpt-3.5-turbo"
+MODEL = "gpt-4o"
 
 
+# TODO: more logging for each step:
+#  messages
+#  compute cost
+#  number of steps
+#  step1 additional info about patched file
+#  ...
 class SWERunner(AgentRunner):
     async def _steps(self, base_url: str, repo_path: Path, *args, **kwargs) -> Dict[str, Any]:
         results: Dict[str, Any] = dict()
+        problem_statement = kwargs["problem_statement"]
+
+        # step1: explore repo, find files that can be useful for the problem
         try:
-            step1 = SetTaskStep(base_url=base_url, model_name=MODEL)
-            results["task"] = await step1.process(problem_statement=kwargs["problem_statement"])
+            step1 = ExploreRepoStep(base_url=base_url, model_name=MODEL, attempts=3)
+            results["filenames_list_all"] = await step1.process(
+                problem_statement=problem_statement,
+                repo_path=repo_path)
+            results["filenames_list"] = "\n".join(filter(
+                lambda x: "test" not in x,
+                results["filenames_list_all"].split("\n")
+            ))
         except Exception as e:
-            raise RuntimeError(f"step1: {type(e)} {str(e) or traceback.format_exc()}")
+            results["error"] = f"step1: {type(e)} {str(e) or traceback.format_exc()}"
+            return results
+
+        # step2: produce patches for the problem with given files from step1
         try:
-            step2 = SolveTaskStep(base_url=base_url, model_name=MODEL)
-            results["model_patch"] = await step2.process(task=results["task"], repo_path=repo_path)
+            results["task"] = problem_statement
+            if results["filenames_list"]:
+                results["task"] = "\n\n".join([
+                    results["task"],
+                    f"Use these files to solve the problem:",
+                    results["filenames_list"],
+                ])
+            step2 = ProducePatchStep(base_url=base_url, model_name=MODEL, temperature=0.3, attempts=3)
+            results["model_patches"] = await step2.process(
+                task=results["task"],
+                repo_path=repo_path)
         except Exception as e:
-            raise RuntimeError(f"step2: {type(e)} {str(e) or traceback.format_exc()}")
+            results["error"] = f"step2: {type(e)} {str(e) or traceback.format_exc()}"
+            return results
+
+        # step3: choose the best solution from the list of patches
+        try:
+            step3 = ChooseSolutionStep(base_url=base_url, model_name=MODEL)
+            results["model_patch"] = await step3.process(
+                problem_statement=problem_statement,
+                model_patches=results["model_patches"],
+                repo_path=repo_path)
+        except Exception as e:
+            results["error"] = f"step3: {type(e)} {str(e) or traceback.format_exc()}"
+            return results
+
         return results
 
 
@@ -36,20 +78,23 @@ async def main():
     parser = ArgumentParser()
     parser.add_argument("instance_id", type=str, help="SWE instance id")
     parser.add_argument("--timeout", type=float, default=None, help="processing timeout")
-    parser.add_argument("--output-dir", type=Path, default="swe/predictions/test", help="output directory")
+    parser.add_argument("--output-dir", type=Path, default=None, help="output directory")
     args = parser.parse_args()
 
-    args.output_dir.mkdir(exist_ok=True, parents=True)
-    output_filename = args.output_dir / f"{args.instance_id}.json"
-    if output_filename.exists():
-        print(f"skip {args.instance_id} because it's already done")
-        exit(0)
+    if args.output_dir is not None:
+        args.output_dir.mkdir(exist_ok=True, parents=True)
+        output_filename = args.output_dir / f"{args.instance_id}.json"
+        if output_filename.exists():
+            print(f"skip {args.instance_id} because it's already done")
+            exit(0)
 
     instance = get_swe_bench_lite_instance(args.instance_id)
+    run_postfix = f"-{args.output_dir.name}" if args.output_dir is not None else ""
     results = {
-        "model_name_or_path": "refact-dev-gpt35-gpt4",
+        "model_name_or_path": f"refact-dev-{MODEL}{run_postfix}",
         "instance_id": args.instance_id,
         "problem_statement": instance["problem_statement"],
+        "problem_patch": instance["patch"],
     }
 
     try:
@@ -63,8 +108,11 @@ async def main():
     except Exception as e:
         results["error"] = str(e) or traceback.format_exc()
 
-    with open(output_filename, "w") as f:
-        json.dump(results, f, indent=4)
+    if args.output_dir is not None:
+        with open(output_filename, "w") as f:
+            json.dump(results, f, indent=4)
+    else:
+        print(json.dumps(results, indent=4))
 
     return results
 
