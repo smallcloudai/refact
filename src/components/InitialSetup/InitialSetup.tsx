79cfc4bda11022d2ed93ae630909ef0af285e363
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2024-07-18 19:12:13 +0200

    fix: no buttons inside of other buttons in InitialSetup

--- src/components/InitialSetup/InitialSetup.tsx
+++ src/components/InitialSetup/InitialSetup.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, RadioCards, RadioGroup, Text } from "@radix-ui/themes";
+import { Button, Flex, Radio, RadioCards, Text } from "@radix-ui/themes";
 import { useState } from "react";
 
 export type Host = "cloud" | "self-hosting" | "enterprise";
@@ -19,62 +19,63 @@ export const InitialSetup: React.FC<InitialSetupProps> = ({
   return (
     <Flex direction="column" gap="2" maxWidth="540px" m="8px">
       <Text size="4">Refact plugin initial setup:</Text>
-      <RadioCards.Root value={selected} onValueChange={onValueChange}>
-        <RadioGroup.Root
-          style={{ gap: 10 }}
-          value={selected}
-          onValueChange={onValueChange}
+      <RadioCards.Root
+        style={{ display: "flex", flexDirection: "column", gap: "6px" }}
+        value={selected}
+        onValueChange={onValueChange}
+      >
+        <RadioCards.Item
+          value="cloud"
+          style={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 0,
+          }}
         >
-          <RadioCards.Item
-            value="cloud"
-            style={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-              gap: 0,
-            }}
-          >
-            <RadioGroup.Item value="cloud">
-              <Text size="3">Cloud</Text>
-            </RadioGroup.Item>
-            <Text size="2">- Easy to start</Text>
-            <Text size="2">- Free tier</Text>
-            <Text size="2">
-              - You can opt-in for code snippets collection to help this open
-              source project, off by default
-            </Text>
-          </RadioCards.Item>
-          <RadioCards.Item
-            value="self-hosting"
-            style={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-              gap: 0,
-            }}
-          >
-            <RadioGroup.Item value="self-hosting">
-              <Text size="3">Self-hosting</Text>
-            </RadioGroup.Item>
-            <Text size="2">- Uses your own server</Text>
-            <Text size="2">- Your code never leaves your control</Text>
-          </RadioCards.Item>
-          <RadioCards.Item
-            value="enterprise"
-            style={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-              gap: 0,
-            }}
-          >
-            <RadioGroup.Item value="enterprise">
-              <Text size="3">Enterprise</Text>
-            </RadioGroup.Item>
-            <Text size="2">{"- Doesn't connect to a public cloud"}</Text>
-            <Text size="2">- Uses your private server only</Text>
-            <Text size="2">
-              - Sends telemetry and code snippets to your private server
-            </Text>
-          </RadioCards.Item>
-        </RadioGroup.Root>
+          <Flex gap="6px">
+            <Radio value="cloud" checked={selected === "cloud"} />
+            <Text size="3">Cloud</Text>
+          </Flex>
+          <Text size="2">- Easy to start</Text>
+          <Text size="2">- Free tier</Text>
+          <Text size="2">
+            - You can opt-in for code snippets collection to help this open
+            source project, off by default
+          </Text>
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="self-hosting"
+          style={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 0,
+          }}
+        >
+          <Flex gap="6px">
+            <Radio value="self-hosting" checked={selected === "self-hosting"} />
+            <Text size="3">Self-hosting</Text>
+          </Flex>
+          <Text size="2">- Uses your own server</Text>
+          <Text size="2">- Your code never leaves your control</Text>
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="enterprise"
+          style={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 0,
+          }}
+        >
+          <Flex gap="6px">
+            <Radio value="enterprise" checked={selected === "enterprise"} />
+            <Text size="3">Enterprise</Text>
+          </Flex>
+          <Text size="2">{"- Doesn't connect to a public cloud"}</Text>
+          <Text size="2">- Uses your private server only</Text>
+          <Text size="2">
+            - Sends telemetry and code snippets to your private server
+          </Text>
+        </RadioCards.Item>
       </RadioCards.Root>
       <Button
         variant="outline"
