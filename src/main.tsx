506ddf5ddaf5189d35f4bdd299c9f8f5617403bd
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-07-22 15:15:15 +0200

    ci: fix `main.tsx` change renderApp to render

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@
  * Only used by the dev server
  */
 
-import { renderApp } from "./lib";
+import { render } from "./lib";
 
 const element = document.getElementById("refact-chat");
 
 if (element) {
-  renderApp(element, {
+  render(element, {
     host: "web",
     features: { statistics: false, vecdb: true, ast: true },
   });
