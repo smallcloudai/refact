66f208fe119d99e212cf1ae675d696c564a22d21
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2024-07-24 15:15:19 +0200

    feat: changing max depth and max pages

--- src/features/Documentation.tsx
+++ src/features/Documentation.tsx
@@ -10,9 +10,39 @@ import {
   DOCUMENTATION_REMOVE,
 } from "../services/refact/consts";
 
+type DocListResponse = {
+  url: string;
+  max_depth: number;
+  max_pages: number;
+  pages: Record<string, string>;
+};
+
+function isArray(object: unknown): object is unknown[] {
+  return Array.isArray(object);
+}
+
+function isDocListResponse(arr: unknown): arr is DocListResponse[] {
+  if (!arr) return false;
+  if (!isArray(arr)) return false;
+  for (const x of arr) {
+    if (!x) return false;
+    if (typeof x !== "object") return false;
+    if (!("url" in x)) return false;
+    if (!("max_depth" in x)) return false;
+    if (!("max_pages" in x)) return false;
+    if (!("pages" in x)) return false;
+    const { url, max_depth, max_pages, pages } = x;
+    if (typeof url !== "string") return false;
+    if (typeof max_depth !== "number") return false;
+    if (typeof max_pages !== "number") return false;
+    if (typeof pages !== "object") return false;
+  }
+  return true;
+}
+
 async function fetchDocumentationList(
   lspUrl: string | undefined,
-): Promise<string[]> {
+): Promise<DocumentationSource[]> {
   const docsEndpoint = lspUrl
     ? `${lspUrl.replace(/\/*$/, "")}${DOCUMENTATION_LIST}`
     : DOCUMENTATION_LIST;
@@ -31,11 +61,18 @@ async function fetchDocumentationList(
 
   const json: unknown = await response.json();
 
-  if (!Array.isArray(json)) {
+  if (!isDocListResponse(json)) {
     return [];
   }
 
-  return json as string[];
+  return json.map((x) => {
+    return {
+      url: x.url,
+      maxDepth: x.max_depth,
+      maxPages: x.max_pages,
+      pages: Object.keys(x.pages).length,
+    };
+  });
 }
 
 export const Documentation: React.FC<{ goBack?: () => void }> = () => {
@@ -46,23 +83,18 @@ export const Documentation: React.FC<{ goBack?: () => void }> = () => {
 
   const refetch = useCallback(async () => {
     const docSources = await fetchDocumentationList(lspUrl);
-    setDocumentationSources(
-      docSources.map((source) => {
-        return {
-          url: source,
-          maxDepth: 2,
-          maxPages: 50,
-          pages: 1,
-        };
-      }),
-    );
+    setDocumentationSources(docSources);
   }, [lspUrl]);
 
   useEffect(() => {
     void refetch();
   }, [refetch]);
 
-  const addDocumentation = (url: string) => {
+  const addDocumentation = (
+    url: string,
+    max_depth: number,
+    max_pages: number,
+  ) => {
     const f = async () => {
       const docsEndpoint = lspUrl
         ? `${lspUrl.replace(/\/*$/, "")}${DOCUMENTATION_ADD}`
@@ -70,7 +102,7 @@ export const Documentation: React.FC<{ goBack?: () => void }> = () => {
 
       const response = await fetch(docsEndpoint, {
         method: "POST",
-        body: JSON.stringify({ source: url }),
+        body: JSON.stringify({ source: url, max_depth, max_pages }),
       });
 
       if (!response.ok) {
