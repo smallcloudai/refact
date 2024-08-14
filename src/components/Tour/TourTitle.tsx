0f38e140ab621acce8c565040ee6804cddc57455
marcomeijer <marcomeijerm@gmail.com>
2024-08-14 16:25:00 +0200

    style: don't center refact logo

--- src/components/Tour/TourTitle.tsx
+++ src/components/Tour/TourTitle.tsx
@@ -7,14 +7,20 @@ export type TourTitle = {
 
 export function TourTitle({ title }: TourTitle) {
   return (
-    <Flex direction="row" style={{ alignItems: "center" }}>
-      <img src={imgUrl} width={28} height={28} />
+    <Flex direction="row" style={{ alignItems: "flex-start" }}>
+      <img
+        src={imgUrl}
+        width={28}
+        height={28}
+        style={{ marginTop: 5, marginBottom: 5 }}
+      />
       <Text
         style={{
           color: "black",
           fontSize: 16,
           margin: 4,
           paddingRight: 30,
+          alignSelf: "center",
         }}
       >
         {title}
