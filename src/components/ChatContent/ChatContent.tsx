4ebbdba64320e3000ef9e8d7cfd990c9998a43e3
marcomeijer <marcomeijerm@gmail.com>
2024-08-14 16:19:58 +0200

    feat: tip of the day text

--- src/components/ChatContent/ChatContent.tsx
+++ src/components/ChatContent/ChatContent.tsx
@@ -34,7 +34,7 @@ export const TipOfTheDay: React.FC = () => {
     dispatch(next(config));
   }, [dispatch]);
 
-  return <Text>💡 {state.tip}</Text>;
+  return <Text>💡 Tip of the day: {state.tip}</Text>;
 };
 
 const PlaceHolderText: React.FC<{ onClick: () => void }> = ({ onClick }) => {
