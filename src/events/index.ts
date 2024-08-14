f4c9fcc2dab3faa79895fd3835e783525f365449
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-08-14 17:48:45 +0200

    wip: remove open in a new tab button.

--- src/events/index.ts
+++ src/events/index.ts
@@ -19,7 +19,7 @@ export {
 } from "../features/Chat/selectedSnippet";
 export type { FimDebugData } from "../services/refact/fim";
 export type { ChatHistoryItem } from "../features/History/historySlice";
-
+export type { ChatThread } from "../features/Chat/chatThread";
 // TODO: re-exporting from redux seems to break things :/
 export type InitialState = {
   fim: FIMDebugState;
