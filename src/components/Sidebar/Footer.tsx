4fa342e38c17e96b2e8d459cc043ff07d1d0522c
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-08-20 18:56:07 +0200

    ui(sidebar footer): move some of the links to the menu button.

--- src/components/Sidebar/Footer.tsx
+++ src/components/Sidebar/Footer.tsx
@@ -2,25 +2,24 @@ import React, { useCallback } from "react";
 import {
   Flex,
   IconButton,
-  Text,
   Link,
   DropdownMenu,
   LinkProps,
 } from "@radix-ui/themes";
 import {
-  GearIcon,
   ExitIcon,
-  Link2Icon,
-  GitHubLogoIcon,
   DiscordLogoIcon,
+  DotsVerticalIcon,
 } from "@radix-ui/react-icons";
 
 // import { Coin } from "../../images";
 
-import { useConfig } from "../../app/hooks";
-import type { Config } from "../../features/Config/configSlice";
+import { useAppSelector } from "../../app/hooks";
+import { selectHost, type Config } from "../../features/Config/configSlice";
 import { useTourRefs } from "../../features/Tour";
 import { useGetUser, useLogout } from "../../hooks";
+import { Coin } from "../../images/coin";
+import styles from "./sidebar.module.css";
 
 const Logout: React.FC<{
   onClick: React.MouseEventHandler<HTMLAnchorElement>;
@@ -77,31 +76,6 @@ function linkForAccount(host: Config["host"]): string {
   }
 }
 
-const Links: React.FC<{ hasAccount: boolean }> = ({ hasAccount }) => {
-  const { host } = useConfig();
-  const bugUrl = linkForBugReports(host);
-  const accountLink = linkForAccount(host);
-  return (
-    <Text size="1">
-      <Flex gap="2" justify="between">
-        {hasAccount && (
-          <LinkItem href={accountLink}>
-            <Link2Icon width="10px" height="10px" /> Your Account
-          </LinkItem>
-        )}
-
-        <LinkItem href={bugUrl}>
-          <GitHubLogoIcon width="10px" height="10px" /> Report Bug
-        </LinkItem>
-
-        <LinkItem href="https://www.smallcloud.ai/discord">
-          <DiscordLogoIcon width="10px" height="10px" /> Discord
-        </LinkItem>
-      </Flex>
-    </Text>
-  );
-};
-
 export type DropdownNavigationOptions =
   | "fim"
   | "stats"
@@ -115,29 +89,76 @@ type SettingsProps = {
 };
 const Settings: React.FC<SettingsProps> = ({ handleNavigation }) => {
   const refs = useTourRefs();
+  const user = useGetUser();
+  const host = useAppSelector(selectHost);
+
+  const bugUrl = linkForBugReports(host);
+  const accountLink = linkForAccount(host);
 
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
         <IconButton variant="outline" ref={(x) => refs.setMore(x)}>
-          <GearIcon />
+          <DotsVerticalIcon />
         </IconButton>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content>
-        <DropdownMenu.Item onSelect={() => handleNavigation("fim")}>
-          FIM debug
-        </DropdownMenu.Item>
+        {user.data && (
+          <DropdownMenu.Item
+            onSelect={(event) => {
+              event.preventDefault();
+              window.open(accountLink, "_blank");
+            }}
+          >
+            {user.data.account}
+          </DropdownMenu.Item>
+        )}
+
+        {user.data && (
+          <DropdownMenu.Label>
+            <Flex align="center" gap="1">
+              <Coin className={styles.coin} /> {user.data.metering_balance}{" "}
+              coins
+            </Flex>
+          </DropdownMenu.Label>
+        )}
+
         <DropdownMenu.Item onSelect={() => handleNavigation("stats")}>
           Statistics
         </DropdownMenu.Item>
+
+        <DropdownMenu.Separator />
+
+        <DropdownMenu.Item
+          onSelect={(event) => {
+            event.preventDefault();
+            window.open(bugUrl, "_blank");
+          }}
+        >
+          Report a bug
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item onSelect={() => handleNavigation("fim")}>
+          FIM debug
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Separator />
+        {/* TODO: enable these */}
+        {/* <DropdownMenu.Item hidden>Edit customization.yaml</DropdownMenu.Item>
+
+        <DropdownMenu.Item hidden>
+          Edit Bring-Your-Own-Key.yaml
+        </DropdownMenu.Item> */}
+
         <DropdownMenu.Item onSelect={() => handleNavigation("hot keys")}>
           Hot Keys
         </DropdownMenu.Item>
+
         <DropdownMenu.Item onSelect={() => handleNavigation("restart tour")}>
           Restart tour
         </DropdownMenu.Item>
-        <DropdownMenu.Separator />
+
         <DropdownMenu.Item onSelect={() => handleNavigation("settings")}>
           Settings
         </DropdownMenu.Item>
@@ -151,7 +172,6 @@ export type FooterProps = {
 };
 
 export const Footer: React.FC<FooterProps> = ({ handleNavigation }) => {
-  const user = useGetUser();
   const logout = useLogout();
   const handleLogout = useCallback(
     (e: React.MouseEvent<HTMLAnchorElement>) => {
@@ -161,12 +181,16 @@ export const Footer: React.FC<FooterProps> = ({ handleNavigation }) => {
     [logout],
   );
   return (
-    <Flex direction="column" gap="2" flexGrow="1">
-      <Flex justify="between">
-        <Logout onClick={handleLogout} />
+    <Flex direction="column" gap="2" flexGrow="1" justify="center">
+      <Flex justify="between" align="center">
+        <Flex gap="2" direction="column">
+          <Logout onClick={handleLogout} />
+          <LinkItem href="https://www.smallcloud.ai/discord">
+            <DiscordLogoIcon width="10px" height="10px" /> Discord
+          </LinkItem>
+        </Flex>
         <Settings handleNavigation={handleNavigation} />
       </Flex>
-      <Links hasAccount={!!user.data} />
     </Flex>
   );
 };
