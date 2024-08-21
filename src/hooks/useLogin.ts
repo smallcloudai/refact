60d3111f79e6dffd26a5eb7226cff5ba90f3dab6
marcomeijer <marcomeijerm@gmail.com>
2024-08-21 14:10:48 +0200

    feat: selecting your plan

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -6,10 +6,15 @@ import { selectHost, setApiKey } from "../features/Config/configSlice";
 import { useGetUser } from "./useGetUser";
 import { useLogout } from "./useLogout";
 
-const useLoginPolling = (skip: boolean) => {
+export const useLogin = () => {
+  const dispatch = useAppDispatch();
+  const user = useGetUser();
+  const logout = useLogout();
+
+  const [isPollingLogin, setIsPollingLogin] = useState<boolean>(false);
+  const canLogin = !user.data && !isPollingLogin;
   const host = useAppSelector(selectHost);
 
-  // TODO: check if this is reset after logout.
   const newLoginTicket = useMemo(() => {
     return (
       Math.random().toString(36).substring(2, 15) +
@@ -18,42 +23,30 @@ const useLoginPolling = (skip: boolean) => {
     );
   }, []);
 
-  const result = smallCloudApi.useLoginQuery(newLoginTicket, {
-    skip: skip,
-    // refetchOnMountOrArgChange: true,
-    // skipPollingIfUnfocused: true,
+  const loginPollingResult = smallCloudApi.useLoginQuery(newLoginTicket, {
+    skip: canLogin,
   });
 
-  useEffect(() => {
-    if (!skip) {
-      const initUrl = new URL("https://refact.smallcloud.ai/authentication");
-      initUrl.searchParams.set("token", newLoginTicket);
-      initUrl.searchParams.set("utm_source", "plugin");
-      initUrl.searchParams.set("utm_medium", host);
-      initUrl.searchParams.set("utm_campaign", "login");
-      const initUrlString = initUrl.toString();
-      const openUrlMessage: OpenExternalUrl = {
-        type: EVENT_NAMES_FROM_SETUP.OPEN_EXTERNAL_URL,
-        payload: { url: initUrlString },
-      };
-      postMessage(openUrlMessage);
-    }
-  }, [host, newLoginTicket, skip]);
-
-  return result;
-};
-
-export const useLogin = () => {
-  const dispatch = useAppDispatch();
-  const user = useGetUser();
-  const logout = useLogout();
-
-  const [isPollingLogin, setIsPollingLogin] = useState<boolean>(false);
-  const canLogin = !user.data && !isPollingLogin;
-  const loginPollingResult = useLoginPolling(canLogin);
-
-  const loginThroughWeb = useCallback(() => {
+  const loginThroughWeb = useCallback((pro: boolean) => {
     setIsPollingLogin(true);
+    const baseUrl = pro
+      ? "https://refact.smallcloud.ai/pro?sidebar"
+      : "https://refact.smallcloud.ai/authentication";
+    const initUrl = new URL(baseUrl);
+    initUrl.searchParams.set("token", newLoginTicket);
+    initUrl.searchParams.set("utm_source", "plugin");
+    initUrl.searchParams.set("utm_medium", host);
+    initUrl.searchParams.set("utm_campaign", "login");
+    const initUrlString = initUrl.toString();
+    const openUrlMessage: OpenExternalUrl = {
+      type: EVENT_NAMES_FROM_SETUP.OPEN_EXTERNAL_URL,
+      payload: { url: initUrlString },
+    };
+    postMessage(openUrlMessage);
+  }, []);
+
+  const cancelLogin = useCallback(() => {
+    setIsPollingLogin(false);
   }, []);
 
   // TODO: handle errors
@@ -77,6 +70,7 @@ export const useLogin = () => {
     loginWithKey,
     user,
     polling: loginPollingResult,
+    cancelLogin,
     logout,
   };
 };
