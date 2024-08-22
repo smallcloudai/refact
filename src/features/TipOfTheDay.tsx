650fa7c1fd51b3785f733b6ead7877bc841771c3
marcomeijer <marcomeijerm@gmail.com>
2024-08-22 16:31:46 +0200

    fix: only show certain tips in vscode

--- src/features/TipOfTheDay.tsx
+++ src/features/TipOfTheDay.tsx
@@ -1,17 +1,50 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 import type { Config } from "../features/Config/configSlice";
 
-// todo: get shortcuts from settings
-export const tips: string[] = [
-  "Press 'Shift + Enter' to move to a new line in the Chat.",
-  "Need a break from suggestions? You can pause them by clicking the 'Refact' icon in the status bar. The manual completion trigger still works: press [MANUAL_COMPLETION].",
-  "Use the @file <file_name> command to attach a file to the chat context. To add the definition of a symbol to the chat context, use @definition <ClassOrFunctionName>.",
-  "Need to find and fix bugs in your code? Select a piece of code, such as a function, press F1 to open the Toolbox, and write /bugs. It will also work on the whole file, provided it's not too large.",
-  "Looking to edit your code? Select the lines you want to change, press F1, and write /edit <Instructions>.",
-  "Need to explain code? Select the code snippet, press F1, and type /explain",
-  "Need to summarize code? Select the part, press F1, and write /summarize",
-  "Want to create new code? Ask Refact to do it for you: press F1 and write /gen with a task description.",
-  "Make your code shorter: Select the code, press F1, and write /shorter",
+type TipHost = "all" | "vscode";
+
+function matchesHost(tipHost: TipHost, host: string): boolean {
+  return tipHost === "all" || tipHost === host;
+}
+
+export const tips: [TipHost, string][] = [
+  ["all", "Press 'Shift + Enter' to move to a new line in the Chat."],
+  [
+    "all",
+    "Need a break from suggestions? You can pause them by clicking the 'Refact' icon in the status bar. The manual completion trigger still works: press [MANUAL_COMPLETION].",
+  ],
+  [
+    "all",
+    "Use the @file <file_name> command to attach a file to the chat context.",
+  ],
+  [
+    "all",
+    "To add the definition of a symbol to the chat context, use @definition <ClassOrFunctionName>.",
+  ],
+  [
+    "vscode",
+    "Need to find and fix bugs in your code? Select a piece of code, such as a function, press F1 to open the Toolbox, and write /bugs. It will also work on the whole file, provided it's not too large.",
+  ],
+  [
+    "vscode",
+    "Looking to edit your code? Select the lines you want to change, press F1, and write /edit <Instructions>.",
+  ],
+  [
+    "vscode",
+    "Need to explain code? Select the code snippet, press F1, and type /explain",
+  ],
+  [
+    "vscode",
+    "Need to summarize code? Select the part, press F1, and write /summarize",
+  ],
+  [
+    "vscode",
+    "Want to create new code? Ask Refact to do it for you: press F1 and write /gen with a task description.",
+  ],
+  [
+    "vscode",
+    "Make your code shorter: Select the code, press F1, and write /shorter",
+  ],
 ];
 
 export type TipOfTheDayState = {
@@ -66,19 +99,34 @@ export const tipOfTheDayReducer = createReducer<TipOfTheDayState>(
   (builder) => {
     builder.addCase(next, (state, action) => {
       const keyBindings = action.payload.keyBindings;
-      let tip = tips[state.next % tips.length];
+      const host = action.payload.host;
 
-      if (keyBindings?.completeManual !== undefined) {
-        tip = tip.replace("[MANUAL_COMPLETION]", keyBindings.completeManual);
-      } else {
-        tip = tip.replace(
-          "[MANUAL_COMPLETION]",
-          "the key binding for manual completion",
-        );
+      let tip: string | undefined = undefined;
+      let next = state.next;
+
+      while (tip === undefined) {
+        const [tipHost, curTip] = tips[next];
+        next = (next + 1) % tips.length;
+
+        if (!matchesHost(tipHost, host)) {
+          continue;
+        }
+
+        if (keyBindings?.completeManual !== undefined) {
+          tip = curTip.replace(
+            "[MANUAL_COMPLETION]",
+            keyBindings.completeManual,
+          );
+        } else {
+          tip = curTip.replace(
+            "[MANUAL_COMPLETION]",
+            "the key binding for manual completion",
+          );
+        }
       }
 
       return {
-        next: (state.next + 1) % tips.length,
+        next,
         tip,
       };
     });
