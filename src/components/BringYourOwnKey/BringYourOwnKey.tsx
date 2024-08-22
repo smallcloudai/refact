da61892738563fa6ebf6cd3e71a3c960e8d08834
Oleg Klimov <omgtech@gmail.com>
2024-08-22 09:31:55 +0200

    login wizard text 1

--- src/components/BringYourOwnKey/BringYourOwnKey.tsx
+++ src/components/BringYourOwnKey/BringYourOwnKey.tsx
@@ -15,16 +15,32 @@ export const BringYourOwnKey: React.FC<BringYourOwnKeyProps> = ({
 
   return (
     <Flex direction="column" gap="2" maxWidth="540px" m="8px">
-      <Text size="3">Bring Your Own Key</Text>
+      <Text size="4">Bring Your Own Key</Text>
       <Text size="2">
-        Allows you to connect to any OpenAi or Huggingface style server.
+        Allows you to connect to any service that has OpenAI- or
+        HuggingFace-style API.
+      </Text>
+      <Text size="2">
+        Works with llama.cpp, OpenRouter, or almost any other service. You can
+        set up separate endpoints and keys for chat, completion, embedding.
+      </Text>
+      <Text size="2">
+        Please report any problems to the{" "}
+        <a href="https://github.com/smallcloudai/refact-lsp/issues">
+          refact-lsp issues
+        </a>{" "}
+        page. Also, report positive experience to discord.
+      </Text>
+      <Text size="2">
+        The button below creates bring-your-own-key.yaml and opens it in the
+        IDE.
       </Text>
       <Flex gap="2">
         <Button variant="outline" mr="auto" onClick={goBack}>
           {"< Back"}
         </Button>
         <Button ml="auto" type="submit" onClick={onSubmit}>
-          {"Create custom BYOK file"}
+          {"Create BYOK file"}
         </Button>
       </Flex>
     </Flex>
--- src/components/InitialSetup/InitialSetup.tsx
+++ src/components/InitialSetup/InitialSetup.tsx
@@ -22,7 +22,7 @@ export const InitialSetup: React.FC<InitialSetupProps> = ({
 
   return (
     <Flex direction="column" gap="2" maxWidth="540px" m="8px">
-      <Text size="4">Refact plugin initial setup:</Text>
+      <Text size="4">Initial Setup</Text>
       <RadioCards.Root
         style={{ display: "flex", flexDirection: "column", gap: "6px" }}
         value={selected}
@@ -38,29 +38,11 @@ export const InitialSetup: React.FC<InitialSetupProps> = ({
         >
           <Flex gap="6px">
             <Radio value="cloud" checked={selected === "cloud"} />
-            <Text size="3">Cloud</Text>
+            <Text size="3">Refact Cloud</Text>
           </Flex>
           <Text size="2">- Easy to start</Text>
           <Text size="2">- Free tier</Text>
-          <Text size="2">
-            - You can opt-in for code snippets collection to help this open
-            source project, off by default
-          </Text>
-        </RadioCards.Item>
-        <RadioCards.Item
-          value="self-hosting"
-          style={{
-            flexDirection: "column",
-            alignItems: "flex-start",
-            gap: 0,
-          }}
-        >
-          <Flex gap="6px">
-            <Radio value="self-hosting" checked={selected === "self-hosting"} />
-            <Text size="3">Self-hosting</Text>
-          </Flex>
-          <Text size="2">- Uses your own server</Text>
-          <Text size="2">- Your code never leaves your control</Text>
+          <Text size="2">- PRO plan with a great choice of models</Text>
         </RadioCards.Item>
         <RadioCards.Item
           value="enterprise"
@@ -74,11 +56,27 @@ export const InitialSetup: React.FC<InitialSetupProps> = ({
             <Radio value="enterprise" checked={selected === "enterprise"} />
             <Text size="3">Enterprise</Text>
           </Flex>
-          <Text size="2">{"- Doesn't connect to a public cloud"}</Text>
           <Text size="2">- Uses your private server only</Text>
-          <Text size="2">
-            - Sends telemetry and code snippets to your private server
-          </Text>
+          <Text size="2">- Sends telemetry to your private server</Text>
+          <Text size="2">- Fine-tune completion models to your codebase</Text>
+          <Text size="2">- Customize for the entire team</Text>
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="self-hosting"
+          style={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 0,
+          }}
+        >
+          <Flex gap="6px">
+            <Radio value="self-hosting" checked={selected === "self-hosting"} />
+            <Text size="3">Self-Hosting with Refact Server</Text>
+          </Flex>
+          {/* TODO: add link to self-hosting doc */}
+          <Text size="2">- Uses your own server</Text>
+          <Text size="2">- Fine-tune completion models to your codebase</Text>
+          <Text size="2">- Your code never leaves your control</Text>
         </RadioCards.Item>
         <RadioCards.Item
           value="bring-your-own-key"
@@ -93,10 +91,13 @@ export const InitialSetup: React.FC<InitialSetupProps> = ({
               value="bring-your-own-key"
               checked={selected === "bring-your-own-key"}
             />
-            <Text size="3">Bring your own key</Text>
+            <Text size="3">Bring Your Own Key</Text>
           </Flex>
           <Text size="2">
-            - Connect to any OpenAI or HuggingFace style server.
+            - Connect to any OpenAI- or HuggingFace-style server
+          </Text>
+          <Text size="2">
+            - Separate endpoints and keys for chat, completion, embedding
           </Text>
         </RadioCards.Item>
       </RadioCards.Root>
