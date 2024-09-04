000eaf0b81bdc04383653ae977a8540b4f5ea262
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-09-04 15:45:24 +0200

    ci: add timeout for missing context file test.

--- src/__tests__/ChatMissingContentFile.test.tsx
+++ src/__tests__/ChatMissingContentFile.test.tsx
@@ -14,73 +14,82 @@ import { http, HttpResponse } from "msw";
 import { responseStream } from "../__fixtures__/context_response";
 
 describe("Content file issue", () => {
-  test("response from lsp", async () => {
-    server.use(
-      goodCaps,
-      goodPrompts,
-      noTools,
-      noCommandPreview,
-      noCompletions,
-      goodUser,
-    );
+  test(
+    "response from lsp",
+    async () => {
+      server.use(
+        goodCaps,
+        goodPrompts,
+        noTools,
+        noCommandPreview,
+        noCompletions,
+        goodUser,
+      );
 
-    server.use(
-      http.post("http://127.0.0.1:8001/v1/chat", () => {
-        console.log("chat called");
-        return new HttpResponse(responseStream(), {
-          headers: {
-            "Content-Type": "application/json",
-            "Transfer-Encoding": "chunked",
+      server.use(
+        http.post("http://127.0.0.1:8001/v1/chat", () => {
+          return new HttpResponse(responseStream(), {
+            headers: {
+              "Content-Type": "application/json",
+              "Transfer-Encoding": "chunked",
+            },
+          });
+        }),
+      );
+
+      const { user: _user, ...app } = render(<InnerApp />, {
+        preloadedState: {
+          pages: [{ name: "history" }, { name: "chat" }],
+          config: {
+            apiKey: "test",
+            lspPort: 8001,
+            themeProps: {},
+            host: "vscode",
+            addressURL: "Refact",
           },
-        });
-      }),
-    );
-
-    const { user: _user, ...app } = render(<InnerApp />, {
-      preloadedState: {
-        pages: [{ name: "history" }, { name: "chat" }],
-        config: {
-          apiKey: "test",
-          lspPort: 8001,
-          themeProps: {},
-          host: "vscode",
-          addressURL: "Refact",
-        },
-        chat: {
-          thread: {
-            id: "12345",
-            title: "foo",
-            messages: [
-              { role: "user", content: "search" },
-              {
-                role: "assistant",
-                content: "ok",
-                tool_calls: [
-                  { function: { name: "search", arguments: "foo" }, index: 0 },
-                ],
-              },
-            ],
-            model: "test",
+          chat: {
+            thread: {
+              id: "12345",
+              title: "foo",
+              messages: [
+                { role: "user", content: "search" },
+                {
+                  role: "assistant",
+                  content: "ok",
+                  tool_calls: [
+                    {
+                      function: { name: "search", arguments: "foo" },
+                      index: 0,
+                    },
+                  ],
+                },
+              ],
+              model: "test",
+            },
+            prevent_send: false,
+            streaming: false,
+            cache: {},
+            waiting_for_response: false,
+            system_prompt: {},
+            tool_use: "agent",
+            send_immediately: true,
+            error: null,
           },
-          prevent_send: false,
-          streaming: false,
-          cache: {},
-          waiting_for_response: false,
-          system_prompt: {},
-          tool_use: "agent",
-          send_immediately: true,
-          error: null,
         },
-      },
-    });
+      });
 
-    await new Promise((r) => setTimeout(r, 1000));
-    app.debug(app.container, Infinity);
-    // screen.logTestingPlaygroundURL();
-    await waitFor(() => {
-      // seems duplicated :/
-      const elem = app.getAllByText(/📎 main\.rs\.json/i);
-      expect(elem.length).toBeGreaterThan(0);
-    });
-  });
+      // await new Promise((r) => setTimeout(r, 1000));
+      // app.debug(app.container, Infinity);
+      // screen.logTestingPlaygroundURL();
+      await waitFor(
+        () => {
+          // seems duplicated :/
+          const elem = app.getAllByText(/📎 main\.rs\.json/i);
+          expect(elem.length).toBeGreaterThan(0);
+        },
+        { timeout: 10000 },
+      );
+    },
+    { timeout: 10000 },
+  );
 });
