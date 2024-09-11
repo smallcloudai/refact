2f74db1719802b38cc90cb4ee10ac5f2910d7693
alashchev17 <andrew.lashchev15@gmail.com>
2024-09-11 12:05:57 +0200

    fix: made a wrapper around radix link component

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,26 +1,25 @@
 import { FC } from "react";
+import { Link as RadixLink } from "@radix-ui/themes";
 import { useConfig } from "../../hooks";
+import styles from "./Link.module.css";
+import classNames from "classnames";
 
 interface LinkProps {
   href: string;
   children: React.ReactNode;
+  className?: string;
 }
 
-export const Link: FC<LinkProps> = ({ href, children }) => {
+export const Link: FC<LinkProps> = (props) => {
   const config = useConfig();
 
   return (
-    <a
-      href={href}
-      style={{
-        color:
-          config.host === "jetbrains"
-            ? config.themeProps.accentColor
-            : undefined,
-        filter: config.host === "jetbrains" ? "brightness(120%)" : undefined,
-      }}
-    >
-      {children}
-    </a>
+    <RadixLink
+      {...props}
+      className={classNames(
+        { [styles.jetbrains]: config.host === "jetbrains" },
+        props.className,
+      )}
+    />
   );
 };
