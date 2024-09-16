0f8f8b7c62238e43202c159ee35c541748360985
Oleg Klimov <omgtech@gmail.com>
2024-09-16 08:37:37 +0200

    don't panic, rewrite compiled in privacy

--- src/privacy.rs
+++ src/privacy.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use std::path::Path;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use tokio::sync::RwLock as ARwLock;
 use tokio::time::Duration;
 use tokio::fs;
@@ -12,6 +12,7 @@ use tokio::io::AsyncWriteExt;
 use crate::global_context::GlobalContext;
 use crate::privacy_compiled_in::COMPILED_IN_INITIAL_PRIVACY_YAML;
 
+
 #[derive(Debug, PartialEq, PartialOrd)]
 pub enum FilePrivacyLevel {
     Blocked = 0,
@@ -19,11 +20,30 @@ pub enum FilePrivacyLevel {
     AllowToSendAnywhere = 2,
 }
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 pub struct PrivacySettings {
-    pub file_privacy: FilePrivacySettings,
+    pub privacy_rules: FilePrivacySettings,
     #[serde(default = "default_expiry_time", skip)]
     pub expiry_time: u64,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+pub struct FilePrivacySettings {
+    pub only_send_to_servers_I_control: Vec<String>,
+    pub blocked: Vec<String>,
+}
+
+impl Default for PrivacySettings {
+    fn default() -> Self {
+        PrivacySettings {
+            privacy_rules: FilePrivacySettings {
+                blocked: vec!["*".to_string()],
+                only_send_to_servers_I_control: vec![],
+            },
+            expiry_time: default_expiry_time(),
+        }
+    }
 }
 
 const PRIVACY_RELOAD_EACH_N_SECONDS: Duration = Duration::from_secs(10);
@@ -31,14 +51,6 @@ fn default_expiry_time() -> u64 {
     SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() + PRIVACY_RELOAD_EACH_N_SECONDS.as_secs()
 }
 
-#[allow(non_snake_case)]
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
-pub struct FilePrivacySettings {
-    #[serde(default)]
-    pub only_send_to_servers_I_control: Vec<String>,
-    #[serde(default)]
-    pub blocked: Vec<String>,
-}
 
 async fn read_privacy_yaml(path: &Path) -> PrivacySettings
 {
@@ -51,13 +63,13 @@ async fn read_privacy_yaml(path: &Path) -> PrivacySettings
                 }
                 Err(e) => {
                     error!("deserialization of YAML from {} failed: {}, terminating process", path.display(), e);
-                    panic!("Failed to deserialize YAML from {}: {}", path.display(), e);
+                    return PrivacySettings::default();
                 }
             }
         }
         Err(e) => {
             error!("unable to read content from {}: {}, terminating process", path.display(), e);
-            panic!("Failed to read content from {}: {}", path.display(), e);
+            return PrivacySettings::default();
         }
     }
 }
@@ -100,15 +112,16 @@ pub async fn load_privacy_if_needed(gcx: Arc<ARwLock<GlobalContext>>) -> Arc<Pri
 fn any_glob_matches_path(globs: &Vec<String>, path: &Path) -> bool {
     globs.iter().any(|glob| {
         let pattern = Pattern::new(glob).unwrap();
-        pattern.matches_path(path)
+        let matches = pattern.matches_path(path);
+        println!("Checking pattern: {}, Path: {}, Matches: {}", glob, path.display(), matches); // Debugging print
+        matches
     })
 }
-
 fn get_file_privacy_level(privacy_settings: Arc<PrivacySettings>, path: &Path) -> FilePrivacyLevel
 {
-    if any_glob_matches_path(&privacy_settings.file_privacy.blocked, path) {
+    if any_glob_matches_path(&privacy_settings.privacy_rules.blocked, path) {
         FilePrivacyLevel::Blocked
-    } else if any_glob_matches_path(&privacy_settings.file_privacy.only_send_to_servers_I_control, path) {
+    } else if any_glob_matches_path(&privacy_settings.privacy_rules.only_send_to_servers_I_control, path) {
         FilePrivacyLevel::OnlySendToServersIControl
     } else {
         FilePrivacyLevel::AllowToSendAnywhere
@@ -135,9 +148,9 @@ mod tests {
     fn test_privacy_patterns() {
         // Arrange
         let privacy_settings = Arc::new(PrivacySettings {
-            file_privacy: FilePrivacySettings {
-                only_send_to_servers_I_control: vec!["*.cat.txt".to_string(), "*.md".to_string(), "*/.venv/*".to_string(), "**/tests_dir/**/*".to_string()],
-                blocked: vec!["*/make.png".to_string(), "*.txt".to_string()],
+            privacy_rules: FilePrivacySettings {
+                only_send_to_servers_I_control: vec!["*.pem".to_string(), "*/semi_private_dir/*.md".to_string()],
+                blocked: vec!["*.pem".to_string(), "*/secret_dir/*".to_string(), "secret_passwords.txt".to_string()],
             },
             expiry_time: default_expiry_time(),
         });
@@ -145,20 +158,17 @@ mod tests {
         let current_dir = std::env::current_dir().unwrap();
 
         let cases: Vec<(PathBuf, FilePrivacyLevel)> = vec![
-            (current_dir.join("test.txt"), FilePrivacyLevel::Blocked),
-            (current_dir.join("test.cat.txt"), FilePrivacyLevel::Blocked),
-            (current_dir.join("car/steps/make.png"), FilePrivacyLevel::Blocked),
-            (current_dir.join("build/rename.md"), FilePrivacyLevel::OnlySendToServersIControl),
-            (current_dir.join(".venv/bin/activate"), FilePrivacyLevel::OnlySendToServersIControl),
-            (PathBuf::from("/home/user/.venv/bin/activate"), FilePrivacyLevel::OnlySendToServersIControl),
-            (PathBuf::from("/home/user/venv/bin/activate"), FilePrivacyLevel::AllowToSendAnywhere),
-            (current_dir.join("car/steps/load.make.png"), FilePrivacyLevel::AllowToSendAnywhere),
-            (current_dir.join("test.cat.txt.zip"), FilePrivacyLevel::AllowToSendAnywhere),
-            (current_dir.join("car/steps/make.pngs"), FilePrivacyLevel::AllowToSendAnywhere),
-            (current_dir.join("car/tests_dir/cars.rs"), FilePrivacyLevel::OnlySendToServersIControl),
-            (current_dir.join("car/tests_dir/bul/tar/gip.rs"), FilePrivacyLevel::OnlySendToServersIControl),
-            (current_dir.join("tests_dir/.hidden"), FilePrivacyLevel::OnlySendToServersIControl),
-            (current_dir.join("/tests_dir/.hidden"), FilePrivacyLevel::OnlySendToServersIControl),
+            (current_dir.join("secret.pem"), FilePrivacyLevel::Blocked),          // matches both
+            (current_dir.join("somedir/secret.pem"), FilePrivacyLevel::Blocked),  // matches both
+            (current_dir.join("secret.pub"), FilePrivacyLevel::AllowToSendAnywhere),
+            (current_dir.join("secret_passwords.txt"), FilePrivacyLevel::AllowToSendAnywhere),
+            (current_dir.join("secret_passwords.jpeg"), FilePrivacyLevel::AllowToSendAnywhere),
+            (current_dir.join("secret_dir/anything.jpg"), FilePrivacyLevel::Blocked),
+            (current_dir.join("semi_private_dir/wow1.md"), FilePrivacyLevel::OnlySendToServersIControl),
+            (current_dir.join("semi_private_dir/wow1.jpeg"), FilePrivacyLevel::AllowToSendAnywhere),
+            (current_dir.join("1/2/3/semi_private_dir/wow1.md"), FilePrivacyLevel::OnlySendToServersIControl),
+            (current_dir.join("1/2/3/semi_private_dir/4/5/6/wow1.md"), FilePrivacyLevel::OnlySendToServersIControl),
+            (current_dir.join("wow1.md"), FilePrivacyLevel::AllowToSendAnywhere),
         ];
 
         for (path, expected_privacy_level) in cases {
@@ -177,7 +187,7 @@ mod tests {
     #[test]
     fn test_privacy_minimum() {
         let privacy_settings = Arc::new(PrivacySettings {
-            file_privacy: FilePrivacySettings {
+            privacy_rules: FilePrivacySettings {
                 only_send_to_servers_I_control: vec!["*.cat.txt".to_string(), "*.md".to_string(), "*/.venv/*".to_string(), "**/tests_dir/**/*".to_string()],
                 blocked: vec!["*/make.png".to_string(), "*.txt".to_string()],
             },
--- src/privacy_compiled_in.rs
+++ src/privacy_compiled_in.rs
@@ -1,17 +1,22 @@
-pub const COMPILED_IN_INITIAL_PRIVACY_YAML: &str = r#"# Define file access levels based on patterns.
+pub const COMPILED_IN_INITIAL_PRIVACY_YAML: &str = r#"#
+# This config file determines if Refact is allowed to read, index or send a file to remote servers.
+#
+# If you have a syntax error in this file, the refact-lsp process will panic and crash, because it
+# can't be sure which files it can touch.
+#
 # Uses glob patterns: https://en.wikipedia.org/wiki/Glob_(programming)
+#
 # The most restrictive rule applies if a file matches multiple patterns.
 
-file_privacy:
-  only_send_to_servers_I_control:
-    # Files here will only be sent to servers you control (e.g., self-hosted).
-    # You can add specific files or use patterns (e.g., "*.config").
-    - "*.env"
-    - "*.env.*"
+privacy_rules:
+    blocked:
+        - */secret_project1/*            # Don't forget leading */ if you are matching directory names
+        - */secret_project2/*.txt
+        - *.pem
 
-  blocked:
-    # Files listed here are completely blocked from being accessed.
-    - "*.pem"
-    - "*.key"
-    - "*.pub"
+    only_send_to_servers_I_control:      # You can set up which ones you control in bring-your-own-key.yaml, otherwise you control none
+        - "secret_passwords.txt"
+
+
+# See unit tests in privacy.rs for more examples
 "#;
