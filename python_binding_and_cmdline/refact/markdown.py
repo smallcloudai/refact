1219568cb6af0b94923b446299d3f74a8cf26edd
marcomeijer <marcomeijerm@gmail.com>
2024-09-19 15:08:14 +0200

    fix: handling borders in markdown better

--- python_binding_and_cmdline/refact/markdown.py
+++ python_binding_and_cmdline/refact/markdown.py
@@ -2,6 +2,15 @@ from refact.printing import Tokens, Lines
 
 gray = "#252b37"
 
+def is_special_boundary(char: str) -> bool:
+    return char in "*_[](){}:.,;!?-"
+
+def is_word_boundary(text: str, i: int, after: bool = False, l: int = 1) -> bool:
+    if after:
+        return i + l >= len(text) or text[i+l].isspace() or is_special_boundary(text[i+l])
+    else:
+        return i - l < 0 or text[i-1].isspace() or is_special_boundary(text[i-1])
+
 def to_markdown(text: str, width: int) -> Tokens:
     result = []
     last = -1
@@ -41,26 +50,26 @@ def to_markdown(text: str, width: int) -> Tokens:
                 i += 1
 
         # *italic text*
-        elif text[i] == "*" and text[i+1] != "*":
+        elif text[i] == "*" and text[i+1] != "*" and is_word_boundary(text, i, is_italic):
             result.append((get_format(), text[last + 1:i]))
             last = i
             is_italic = not is_italic
 
         # _italic text_
-        elif text[i] == "_" and text[i+1] != "_":
+        elif text[i] == "_" and text[i+1] != "_" and is_word_boundary(text, i, is_italic):
             result.append((get_format(), text[last + 1:i]))
             last = i
             is_italic = not is_italic
 
         # **bold text**
-        elif text[i:i+2] == "**":
+        elif text[i:i+2] == "**" and is_word_boundary(text, i, is_bold, 2):
             result.append((get_format(), text[last + 1:i]))
             i += 1
             last = i
             is_bold = not is_bold
 
         # __bold text__
-        elif text[i:i+2] == "__":
+        elif text[i:i+2] == "__" and is_word_boundary(text, i, is_bold, 2):
             result.append((get_format(), text[last + 1:i]))
             i += 1
             last = i
