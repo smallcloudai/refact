53d41d710a8cab9d9588b6651129df229d8d90bd
marcomeijer <marcomeijerm@gmail.com>
2024-09-19 14:53:26 +0200

    feat: markdown bold and italic text

--- python_binding_and_cmdline/refact/markdown.py
+++ python_binding_and_cmdline/refact/markdown.py
@@ -6,19 +6,65 @@ def to_markdown(text: str, width: int) -> Tokens:
     result = []
     last = -1
     i = 0
+
+    is_bold = False
+    is_italic = False
+    is_inline_code = False
+
+    def get_format():
+        res = []
+        if is_bold:
+            res.append("bold")
+        if is_italic:
+            res.append("italic")
+        if is_inline_code:
+            res.append(f"bg:{gray}")
+        return " ".join(res)
+
+
     while i < len(text):
 
+
         # `text`
-        if text[i] == "`":
-            result.append(("", text[last + 1:i]))
+        if text[i] == "`" and text[i+1] != "`":
+            result.append((get_format(), text[last + 1:i]))
+            if is_inline_code:
+                result.append((gray, ""))
+            else:
+                result.append((gray, ""))
             last = i
-            i += 1
-            while i < len(text) and text[i]!= "`":
+            is_inline_code = not is_inline_code
+
+        # skip all backticks
+        elif text[i] == "`":
+            while text[i] == "`":
                 i += 1
-            result.append((gray, ""))
-            result.append((f"italic bg:{gray}", text[last + 1:i]))
-            result.append((gray, ""))
+
+        # *italic text*
+        elif text[i] == "*" and text[i+1] != "*":
+            result.append((get_format(), text[last + 1:i]))
             last = i
+            is_italic = not is_italic
+
+        # _italic text_
+        elif text[i] == "_" and text[i+1] != "_":
+            result.append((get_format(), text[last + 1:i]))
+            last = i
+            is_italic = not is_italic
+
+        # **bold text**
+        elif text[i:i+2] == "**":
+            result.append((get_format(), text[last + 1:i]))
+            i += 1
+            last = i
+            is_bold = not is_bold
+
+        # __bold text__
+        elif text[i:i+2] == "__":
+            result.append((get_format(), text[last + 1:i]))
+            i += 1
+            last = i
+            is_bold = not is_bold
 
         i += 1
 
