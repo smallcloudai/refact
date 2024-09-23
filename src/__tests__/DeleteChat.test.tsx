83e6dea62395fa314cf23d14307d2c946243a37f
alashchev17 <andrew.lashchev15@gmail.com>
2024-09-23 17:16:59 +0200

    chore: type issues fix inside of tests

--- src/__tests__/DeleteChat.test.tsx
+++ src/__tests__/DeleteChat.test.tsx
@@ -14,6 +14,7 @@ describe("Delete a Chat form history", () => {
         messages: [],
         id: "abc123",
         model: "foo",
+        tool_use: "quick",
         createdAt: now,
         updatedAt: now,
         read: true,
