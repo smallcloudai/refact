0cac22518298b42dd4d5b5096e36db27d3089ffb
JegernOUTT <sergey.vakhreev@gmail.com>
2024-10-02 13:36:44 +0930

    Update prompt pins guidelines to include symbols refactoring notation

--- src/yaml_configs/customization_compiled_in.rs
+++ src/yaml_configs/customization_compiled_in.rs
@@ -19,11 +19,10 @@ PROMPT_DEFAULT: |
 
 PROMPT_PINS: |
   Use triple backquotes for code blocks. The indent in the code blocks you write must be identical to the input indent, ready to paste back into the file.
-  Before any code block, you need to write one of: 📍ADD_TO_FILE, 📍REWRITE_ONE_SYMBOL, 📍REWRITE_WHOLE_FILE, 📍PARTIAL_EDIT, 📍NEW_FILE, 📍OTHER followed by a
+  Before any code block, you need to write one of: 📍REWRITE_ONE_SYMBOL, 📍REWRITE_WHOLE_FILE, 📍PARTIAL_EDIT, 📍NEW_FILE, 📍OTHER followed by a
   unique ticket (3-digit number that you need to start from 000 and increase by one each code block) and the absolute path to the file the
   changes apply to, then write the code block. Explanation:
-  📍ADD_TO_FILE <ticket> </dir/dir/existing_file.ext> [BEFORE/AFTER <namespace::class::method>]      -- add code to a file, optionally point to a good place to add the code
-  📍REWRITE_ONE_SYMBOL <ticket> </dir/dir/existing_file.ext> SYMBOL_NAME <namespace::class::method>  -- when you need to update a single functiton or class
+  📍REWRITE_ONE_SYMBOL <ticket> </dir/dir/existing_file.ext> SYMBOL_NAME <namespace::class::method>  -- when you need to rewrite a single function or class
   📍REWRITE_WHOLE_FILE <ticket> </dir/dir/existing_file.ext>                                         -- when you need to rewrite the whole file
   📍PARTIAL_EDIT <ticket> </dir/dir/existing_file.ext>                                               -- for an edit doesn't start at the top and end at the bottom
   📍NEW_FILE <ticket> </dir/dir/new_file.ext>                  -- create a new file, you need an absolute path here even more than anywhere else
@@ -83,7 +82,7 @@ PROMPT_AGENTIC_TOOLS: |
   to the original request by the user.
 
   Good practice for using patch(): write your changes using 📍-notation first. Then call patch() in parallel for each of the files you want to change,
-  and put all 📍PARTIAL_EDIT tickets you want to apply in a comma-separated list.
+  and put all tickets you want to apply in a comma-separated list.
 
   Thinking strategy for the answers:
 
