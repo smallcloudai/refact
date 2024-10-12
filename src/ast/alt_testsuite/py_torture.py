4225f235785bc6181f132c93a67b2c5158c9114a
Oleg Klimov <omgtech@gmail.com>
2024-10-12 10:25:29 +0200

    ooops mistake in torture.py

--- src/ast/alt_testsuite/py_torture.py
+++ src/ast/alt_testsuite/py_torture.py
@@ -25,12 +25,13 @@ class WobbleNoble:
     def __init__(self):
         self.trouble = "double"
 
+    def do_the_thing(self):
+        print("wobble", self.trouble)
+
+
 class FumbleNoble(object):
     def __init__(self):
         self.humble = "mumble"
-
-    def do_the_thing(self):
-        print("wobble", self.trouble)
 
 
 def wobble_generator(n: int) -> List[Optional[WobbleNoble]]:
