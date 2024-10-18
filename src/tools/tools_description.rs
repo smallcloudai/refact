26e2b9d159b7bb6ba557f9ce814959da59360c23
Sergey Vakhreev <sergey.vakhreev@gmail.com>
2024-10-19 00:46:25 +1030

    'tree' tool descriptions (#338)
    * Add descriptions for 'tree' tool parameters
    
    * Update tree tool description to

--- src/tools/tools_description.rs
+++ src/tools/tools_description.rs
@@ -168,10 +168,10 @@ tools:
     parameters:
       - name: "path"
         type: "string"
-        description: "An optional absolute path to get files tree for a particular folder or file. Do not pass it if you need full project tree."
+        description: "An absolute path to get files tree for. Do not pass it if you need a full project tree."
       - name: "use_ast"
         type: "boolean"
-        description: "if true, for each file an array of AST symbols will appear as well as its filename"
+        description: "If true, for each file an array of AST symbols will appear as well as its filename"
     parameters_required: []
 
   - name: "web"
