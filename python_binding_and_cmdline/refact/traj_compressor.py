480246021759ca1adb9b764554598955927dcd46
Oleg Klimov <omgtech@gmail.com>
2024-11-07 16:13:00 +0100

    CLI improved trajectory compressor

--- python_binding_and_cmdline/refact/traj_compressor.py
+++ python_binding_and_cmdline/refact/traj_compressor.py
@@ -8,26 +8,33 @@ from refact import chat_client, cli_main, cli_settings, cli_export
 QUESTION = """
 Compress the chat above.
 
-Some guidelines:
+Guidelines:
 
 1. Always prefer specifics over generic phrases. Write file names, symbol names, folder names, actions, facts, user attitude
 towards entities in the project. If something is junk according to the user, that's the first priority to remember.
 2. The first message in the chat is the goal. Summarize it up to 15 words, always prefer specifics.
 3. The most important part is decision making by assistant. What new information assistant has learned? Skip the plans,
-fluff, explanations for the user. Keep the line from evidence to decision when summarizing the actions of the assistant.
-Write just one sentense. Prefer specifics and facts.
+fluff, explanations for the user. Write one sentense: the evidence (specifics and facts), the thought process, motivated decision.
 4. Each tool call should be a separate record. Write all the parameters. Summarize facts about output of a tool, especially the facts
-useful for the goal.
-5. Skip unsuccesful calls that are later corrected, doesn't matter.
-7. The last line is outcome, pick SUCCESS/FAIL/PROGRESS
+useful for the goal, what the assistant learned, what was surprising to see?
+5. Each 📍-ticket should become a separate record, starts with "coding". The second starts with pin emoji and ticket number (but don't
+repeat all caps action), facts how the new code is different, in less than 15 words.
+6. Skip unsuccesful calls that are later corrected. Keep the corrected one.
+7. When writing paths to files, only output short relative paths from the project dir.
+8. The last line is the outcome, pick SUCCESS/FAIL/PROGRESS
 
-Output format:
+Output format is list of tuples, each tuple is has:
+EITHER (1) call with all parameters, maybe shortened, but all parameters, (2) explanation of significance of tool output
+OR     (1) goal/thinking/coding/outcome (2) string according to the guidelines
+
+Example:
 
 [
 ["goal", "rename my_function1 to my_function2"],
 ["thinking", "there are definition(), search() and locate() tools, all can be used to find my_function1, system prompt says I need to start with locate()"],
 ["locate(problem_statement=\"rename my_function1 to my_function2\")", "file my_script.py (1337 lines) has my_function1 on line 42"],
 ["thinking", "I can rewrite my_function1 inside my_script.py using 📍-notation, so I'll do that"],
+["coding", "📍000 replace my_function1, my new version has the new name"],
 ["patch(path=\"my_script\", tickets=\"000\")", "The output of patch() has 15 lines_add and 15 lines_remove, confirming the operation"],
 ["outcome", "SUCCESS"]
 ]
