06d268c35712045de50bf7efa1406b780b630007
JegernOUTT <sergey.vakhreev@gmail.com>
2024-11-26 23:59:00 +1030

    add completion support for the passthrough models

--- refact_known_models/passthrough.py
+++ refact_known_models/passthrough.py
@@ -10,7 +10,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 5_000,
         "pp1000t_generated": 15_000,  # $15.00 / 1M tokens (2024 may)
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "gpt-4-turbo": {
         "backend": "litellm",
@@ -21,7 +21,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 10_000,
         "pp1000t_generated": 30_000,  # $30.00 / 1M tokens (2024 may)
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "gpt-4": {
         "backend": "litellm",
@@ -32,7 +32,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 10_000,
         "pp1000t_generated": 30_000,
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "gpt-3.5-turbo": {
         "backend": "litellm",
@@ -43,7 +43,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 1000,
         "pp1000t_generated": 2000,
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "claude-3-5-sonnet": {
         "backend": "litellm",
@@ -54,7 +54,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 3_000,  # $3.00 / 1M tokens (2024 jun)
         "pp1000t_generated": 15_000,  # $15.00 / 1M tokens (2024 jun)
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "claude-3-haiku": {
         "backend": "litellm",
@@ -65,7 +65,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 250,
         "pp1000t_generated": 1_250,
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "claude-3-opus": {
         "backend": "litellm",
@@ -76,7 +76,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 15_000,
         "pp1000t_generated": 75_000,
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "claude-3-sonnet": {
         "backend": "litellm",
@@ -87,7 +87,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 3_000,
         "pp1000t_generated": 15_000,
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "gpt-4o-2024-05-13": {
         "backend": "litellm",
@@ -98,7 +98,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 5_000,
         "pp1000t_generated": 15_000,  # $15.00 / 1M tokens
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "gpt-4o-2024-08-06": {
         "backend": "litellm",
@@ -109,7 +109,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 2_500,
         "pp1000t_generated": 10_000,  # $15.00 / 1M tokens
-        "filter_caps": ["chat", "tools"]
+        "filter_caps": ["chat", "tools", "completion"]
     },
     "gpt-4o-mini": {
         "backend": "litellm",
@@ -120,7 +120,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # $0.60 / 1M tokens
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "claude-3-5-sonnet-20241022": {
         "backend": "litellm",
@@ -131,7 +131,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 3_000,  # $3.00 / 1M tokens (2024 oct)
         "pp1000t_generated": 15_000,  # $15.00 / 1M tokens (2024 oct)
-        "filter_caps": ["chat", "tools"],
+        "filter_caps": ["chat", "tools", "completion"],
     },
     "groq-llama-3.1-8b": {
         "backend": "litellm",
@@ -142,7 +142,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "groq-llama-3.1-70b": {
         "backend": "litellm",
@@ -153,7 +153,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "groq-llama-3.2-1b": {
         "backend": "litellm",
@@ -164,7 +164,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "groq-llama-3.2-3b": {
         "backend": "litellm",
@@ -175,7 +175,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "groq-llama-3.2-11b-vision": {
         "backend": "litellm",
@@ -186,7 +186,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "groq-llama-3.2-90b-vision": {
         "backend": "litellm",
@@ -197,7 +197,7 @@ passthrough_mini_db = {
         "T_out": 8000,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "cerebras-llama3.1-8b": {
         "backend": "litellm",
@@ -208,7 +208,7 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     },
     "cerebras-llama3.1-70b": {
         "backend": "litellm",
@@ -219,6 +219,6 @@ passthrough_mini_db = {
         "T_out": 4096,
         "pp1000t_prompt": 150,
         "pp1000t_generated": 600,  # TODO: don't know the price
-        "filter_caps": ["chat"],
+        "filter_caps": ["chat", "completion"],
     }
 }
