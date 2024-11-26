96275d210dba6355ea513664e8492e1e77127436
Oleg Klimov <omgtech@gmail.com>
2024-11-26 09:02:21 +0100

    string_short, string_long, string_multiline

--- src/integrations/integr_postgres.rs
+++ src/integrations/integr_postgres.rs
@@ -175,27 +175,27 @@ impl Tool for ToolPostgres {
 pub const POSTGRES_INTEGRATION_SCHEMA: &str = r#"
 fields:
   host:
-    f_type: string
+    f_type: string_long
     f_desc: "Connect to this host, for example 127.0.0.1 or docker container name."
     f_placeholder: marketing_db_container
   port:
-    f_type: int
+    f_type: string_short
     f_desc: "Which port to use."
     f_default: "5432"
   user:
-    f_type: string
+    f_type: string_short
     f_placeholder: john_doe
   password:
-    f_type: string
+    f_type: string_short
     f_default: "$POSTGRES_PASSWORD"
     smartlinks:
       - sl_label: "Open passwords.yaml"
         sl_goto: "EDITOR:passwords.yaml"
   database:
-    f_type: string
+    f_type: string_short
     f_placeholder: marketing_db
   psql_binary_path:
-    f_type: string
+    f_type: string_long
     f_desc: "If it can't find a path to `psql` you can provide it here, leave blank if not sure."
     f_placeholder: "psql"
 available:
