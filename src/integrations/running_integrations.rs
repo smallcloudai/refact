7795364c51804ef01bfd2c583992eb8217a53ac3
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-12-05 22:40:01 +0100

    feat: load integrations apart from tools before upgrading, integrations may not need to be upgraded to tool

--- src/integrations/running_integrations.rs
+++ src/integrations/running_integrations.rs
@@ -6,13 +6,28 @@ use tokio::sync::Mutex as AMutex;
 
 use crate::tools::tools_description::Tool;
 use crate::global_context::GlobalContext;
-
+use crate::integrations::integr_abstract::IntegrationTrait;
 
 pub async fn load_integration_tools(
     gcx: Arc<ARwLock<GlobalContext>>,
     _current_project: String,
     _allow_experimental: bool,
 ) -> IndexMap<String, Arc<AMutex<Box<dyn Tool + Send>>>> {
+    let integraions_map = load_integrations(gcx.clone(), _current_project, _allow_experimental).await;
+    let mut tools = IndexMap::new();
+    for (name, integr) in integraions_map {
+        if integr.can_upgrade_to_tool() {
+            tools.insert(name.clone(), Arc::new(AMutex::new(integr.integr_upgrade_to_tool(&name))));
+        }
+    }
+    tools
+}
+
+pub async fn load_integrations(
+    gcx: Arc<ARwLock<GlobalContext>>,
+    _current_project: String,
+    _allow_experimental: bool,
+) -> IndexMap<String, Box<dyn IntegrationTrait + Send + Sync>> {
     // XXX filter _workspace_folders_arc that fit _current_project
     let (config_dirs, global_config_dir) = crate::integrations::setting_up_integrations::get_config_dirs(gcx.clone()).await;
     let integrations_yaml_path = crate::integrations::setting_up_integrations::get_integrations_yaml_path(gcx.clone()).await;
@@ -22,7 +37,7 @@ pub async fn load_integration_tools(
     let vars_for_replacements = crate::integrations::setting_up_integrations::get_vars_for_replacements(gcx.clone()).await;
     let records = crate::integrations::setting_up_integrations::read_integrations_d(&config_dirs, &global_config_dir, &integrations_yaml_path, &vars_for_replacements, &lst, &mut error_log);
 
-    let mut tools = IndexMap::new();
+    let mut integrations_map = IndexMap::new();
     for rec in records {
         if !rec.on_your_laptop {
             continue;
@@ -41,7 +56,7 @@ pub async fn load_integration_tools(
         if should_be_fine.is_err() {
             tracing::error!("failed to apply settings for integration {}: {:?}", rec.integr_name, should_be_fine.err());
         }
-        tools.insert(rec.integr_name.clone(), Arc::new(AMutex::new(integr.integr_upgrade_to_tool(&rec.integr_name))));
+        integrations_map.insert(rec.integr_name.clone(), integr);
     }
 
     for e in error_log {
@@ -53,5 +68,5 @@ pub async fn load_integration_tools(
         );
     }
 
-    tools
+    integrations_map
 }
