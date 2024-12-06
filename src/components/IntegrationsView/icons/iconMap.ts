40ec5a93cb05df755c9670efcf0dda9c4931451d
Nick Frolov <nikolai.frolov@gmail.com>
2024-12-06 14:15:51 +0100

    add icon for mysql

--- src/components/IntegrationsView/icons/iconMap.ts
+++ src/components/IntegrationsView/icons/iconMap.ts
@@ -5,10 +5,12 @@ import chromeIcon from "./chrome.png";
 import pdbIcon from "./pdb.png";
 import githubIcon from "./github.png";
 import gitlabIcon from "./gitlab.png";
+import mysqlIcon from "./mysql.png";
 
 export const iconMap: Record<string, string> = {
   cmdline: terminalIcon,
   postgres: postgresIcon,
+  mysql: mysqlIcon,
   docker: dockerIcon,
   chrome: chromeIcon,
   pdb: pdbIcon,
