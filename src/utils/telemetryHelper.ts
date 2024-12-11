29ae85a3980b699f4ef7aba620c5720feaf6b532
Kirill Starkov <starkov.kirill123@gmail.com>
2024-12-11 18:51:58 +0800

    add net telemetry

--- src/utils/telemetryHelper.ts
+++ src/utils/telemetryHelper.ts
@@ -1,4 +1,5 @@
 const TELEMETRY_CHAT_PATH = "/v1/telemetry-chat";
+const TELEMETRY_NET_PATH = "/v1/telemetry-network";
 
 export function sendTelemetryEvent({
   port = 8001,
@@ -21,6 +22,47 @@ export function sendTelemetryEvent({
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      scope,
+      success,
+      error_message,
+    }),
+  }).catch((error: { message: string }) => {
+    // todo make any error stringified
+    sendTelemetryNetworkEvent({
+      port,
+      relativeUrl: TELEMETRY_CHAT_PATH,
+      scope,
+      success: false,
+      error_message: error.message,
+    });
+  });
+}
+
+export function sendTelemetryNetworkEvent({
+  port = 8001,
+  relativeUrl,
+  scope,
+  success,
+  error_message,
+}: {
+  port?: number;
+  relativeUrl: string;
+  scope: string;
+  success: boolean;
+  error_message: string;
+}) {
+  const url = `http://127.0.0.1:${port}${TELEMETRY_NET_PATH}`;
+  if (error_message.length > 200) {
+    error_message = error_message.substring(0, 200) + "...";
+  }
+  void fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: relativeUrl,
+
       scope,
       success,
       error_message,
