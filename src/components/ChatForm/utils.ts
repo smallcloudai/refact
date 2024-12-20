87ebc9553768ff5a50b9a2b702d3836c8ad201d0
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-12-20 17:07:03 +0100

    chore(search workspace): remove the checkbox

--- src/components/ChatForm/utils.ts
+++ src/components/ChatForm/utils.ts
@@ -17,29 +17,8 @@ export function addCheckboxValuesToInput(
     result = `${checkboxes.selected_lines.value ?? ""}\n` + result;
   }
 
-  // TODO: remove these if it's no longer a feature
-  // if (checkboxes.use_memory.checked && checkboxes.use_memory.hide !== true) {
-  //   result = `@local-notes-to-self\n` + result;
-  // }
-
-  // if (
-  //   checkboxes.lookup_symbols.checked &&
-  //   checkboxes.lookup_symbols.hide !== true
-  // ) {
-  //   result = `@symbols-at ${checkboxes.lookup_symbols.value ?? ""}\n` + result;
-  // }
-
   if (checkboxes.file_upload.checked && checkboxes.file_upload.hide !== true) {
     result = `@file ${checkboxes.file_upload.value ?? ""}\n` + result;
-    // const cmd = vecdb ? "@file-search" : "@file";
-    // result = `${cmd} ${checkboxes.file_upload.value ?? ""}\n` + result;
-  }
-
-  if (
-    checkboxes.search_workspace.checked &&
-    checkboxes.search_workspace.hide !== true
-  ) {
-    result = `@workspace\n` + result;
   }
 
   if (!result.endsWith("\n")) {
