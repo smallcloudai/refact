a94041bf882d0ed794f3610a09c257ba5b8792f2
Oleg Klimov <omgtech@gmail.com>
2024-12-23 11:13:15 +0100

    Confirmation Rules Text

--- src/components/IntegrationsView/Confirmation/Confirmation.tsx
+++ src/components/IntegrationsView/Confirmation/Confirmation.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 import { ToolConfirmation } from "../../../services/refact";
-import { Flex, Heading } from "@radix-ui/themes";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import { ConfirmationTable } from "../IntegrationsTable/ConfirmationTable";
 
 type ConfirmationProps = {
@@ -15,8 +15,13 @@ export const Confirmation: FC<ConfirmationProps> = ({
   return (
     <Flex direction="column" width="100%" gap="4">
       <Heading as="h4" size="4">
-        Setup rules for your integration
+        Confirmation Rules
       </Heading>
+      <Text>
+        Some commands might have destructive effects, here you can set up a list
+        of patterns such that if a command matches one, you&apos;ll see a
+        confirmation request or the command will be blocked completely.
+      </Text>
       {Object.entries(confirmationObject).map(([key, values]) => (
         <ConfirmationTable
           key={key}
