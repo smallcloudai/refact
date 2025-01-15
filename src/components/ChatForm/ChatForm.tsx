4b3ddcfba4633b0befd087ed434df4e76324979b
alashchev17 <andrew.lashchev15@gmail.com>
2025-01-15 17:46:19 +0100

    fix: resetting prevent send on chat restoration

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -34,6 +34,8 @@ import { getPauseReasonsWithPauseStatus } from "../../features/ToolConfirmation/
 import { AttachFileButton, FileList } from "../Dropzone";
 import { useAttachedImages } from "../../hooks/useAttachedImages";
 import {
+  enableSend,
+  selectChatId,
   selectIsStreaming,
   selectIsWaiting,
   selectMessages,
@@ -64,6 +66,8 @@ export const ChatForm: React.FC<ChatFormProps> = ({
   const onClearError = useCallback(() => dispatch(clearError()), [dispatch]);
   const { disableInput } = useAgentUsage();
   const isOnline = useIsOnline();
+
+  const chatId = useAppSelector(selectChatId);
   const messages = useAppSelector(selectMessages);
   const preventSend = useAppSelector(selectPreventSend);
 
@@ -191,6 +195,13 @@ export const ChatForm: React.FC<ChatFormProps> = ({
     },
     [handleHelpInfo, setValue, setFileInteracted, setLineSelectionInteracted],
   );
+
+  useEffect(() => {
+    // this use effect is required to reset preventSend when chat was restored
+    if (preventSend && !isStreaming && !isWaiting && isOnline) {
+      dispatch(enableSend({ id: chatId }));
+    }
+  }, [dispatch, isOnline, isWaiting, isStreaming, preventSend, chatId]);
 
   useEffect(() => {
     if (isSendImmediately && !isWaiting && !isStreaming) {
