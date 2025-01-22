87b59db621df2477880c466adc5852c2427492e4
alashchev17 <andrew.lashchev15@gmail.com>
2025-01-22 16:14:59 +0100

    wip: updated skip condition for fetching user

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -2,11 +2,17 @@ import { useAppSelector } from "./useAppSelector";
 import { selectAddressURL, selectApiKey } from "../features/Config/configSlice";
 import { smallCloudApi } from "../services/smallcloud";
 
+const NOT_SKIPPABLE_ADDRESS_URLS = [
+  "Refact",
+  "https://inference-backup.smallcloud.ai",
+];
+
 export const useGetUser = () => {
   const addressURL = useAppSelector(selectAddressURL);
+  console.log(`[DEBUG]: addressURL: `, addressURL);
   const maybeApiKey = useAppSelector(selectApiKey);
   const apiKey = maybeApiKey ?? "";
   return smallCloudApi.useGetUserQuery(apiKey, {
-    skip: !maybeApiKey || addressURL !== "Refact",
+    skip: !NOT_SKIPPABLE_ADDRESS_URLS.includes(addressURL ?? ""),
   });
 };
