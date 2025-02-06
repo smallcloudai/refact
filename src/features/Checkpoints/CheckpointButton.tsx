7209e67832b7a470c061900f9416219d5558c705
alashchev17 <andrew.lashchev15@gmail.com>
2025-02-06 18:08:55 +0100

    fix: disable revert checkpoints button if waiting, streaming or not online

--- src/features/Checkpoints/CheckpointButton.tsx
+++ src/features/Checkpoints/CheckpointButton.tsx
@@ -2,6 +2,8 @@ import { ResetIcon } from "@radix-ui/react-icons";
 import { IconButton } from "@radix-ui/themes";
 import { Checkpoint } from "./types";
 import { useCheckpoints } from "../../hooks/useCheckpoints";
+import { useAppSelector, useIsOnline } from "../../hooks";
+import { selectIsStreaming, selectIsWaiting } from "../Chat";
 
 type CheckpointButtonProps = {
   checkpoints: Checkpoint[] | null;
@@ -12,7 +14,12 @@ export const CheckpointButton = ({
   checkpoints,
   messageIndex,
 }: CheckpointButtonProps) => {
+  const isStreaming = useAppSelector(selectIsStreaming);
+  const isWaiting = useAppSelector(selectIsWaiting);
+  const isOnline = useIsOnline();
+
   const { handleRestore, isLoading } = useCheckpoints();
+
   return (
     <IconButton
       size="2"
@@ -20,6 +27,7 @@ export const CheckpointButton = ({
       title={isLoading ? "Reverting..." : "Revert agent changes"}
       onClick={() => void handleRestore(checkpoints, messageIndex)}
       loading={isLoading}
+      disabled={!isOnline || isStreaming || isWaiting}
     >
       <ResetIcon />
     </IconButton>
