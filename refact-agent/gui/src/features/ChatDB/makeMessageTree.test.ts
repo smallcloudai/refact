dc2c2742e275bfeacb8ef8d56455d1288271aeb3
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-02-20 02:45:34 +0100

    refactoring(message trie): yeet code that doesn't make a trie.

--- refact-agent/gui/src/features/ChatDB/makeMessageTree.test.ts
+++ refact-agent/gui/src/features/ChatDB/makeMessageTree.test.ts
@@ -1,7 +1,8 @@
 import { expect, describe, test } from "vitest";
-import { ChatMessage, CMessage } from "../../services/refact";
+import { CMessage } from "../../services/refact";
 import { CMessageNode } from "./chatDbMessagesSlice";
-import { partition } from "../../utils";
+import { makeMessageTree } from "./makeMessageTrie";
+
 const STUB: CMessage[] = [
   {
     cmessage_belongs_to_cthread_id: "test13thread1739988322_2",
@@ -352,38 +353,3 @@ describe("makeMessageTree", () => {
     expect(tree).toEqual(expected);
   });
 });
-
-const isRoot = (message: CMessage): boolean => {
-  return message.cmessage_prev_alt === -1;
-};
-
-const makeMessageTree = (messages: CMessage[]): CMessageNode | null => {
-  const sortedMessages = messages
-    .slice()
-    .sort((a, b) => a.cmessage_num - b.cmessage_num);
-
-  const [nodes, roots] = partition(sortedMessages, isRoot);
-  if (roots.length === 0) return null;
-  // TODO: handle multiple roots;
-  const root = roots[0];
-  const children = getChildren(root, nodes);
-  return {
-    message: root,
-    children,
-  };
-};
-
-function getChildren(parent: CMessage, messages: CMessage[]): CMessageNode[] {
-  if (messages.length === 0) return [];
-  const rowNumber = parent.cmessage_num + 1;
-  const [other, siblings] = partition(messages, (m) => {
-    return (
-      m.cmessage_num === rowNumber &&
-      m.cmessage_prev_alt === parent.cmessage_alt
-    );
-  });
-
-  return siblings.map((s) => {
-    return { message: s, children: getChildren(s, other) };
-  });
-}
---
+++ refact-agent/gui/src/features/ChatDB/makeMessageTrie.ts
@@ -0,0 +1,38 @@
+import { CMessage } from "../../services/refact";
+import { CMessageNode } from "./chatDbMessagesSlice";
+import { partition } from "../../utils";
+
+const isRoot = (message: CMessage): boolean => {
+  return message.cmessage_prev_alt === -1;
+};
+
+export const makeMessageTree = (messages: CMessage[]): CMessageNode | null => {
+  const sortedMessages = messages
+    .slice()
+    .sort((a, b) => a.cmessage_num - b.cmessage_num);
+
+  const [nodes, roots] = partition(sortedMessages, isRoot);
+  if (roots.length === 0) return null;
+  // TODO: handle multiple roots;
+  const root = roots[0];
+  const children = getChildren(root, nodes);
+  return {
+    message: root,
+    children,
+  };
+};
+
+function getChildren(parent: CMessage, messages: CMessage[]): CMessageNode[] {
+  if (messages.length === 0) return [];
+  const rowNumber = parent.cmessage_num + 1;
+  const [other, siblings] = partition(messages, (m) => {
+    return (
+      m.cmessage_num === rowNumber &&
+      m.cmessage_prev_alt === parent.cmessage_alt
+    );
+  });
+
+  return siblings.map((s) => {
+    return { message: s, children: getChildren(s, other) };
+  });
+}
