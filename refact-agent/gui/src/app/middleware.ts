cb5643242541239546385f057cd3e6eb47d5f48c
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-02-24 13:56:47 +0100

    fix(acceot / reject): prevent sending message no rejection.

--- refact-agent/gui/src/app/middleware.ts
+++ refact-agent/gui/src/app/middleware.ts
@@ -12,6 +12,7 @@ import {
   newIntegrationChat,
   chatResponse,
   setIsWaitingForResponse,
+  upsertToolCall,
 } from "../features/Chat/Thread";
 import { statisticsApi } from "../services/refact/statistics";
 import { integrationsApi } from "../services/refact/integrations";
@@ -46,6 +47,7 @@ import {
   updateMaxAgentUsageAmount,
 } from "../features/AgentUsage/agentUsageSlice";
 import { ideToolCallResponse } from "../hooks/useEventBusForIDE";
+import { upsertToolCallIntoHistory } from "../features/History/historySlice";
 
 const AUTH_ERROR_MESSAGE =
   "There is an issue with your API key. Check out your API Key or re-login";
@@ -508,6 +510,12 @@ startListening({
   actionCreator: ideToolCallResponse,
   effect: (action, listenerApi) => {
     const state = listenerApi.getState();
+
+    // TODO: also handle this in confirmation box
+    if (action.payload.accepted === false) {
+      listenerApi.dispatch(upsertToolCallIntoHistory(action.payload));
+      listenerApi.dispatch(upsertToolCall(action.payload));
+    }
     listenerApi.dispatch(updateConfirmationAfterIdeToolUse(action.payload));
 
     const pauseReasons = state.confirmation.pauseReasons.filter(
@@ -516,6 +524,7 @@ startListening({
 
     if (pauseReasons.length === 0) {
       // TODO: it seems odd tool confirmation opens with no reasons.
+      // changin pause should remove it too.
       listenerApi.dispatch(
         clearPauseReasonsAndHandleToolsStatus({
           wasInteracted: true, // bit of a work around to enable auto send again.
