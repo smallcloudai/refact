6be4813c6e0bd82442ede245327ac0b74c671b6e
Humberto Yusta <humbertoyusta02@gmail.com>
2025-02-25 12:13:49 +0100

    fix: don't allow the model to write files that is not allowed to see due to privacy settings.

--- refact-agent/engine/src/tools/file_edit/tool_update_textdoc_regex.rs
+++ refact-agent/engine/src/tools/file_edit/tool_update_textdoc_regex.rs
@@ -1,6 +1,7 @@
 use crate::at_commands::at_commands::AtCommandsContext;
 use crate::call_validation::{ChatContent, ChatMessage, ContextEnum, DiffChunk};
 use crate::integrations::integr_abstract::IntegrationConfirmation;
+use crate::privacy::{check_file_privacy, load_privacy_if_needed, FilePrivacyLevel, PrivacySettings};
 use crate::tools::file_edit::auxiliary::{await_ast_indexing, convert_edit_to_diffchunks, str_replace_regex, sync_documents_ast};
 use crate::tools::tools_description::{MatchConfirmDeny, MatchConfirmDenyResult, Tool};
 use async_trait::async_trait;
@@ -23,7 +24,7 @@ struct ToolUpdateTextDocRegexArgs {
 
 pub struct ToolUpdateTextDocRegex;
 
-fn parse_args(args: &HashMap<String, Value>) -> Result<ToolUpdateTextDocRegexArgs, String> {
+fn parse_args(args: &HashMap<String, Value>, privacy_settings: Arc<PrivacySettings>) -> Result<ToolUpdateTextDocRegexArgs, String> {
     let path = match args.get("path") {
         Some(Value::String(s)) => {
             let path = PathBuf::from(preprocess_path_for_normalization(s.trim().to_string()));
@@ -34,6 +35,12 @@ fn parse_args(args: &HashMap<String, Value>) -> Result<ToolUpdateTextDocRegexArg
                 ));
             }
             let path = canonicalize_normalized_path(path);
+            if check_file_privacy(privacy_settings, &path, &FilePrivacyLevel::AllowToSendAnywhere).is_err() {
+                return Err(format!(
+                    "Error: Cannot update the file '{:?}' due to privacy settings.",
+                    s.trim()
+                ));
+            }
             if !path.exists() {
                 return Err(format!("argument 'path' doesn't exists: {:?}", path));
             }
@@ -81,7 +88,8 @@ pub async fn tool_update_text_doc_regex_exec(
     args: &HashMap<String, Value>,
     dry: bool
 ) -> Result<(String, String, Vec<DiffChunk>), String> {
-    let args = parse_args(args)?;
+    let privacy_settings = load_privacy_if_needed(gcx.clone()).await;
+    let args = parse_args(args, privacy_settings)?;
     await_ast_indexing(gcx.clone()).await?;
     let (before_text, after_text) = str_replace_regex(gcx.clone(), &args.path, &args.pattern, &args.replacement, args.multiple, dry).await?;
     sync_documents_ast(gcx.clone(), &args.path).await?;
@@ -122,8 +130,11 @@ impl Tool for ToolUpdateTextDocRegex {
         ccx: Arc<AMutex<AtCommandsContext>>,
         args: &HashMap<String, Value>,
     ) -> Result<MatchConfirmDeny, String> {
-        async fn can_execute_tool_edit(args: &HashMap<String, Value>) -> Result<(), String> {
-            let _ = parse_args(args)?;
+        let gcx = ccx.lock().await.global_context.clone();
+        let privacy_settings = load_privacy_if_needed(gcx.clone()).await;
+        
+        async fn can_execute_tool_edit(args: &HashMap<String, Value>, privacy_settings: Arc<PrivacySettings>) -> Result<(), String> {
+            let _ = parse_args(args, privacy_settings)?;
             Ok(())
         }
 
@@ -132,7 +143,7 @@ impl Tool for ToolUpdateTextDocRegex {
         // workaround: if messages weren't passed by ToolsPermissionCheckPost, legacy
         if msgs_len != 0 {
             // if we cannot execute apply_edit, there's no need for confirmation
-            if let Err(_) = can_execute_tool_edit(args).await {
+            if let Err(_) = can_execute_tool_edit(args, privacy_settings).await {
                 return Ok(MatchConfirmDeny {
                     result: MatchConfirmDenyResult::PASS,
                     command: "update_textdoc_regex".to_string(),
