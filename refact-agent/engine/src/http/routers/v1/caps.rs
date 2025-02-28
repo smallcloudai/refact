ba0a87a71f60a7a7c8cfa1d2e8a5c70f2ce7e0fa
MDario123 <manuel.dario.oliver@gmail.com>
2025-02-28 14:29:45 +0100

    refactor: /ping and /caps handlers

--- refact-agent/engine/src/http/routers/v1/caps.rs
+++ refact-agent/engine/src/http/routers/v1/caps.rs
@@ -8,31 +8,29 @@ use hyper::{Body, Response, StatusCode};
 use crate::custom_error::ScratchError;
 use crate::global_context::GlobalContext;
 
-
 pub async fn handle_v1_ping(
     Extension(gcx): Extension<Arc<ARwLock<GlobalContext>>>,
-    _: hyper::body::Bytes,
-) -> Result<Response<Body>, ScratchError> {
+) -> Response<Body> {
     let ping_message: String = gcx.read().await.cmdline.ping_message.clone();
-    let response = Response::builder()
-       .header("Content-Type", "application/json")
-       .body(Body::from(ping_message + "\n"))
-      .unwrap();
-    Ok(response)
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .body(Body::from(ping_message + "\n"))
+        .unwrap()
 }
 
 pub async fn handle_v1_caps(
     Extension(global_context): Extension<Arc<ARwLock<GlobalContext>>>,
-    _: hyper::body::Bytes,
 ) -> Result<Response<Body>, ScratchError> {
-    let caps_result = crate::global_context::try_load_caps_quickly_if_not_present(
-        global_context.clone(),
-        0,
-    ).await;
+    let caps_result =
+        crate::global_context::try_load_caps_quickly_if_not_present(global_context.clone(), 0)
+            .await;
     let caps_arc = match caps_result {
         Ok(x) => x,
         Err(e) => {
-            return Err(ScratchError::new(StatusCode::SERVICE_UNAVAILABLE, format!("{}", e)));
+            return Err(ScratchError::new(
+                StatusCode::SERVICE_UNAVAILABLE,
+                format!("{}", e),
+            ));
         }
     };
     let caps_locked = caps_arc.read().unwrap();
