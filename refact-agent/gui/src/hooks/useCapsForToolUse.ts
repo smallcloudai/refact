7197c57f676d0b221ae557c644a803161c66d5ea
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-03-03 11:12:29 +0100

    fix(model select): using array methods on a string causes gpt-4o and gpt-4o-mini to incorrectly match on caps selection.

--- refact-agent/gui/src/hooks/useCapsForToolUse.ts
+++ refact-agent/gui/src/hooks/useCapsForToolUse.ts
@@ -110,15 +110,14 @@ export function useCapsForToolUse() {
   useEffect(() => {
     if (
       usableModelsForPlan.length > 0 &&
-      usableModelsForPlan.some((elem) => elem.textValue.includes(currentModel))
+      usableModelsForPlan.some((elem) => elem.value === currentModel)
       // !usableModelsForPlan.includes(currentModel)
     ) {
       const models: string[] = usableModelsForPlan.map(
         (elem) => elem.textValue,
       );
       const toChange =
-        models.find((elem) => currentModel.startsWith(elem)) ??
-        (models[0] || "");
+        models.find((elem) => currentModel === elem) ?? (models[0] || "");
 
       setCapModel(toChange);
     }
