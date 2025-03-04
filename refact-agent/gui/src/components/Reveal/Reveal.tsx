27eb7583350548933fed2a13e09f060515cb064f
alashchev17 <andrew.lashchev15@gmail.com>
2025-03-04 14:25:04 +0100

    feat: hide details in revealed tool results

--- refact-agent/gui/src/components/Reveal/Reveal.tsx
+++ refact-agent/gui/src/components/Reveal/Reveal.tsx
@@ -15,27 +15,78 @@ export const Reveal: React.FC<RevealProps> = ({
   isRevealingCode = false,
 }) => {
   const [open, setOpen] = React.useState(defaultOpen);
-  if (open) return <Box width="100%">{children}</Box>;
+  if (open)
+    return (
+      <Box width="100%" position="relative">
+        {children}
+        <Button
+          variant="ghost"
+          onClick={() => {
+            if (defaultOpen) return;
+            setOpen((v) => !v);
+          }}
+          asChild
+          className={classNames(styles.reveal_button, {
+            [styles.reveal_button_inline]: !isRevealingCode,
+          })}
+        >
+          {!defaultOpen && (
+            <Box
+              className={`${styles.reveal_hidden} ${styles.reveal_hidden_exposed}`}
+            >
+              <Flex
+                position="absolute"
+                bottom="2"
+                width="100%"
+                justify="center"
+              >
+                {isRevealingCode ? (
+                  "Hide details"
+                ) : (
+                  <Text className={styles.reveal_text}>Hide details</Text>
+                )}
+              </Flex>
+            </Box>
+          )}
+        </Button>
+      </Box>
+    );
   return (
     <Button
       variant="ghost"
-      onClick={() => setOpen((v) => !v)}
+      onClick={() => {
+        if (defaultOpen) return;
+        setOpen((v) => !v);
+      }}
       asChild
       className={classNames(styles.reveal_button, {
         [styles.reveal_button_inline]: !isRevealingCode,
       })}
     >
       <Flex direction="column" position="relative" align="start">
-        <Box className={styles.reveal_hidden} width="100%">
+        <Box
+          className={classNames({
+            [styles.reveal_hidden]: !open,
+          })}
+          width="100%"
+        >
           {children}
         </Box>
-        <Flex position="absolute" bottom="2" width="100%" justify="center">
-          {isRevealingCode ? (
-            "Click for more"
-          ) : (
-            <Text className={styles.reveal_text}>Click for more</Text>
-          )}
-        </Flex>
+        {!defaultOpen && (
+          <Box
+            className={classNames({
+              [styles.reveal_button_box]: open,
+            })}
+          >
+            <Flex position="absolute" bottom="2" width="100%" justify="center">
+              {isRevealingCode ? (
+                "Click for more"
+              ) : (
+                <Text className={styles.reveal_text}>Click for more</Text>
+              )}
+            </Flex>
+          </Box>
+        )}
       </Flex>
     </Button>
   );
