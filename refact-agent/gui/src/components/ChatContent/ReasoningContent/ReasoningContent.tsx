ece3b2b709af5d7c3981d2add60fef12b970c6bb
alashchev17 <andrew.lashchev15@gmail.com>
2025-03-10 17:35:13 +0100

    feat(reasoning): updated UI for reasoning & thinking button, fixed tool call issue with thinking blocks for anthropic models

---
+++ refact-agent/gui/src/components/ChatContent/ReasoningContent/ReasoningContent.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Heading, Text } from "@radix-ui/themes";
+import React from "react";
+
+import { Markdown } from "../../Markdown";
+
+import styles from "./ReasoningContent.module.css";
+
+type ReasoningContentProps = {
+  reasoningContent: string;
+  onCopyClick: (text: string) => void;
+};
+
+export const ReasoningContent: React.FC<ReasoningContentProps> = ({
+  reasoningContent,
+  onCopyClick,
+}) => {
+  return (
+    <Box py="4">
+      <Flex direction="column" gap="2" className={styles.reasoningCallout}>
+        <Heading as="h3" color="gray" size="2">
+          Model Reasoning
+        </Heading>
+        <Text size="2" color="gray">
+          <Markdown canHaveInteractiveElements={true} onCopyClick={onCopyClick}>
+            {reasoningContent}
+          </Markdown>
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
