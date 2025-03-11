526f8d069fb43b84981797d43eb61870e18314c1
Kirill Starkov <starkov.kirill123@gmail.com>
2025-03-11 20:19:38 +0800

    fix build_base_docker.sh

--- refact-server/.github/scripts/build_base_docker.sh
+++ refact-server/.github/scripts/build_base_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( dirname "$0" )
 REPO_DIR=$(realpath $SCRIPT_DIR/../..)
 
 BASE_DOCKERFILE=${REPO_DIR}/Dockerfile.base
 
-docker buildx build --platform linux/amd64 -t ghcr.io/smallcloudai/refact_base_image:latest --push -f $BASE_DOCKERFILE $REPO_DIR
+docker buildx build --platform linux/amd64 -t ghcr.io/smallcloudai/refact_base_image:latest --push -f $BASE_DOCKERFILE $REPO_DIR --no-cache
