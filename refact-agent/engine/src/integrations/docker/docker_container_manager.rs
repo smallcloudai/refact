ca34da83ec9132d1d497cf19180699ed8b5cb2ae
MDario123 <manuel.dario.oliver@gmail.com>
2025-03-11 14:04:20 +0100

    fix: sync also ignored and big files needed for scenarios and seems more useful than bad

--- refact-agent/engine/src/integrations/docker/docker_container_manager.rs
+++ refact-agent/engine/src/integrations/docker/docker_container_manager.rs
@@ -427,9 +427,8 @@ async fn docker_container_sync_workspace(
     let (all_files, _vcs_folders) = crate::files_in_workspace::retrieve_files_in_workspace_folders(
         vec![workspace_folder.clone()],
         &mut indexing_everywhere,
-        false,
-<<<<<<< HEAD
-        false,
+        true,
+        true,
     )
     .await;
 
