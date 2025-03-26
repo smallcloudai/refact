655f4cc6946cb25338f05104c0a7d7c339c06fab
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-03-26 16:50:15 +0100

    feat: add second condition for pausing the chat.

--- refact-agent/gui/src/hooks/useCompressionStop.ts
+++ refact-agent/gui/src/hooks/useCompressionStop.ts
@@ -1,21 +1,36 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useMemo } from "react";
 import { useAppSelector } from "./useAppSelector";
 import { useAppDispatch } from "./useAppDispatch";
 import {
   selectLastSentCompression,
+  selectMessages,
   selectThreadPaused,
   setThreadPaused,
 } from "../features/Chat";
+import { takeFromEndWhile } from "../utils";
+import { isUserMessage } from "../events";
 
 export function useLastSentCompressionStop() {
   const dispatch = useAppDispatch();
   const lastSentCompression = useAppSelector(selectLastSentCompression);
+  const messages = useAppSelector(selectMessages);
   const stopped = useAppSelector(selectThreadPaused);
   useEffect(() => {
-    if (lastSentCompression && lastSentCompression !== "absent") {
+    if (lastSentCompression && lastSentCompression !== "absent" && !stopped) {
       dispatch(setThreadPaused(true));
     }
-  }, [dispatch, lastSentCompression]);
+  }, [dispatch, lastSentCompression, stopped]);
+
+  const messagesFromLastUserMessage = useMemo(() => {
+    return takeFromEndWhile(messages, (message) => !isUserMessage(message))
+      .length;
+  }, [messages]);
+
+  useEffect(() => {
+    if (messagesFromLastUserMessage >= 40 && !stopped) {
+      dispatch(setThreadPaused(true));
+    }
+  }, [dispatch, messagesFromLastUserMessage, stopped]);
 
   const resume = useCallback(() => {
     dispatch(setThreadPaused(false));
