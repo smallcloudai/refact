0dd59d5e586cabffea4e1006934b7e92b257a7a0
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-04-02 17:59:55 +0200

    take videos for tests

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -29,6 +29,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    video: "on",
   },
 
   /* Configure projects for major browsers */
