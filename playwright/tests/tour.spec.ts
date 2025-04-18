883c8dd20f509e8a85b2e67dec115ca31bafa113
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-04-18 18:26:53 +0200

    playwright: fix tour snapshots.

--- playwright/tests/tour.spec.ts
+++ playwright/tests/tour.spec.ts
@@ -16,13 +16,19 @@ test("Tour", async ({ page, baseURL }) => {
   await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step3();
-  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot({
+    maxDiffPixelRatio: 0.01,
+  });
 
   await tourPage.step4();
-  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot({
+    maxDiffPixelRatio: 0.01,
+  });
 
   await tourPage.step5();
-  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot({
+    maxDiffPixelRatio: 0.01,
+  });
 
   await tourPage.step6();
   await expect(page.getByTestId("tour-box")).toMatchAriaSnapshot();
@@ -31,8 +37,10 @@ test("Tour", async ({ page, baseURL }) => {
   });
 
   await tourPage.step7();
-  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot({
+    maxDiffPixelRatio: 0.01,
+  });
 
   await tourPage.step8();
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
 });
