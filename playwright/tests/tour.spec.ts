ce4f9dc8bd080c504f0c16d2201059c019719ab0
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-04-18 17:08:51 +0200

    playwright: update tour snapshots

--- playwright/tests/tour.spec.ts
+++ playwright/tests/tour.spec.ts
@@ -8,24 +8,25 @@ test("Tour", async ({ page, baseURL, tourPage }) => {
 
   await tourPage.step1();
   await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step2();
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step3();
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step4();
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step5();
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step6();
-  await expect(page).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step7();
-  await expect(page).toHaveScreenshot();
+  await expect(page.getByTestId("tour-box")).toHaveScreenshot();
 
   await tourPage.step8();
   await expect(page).toHaveScreenshot();
