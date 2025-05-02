8af36dbfa7746977c3616e679a02e5fe07d5e5e2
alashchev17 <andrew.lashchev15@gmail.com>
2025-05-02 16:59:35 +0200

    feat: sending login_message to the IDE on getting user datra

--- refact-agent/gui/src/utils/toPascalCase.ts
+++ refact-agent/gui/src/utils/toPascalCase.ts
@@ -1,8 +1,6 @@
 export function toPascalCase(value: string) {
   return value
-    .split("_")
+    .split(/[_\-\s]+/)
     .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
-    .join(" ")
-    .split("-")
     .join(" ");
 }
