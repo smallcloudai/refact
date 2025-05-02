289ad2493161c71ff5c9b45e4a9abb83823a401f
MDario123 <manuel.dario.oliver@gmail.com>
2025-05-02 12:59:52 +0200

    fix: better logging and maybe wait less

--- refact-agent/engine/src/indexing_utils.rs
+++ refact-agent/engine/src/indexing_utils.rs
@@ -3,6 +3,7 @@ use tracing::info;
 use tokio::sync::RwLock as ARwLock;
 use std::sync::Arc;
 
+use crate::ast::ast_structs::AstStatus;
 use crate::global_context::GlobalContext;
 use crate::http::routers::v1::status::get_rag_status;
 
@@ -16,28 +17,36 @@ pub async fn wait_for_indexing(
         gcx_locked.cmdline.clone()
     };
 
-    let mut waiting_ast = cmdline.wait_ast;
+    let ast_done = async || get_rag_status(gcx.clone()).await.ast.is_some_and(|ast_status| ast_status.astate == "done");
     #[cfg(feature = "vecdb")]
-    let mut waiting_vecdb = cmdline.wait_vecdb;
+    let vecdb_done = async || get_rag_status(gcx.clone()).await.vecdb.is_some_and(|vecdb_status| vecdb_status.state == "done");
+
+    let mut waiting_ast = cmdline.wait_ast && !ast_done().await;
+    #[cfg(feature = "vecdb")]
+    let mut waiting_vecdb = cmdline.wait_vecdb && !vecdb_done().await;
     #[cfg(not(feature = "vecdb"))]
     let mut waiting_vecdb = false;
 
+    if waiting_ast {
+        info!("Waiting for AST to finish indexing.");
+    }
+    if waiting_vecdb {
+        info!("Waiting for Vecdb to finish indexing.");
+    }
+
     while waiting_ast || waiting_vecdb {
-        let status = get_rag_status(gcx.clone()).await;
-        info!("Waiting for LSP to finish indexing:\n{:?}", status);
-        if let Some(ast_status) = &status.ast {
-            if waiting_ast && ast_status.astate == "done" {
-                info!("Ast finished indexing.");
-                waiting_ast = false;
-            }
+        if waiting_ast && ast_done().await {
+            info!("Ast finished indexing.");
+            waiting_ast = false;
         }
         #[cfg(feature = "vecdb")]
-        if let Some(vecdb_status) = &status.vecdb {
-            if waiting_vecdb && vecdb_status.state == "done" {
-                info!("Vecdb finished indexing.");
-                waiting_vecdb = false;
-            }
+        if waiting_vecdb && vecdb_done().await {
+            info!("Vecdb finished indexing.");
+            waiting_vecdb = false;
         }
-        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        if waiting_ast || waiting_vecdb {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
     }
 }
