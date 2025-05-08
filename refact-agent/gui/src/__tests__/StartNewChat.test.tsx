a6862965dd74c64fde4914e318b4d5e001b0e7f7
Marc McIntosh <marcmcintosh1987@gmail.com>
2025-05-08 17:52:04 +0200

    Migrate jsdom to happy-dom (#760)
    * wip: migrate jsdom to happy-dom
    
     interactive rebase in progress; onto 42a28de5
     Last command done (1 command done):
        pick 99fa40e7 wip: migrate jsdom to happy-dom
     No commands remaining.
     You are currently rebasing branch 'migrate-to-happy-dom' on '42a28de5'.
    
     Changes to be committed:
    	modified:   refact-agent/gui/package-lock.json
    	modified:   refact-agent/gui/package.json
    	modified:   refact-agent/gui/src/__fixtures__/msw.ts
    	modified:   refact-agent/gui/src/__tests__/DeleteChat.test.tsx
    	modified:   refact-agent/gui/src/utils/test-setup.ts
    	modified:   refact-agent/gui/vite.config.ts
    
    * fix: DOMException [AbortError]: The operation was aborted.
    
    * tmp: patch happy dom and enable node v24 in tests.
    
    * chore: uninstall jsdom

--- refact-agent/gui/src/__tests__/StartNewChat.test.tsx
+++ refact-agent/gui/src/__tests__/StartNewChat.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "../utils/test-utils";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, beforeEach, afterEach } from "vitest";
 import {
   server,
   goodPrompts,
@@ -18,36 +18,42 @@ import { stubResizeObserver } from "../utils/test-utils";
 
 describe("Start a new chat", () => {
   // TODO: this shouldn't need to be called here.
-  stubResizeObserver();
 
-  server.use(
-    goodPing,
-    goodCaps,
-    goodPrompts,
-    noTools,
-    noCommandPreview,
-    noCompletions,
-    goodUser,
-    chatLinks,
-    telemetryChat,
-    telemetryNetwork,
-  );
+  beforeEach(() => {
+    stubResizeObserver();
 
-  const { user, ...app } = render(<InnerApp />, {
-    preloadedState: {
-      pages: [{ name: "history" }],
-      config: {
-        apiKey: "test",
-        lspPort: 8001,
-        themeProps: {},
-        host: "vscode",
-        addressURL: "Refact",
-      },
-    },
+    server.use(
+      goodPing,
+      goodCaps,
+      goodPrompts,
+      noTools,
+      noCommandPreview,
+      noCompletions,
+      goodUser,
+      chatLinks,
+      telemetryChat,
+      telemetryNetwork,
+    );
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
   });
 
   // TODO: copy this for other tests done at a higher level
   test("open chat with New Chat Button", async () => {
+    const { user, ...app } = render(<InnerApp />, {
+      preloadedState: {
+        pages: [{ name: "history" }],
+        config: {
+          apiKey: "test",
+          lspPort: 8001,
+          themeProps: {},
+          host: "vscode",
+          addressURL: "Refact",
+        },
+      },
+    });
     const btn = app.getByText("New chat");
     await user.click(btn);
 
