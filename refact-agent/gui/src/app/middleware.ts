393d3210d657c5aaf21f00f5e7addb06e95f0c12
alashchev17 <andrew.lashchev15@gmail.com>
2025-05-16 13:26:05 +0200

    refactor: removed old tools handler's code

--- refact-agent/gui/src/app/middleware.ts
+++ refact-agent/gui/src/app/middleware.ts
@@ -377,20 +377,12 @@ startListening({
   actionCreator: newIntegrationChat,
   effect: async (_action, listenerApi) => {
     const state = listenerApi.getState();
-
-    // TBD: should the endpoint need tools?
-    const toolGroupsRequest = listenerApi.dispatch(
-      toolsApi.endpoints.getToolGroups.initiate(undefined),
-    );
-    toolGroupsRequest.unsubscribe();
-    // const toolResult = await toolsRequest.unwrap();
     // TODO: set mode to configure ? or infer it later
     // TODO: create a dedicated thunk for this.
     await listenerApi.dispatch(
       chatAskQuestionThunk({
         messages: state.chat.thread.messages,
         chatId: state.chat.thread.id,
-        // tools: toolResult,
       }),
     );
   },
