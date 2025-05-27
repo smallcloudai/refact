30ad919c061b63d98d6facc800ac5b828be16634
alashchev17 <andrew.lashchev15@gmail.com>
2025-05-27 17:20:37 +0200

    feat: fetching user's workspaces, selecting workspace and fetching groups

--- refact-agent/gui/codegen.ts
+++ refact-agent/gui/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:8008/v1/graphql",
+  schema: "http://localhost:8008/v1/graphql", // requires flexus codebase to be running
   documents: ["src/**/*.(tsx|graphql)"],
   ignoreNoDocuments: true,
   generates: {
