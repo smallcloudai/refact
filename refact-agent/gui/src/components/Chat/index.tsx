3056051eca772c1d8b408f17a32aec3fa215d8b7
JegernOUTT <sergey.vakhreev@gmail.com>
2025-11-28 01:14:02 +1030

    model selector in the middle of a thread

--- refact-agent/gui/src/components/Chat/index.tsx
+++ refact-agent/gui/src/components/Chat/index.tsx
@@ -1,2 +1,4 @@
 export { Chat } from "./Chat";
 export type { ChatProps } from "./Chat";
+export { ModelSelector } from "./ModelSelector";
+export type { ModelSelectorProps } from "./ModelSelector";
